8610a3a14a24868bc7418b2f9f166cfeaba87a9e
Alex Ethier <alex@tower3.io>
2019-07-28 23:43:58 -0700

    deploy(si-graphql): implement deployment scripts
    Implement simple deployment scripts to:
     - install dependencies
     - initialize database
     - run service in development mode

---
+++ components/si-graphql/scripts/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that directory
+cd "$DIR"
+
+# Run the app
+echo "-------------------------------------------------"
+echo "==> Starting up service"
+echo "-------------------------------------------------"
+npm start
