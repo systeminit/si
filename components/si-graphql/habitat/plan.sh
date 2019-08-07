8b5190256faff1a9fc03e03f5f998d84648d0374
Adam Jacob <adam@systeminit.com>
2019-08-06 17:44:55 -0700

    build(si-graphql): Adding habitat build (#6)
    This uses habitat to build a minimal si-graphql backend.

---
+++ components/si-graphql/habitat/plan.sh
@@ -0,0 +1,25 @@
+pkg_name=si-graphql
+pkg_origin=si
+pkg_version="0.0.1"
+pkg_maintainer="System Initiative <adam@systeminit.com>"
+pkg_license=("Proprietary")
+pkg_shasum="TODO"
+pkg_deps=(core/node core/sqlite)
+pkg_build_deps=()
+
+do_build() {
+  npm install
+  npm run build
+  npm prune  --production
+}
+
+do_install() {
+  cp -r ./node_modules $pkg_prefix
+  cp -r knexfile.js $pkg_prefix
+  cp -r migrations $pkg_prefix
+  cp -r ./dist $pkg_prefix
+}
+
+do_strip() {
+  return 0;
+}
--- components/si-graphql/webpack.production.js
+++ components/si-graphql/webpack.production.js
@@ -1,14 +1,14 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const merge = require('webpack-merge');
-const nodeExternals = require('webpack-node-externals');
-const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const merge = require("webpack-merge");
+const nodeExternals = require("webpack-node-externals");
+const path = require("path");
 
-const common = require('./webpack.common.js');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  devtool: 'source-map',
-  entry: [path.join(__dirname, 'src/main.ts')],
+  devtool: "source-map",
+  entry: [path.join(__dirname, "src/main.ts")],
   externals: [nodeExternals()],
-  mode: 'production',
+  mode: "production",
   plugins: [new CleanWebpackPlugin()],
 });
