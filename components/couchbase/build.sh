35dd4c91fc567b30ad6252de2a1ac0cdc7957570
Adam Jacob <adam@systeminit.com>
2019-12-20 15:09:53 -0800

    Try and start the service

--- components/couchbase/build.sh
+++ components/couchbase/build.sh
@@ -3,4 +3,4 @@
 TAG="6.5.0-beta2"
 IMAGE="si/couchbase"
 
-docker build -t ${IMAGE}:${TAG} -t ${IMAGE}:latest .
+docker build -t docker.pkg.github.com/systeminit/${IMAGE}:${TAG} -t docker.pkg.github.com/systeminit/${IMAGE}:latest -t ${IMAGE}:${TAG} -t ${IMAGE}:latest .
