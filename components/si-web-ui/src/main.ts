0a5f2f90d0db8829b1b592c4d643915e693857fb
Adam Jacob <adam@systeminit.com>
2020-01-07 16:26:57 -0800

    feat: Add gtag support (#43)

--- components/si-web-ui/src/main.ts
+++ components/si-web-ui/src/main.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
 import { createProvider } from "./vue-apollo";
 import { auth } from "./auth";
 import vuetify from "./plugins/vuetify";
@@ -9,6 +10,17 @@ Vue.config.productionTip = false;
 
 let apolloProvider = createProvider();
 auth.setApollo(apolloProvider);
+
+if (process.env.NODE_ENV == "production") {
+  Vue.use(VueGtag, {
+    config: { id: "UA-151349900-2" },
+  });
+} else {
+  Vue.use(VueGtag, {
+    config: { id: "UA-151349900-2" },
+    disableScriptLoad: true,
+  });
+}
 
 new Vue({
   router,
