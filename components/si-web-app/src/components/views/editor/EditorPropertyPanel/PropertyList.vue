f71766511d0c6885f9aab57f040fd5604b9ec7d9
Alex Ethier <alex@systeminit.com>
2020-05-18 15:29:33 -0700

    save point: org is undefined

---
+++ components/si-web-app/src/components/views/editor/EditorPropertyPanel/PropertyList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="property-panel-list">
+
+<!--     <div v-for="field of objMethod.properties.attrs" v-bind:key="field.name">
+      <div v-if="field.kind() == 'text'">
+        <v-text-field
+          v-model="objVariables[field.name]"
+          :label="field.label"
+          :name="field.name"
+        ></v-text-field>
+      </div>
+    </div> -->
+
+  </div>
+</template>
+
+<script>
+import { registry } from "si-registry";
+const items = registry.get("item");
+console.log(items)
+
+
+export default {
+  name: "PropertyList",
+  components: {
+  },
+};
+</script>
