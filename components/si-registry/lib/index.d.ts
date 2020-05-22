63d142c941d14200e1c8791cb331b5703014b9b0
Alex Ethier <51493434+alex-init@users.noreply.github.com>
2020-05-22 10:03:16 -0700

    refactor(si-registry): export graphql method (#95)
    Publicly expose variablesObjectForProperty()

--- components/si-registry/lib/index.d.ts
+++ components/si-registry/lib/index.d.ts
@@ -3,3 +3,4 @@ export { registry } from "./registry";
 export * from "./components/prelude";
 export { ObjectTypes } from "./systemComponent";
 export { Associations, BelongsTo, HasMany } from "./systemObject/associations";
+export { variablesObjectForProperty } from "./systemObject/graphql";
