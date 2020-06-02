f9b0efcb7ca25d7e309b75831c54dd269195bf61
Alex Ethier <alex@systeminit.com>
2020-06-02 15:21:52 -0700

    save point: fixed replicas number, was passoing string because the backend didn't take an int, now that the backend can take int, I reverted replicas to a number

--- components/si-web-app/src/components/views/editor/EditorPropertyPanel/PropObjectProperty.vue
+++ components/si-web-app/src/components/views/editor/EditorPropertyPanel/PropObjectProperty.vue
@@ -171,14 +171,16 @@
           </div>
 
 <!-- 
+  type="text"
   type="number"
   v-model.number="objectModel" below isn't wokring because the api want string.
--->
+  v-model="objectModel"
+-->  
           <input
             class="appearance-none text-sm leading-tight focus focus:outline-none input-bg-color border-none text-gray-400 pl-2 h-5 w-32"
-            type="text"
+            type="number"
             :aria-label="propObjectProperty.name"
-            v-model="objectModel"
+            v-model.number="objectModel"
             placeholder="number"
           />
         </div>
