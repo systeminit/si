7780015a7c22d084f8a7b8d0751486935b41e506
Adam Jacob <adam@systeminit.com>
2020-06-22 13:22:42 -0700

    chore: no longer check in generated code
    All this was doing for us was causing churn. The idea was it would be
    useful for CI, but that's dumb. We should solve that problem by making
    the environment match, and then doing generation at the build stage.

--- components/si-registry/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import "./loader";
-export { registry } from "./registry";
-export * from "./components/prelude";
-export { ObjectTypes } from "./systemComponent";
-export { Associations, BelongsTo, HasMany } from "./systemObject/associations";
-export { variablesObjectForProperty, QueryArgs } from "./systemObject/graphql";
