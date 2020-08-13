f887633cb946823490905a05edf422eecd018f9c
Adam Jacob <adam@systeminit.com>
2020-08-13 15:03:36 -0700

    chore: fixing bugs for end to end

--- components/si-account/src/model/event_log.rs
+++ components/si-account/src/model/event_log.rs
@@ -6,6 +6,8 @@ pub use crate::protobuf::{
 };
 use si_data::{DataError, Db, ListResult, Result};
 
+use tracing::warn;
+
 impl EventLog {
     pub async fn entity_create(
         db: &Db,
@@ -202,6 +204,172 @@ impl EventLog {
                 workspace_id.into(),
                 organization_id.into(),
                 change_set_id.into(),
+                id.into(),
+            ],
+            Some(timestamp.to_string()),
+            Some(created_by_user_id.to_string()),
+        )
+        .await?;
+        Ok(())
+    }
+
+    pub async fn change_set_entry_execute_end(
+        db: &Db,
+        created_by_user_id: &str,
+        change_set_entry_json: &serde_json::Value,
+    ) -> Result<()> {
+        let billing_account_id = change_set_entry_json["siProperties"]["billingAccountId"]
+            .as_str()
+            .ok_or(DataError::RequiredField(
+                "siProperties.billingAccountId".into(),
+            ))?;
+        let workspace_id = change_set_entry_json["siProperties"]["workspaceId"]
+            .as_str()
+            .ok_or(DataError::RequiredField("siProperties.workspaceId".into()))?;
+        let organization_id = change_set_entry_json["siProperties"]["organizationId"]
+            .as_str()
+            .ok_or(DataError::RequiredField(
+                "siProperties.organizationId".into(),
+            ))?;
+        let type_name = change_set_entry_json["siStorable"]["typeName"]
+            .as_str()
+            .ok_or(DataError::RequiredField("siStorable.typeName".into()))?;
+
+        let message = if type_name.ends_with("entity_event") {
+            let name = change_set_entry_json["inputEntity"]["name"]
+                .as_str()
+                .ok_or(DataError::RequiredField("inputEntity.name".into()))?;
+            let action = change_set_entry_json["actionName"]
+                .as_str()
+                .ok_or(DataError::RequiredField("actionName".into()))?;
+            let type_name = change_set_entry_json["inputEntity"]["siStorable"]["typeName"]
+                .as_str()
+                .ok_or(DataError::RequiredField(
+                    "inputEntity.siStorable.typeName".into(),
+                ))?;
+            format!(
+                "finished executing change set entry action {} for {} named {}",
+                action, type_name, name,
+            )
+        } else {
+            let name = change_set_entry_json["name"]
+                .as_str()
+                .ok_or(DataError::RequiredField("name".into()))?;
+            format!(
+                "finished executing change set entry for {} named {}",
+                type_name, name
+            )
+        };
+        let id = change_set_entry_json["id"]
+            .as_str()
+            .ok_or(DataError::RequiredField("id".into()))?;
+
+        let payload_data = change_set_entry_json.to_string();
+
+        let timestamp = Utc::now();
+
+        EventLog::create(
+            db,
+            Some("Change Set Entry Executed".into()),
+            Some("Change Set Entry Executed".into()),
+            Some(EventLogLevel::Info),
+            Some(EventLogSiProperties {
+                billing_account_id: Some(billing_account_id.into()),
+                workspace_id: Some(workspace_id.into()),
+                organization_id: Some(organization_id.into()),
+            }),
+            Some(message),
+            Some(EventLogPayload {
+                kind: Some("change_set_entry".into()),
+                data: Some(payload_data),
+            }),
+            vec![
+                billing_account_id.into(),
+                workspace_id.into(),
+                organization_id.into(),
+                id.into(),
+            ],
+            Some(timestamp.to_string()),
+            Some(created_by_user_id.to_string()),
+        )
+        .await?;
+        Ok(())
+    }
+
+    pub async fn change_set_entry_execute_start(
+        db: &Db,
+        created_by_user_id: &str,
+        change_set_entry_json: &serde_json::Value,
+    ) -> Result<()> {
+        let billing_account_id = change_set_entry_json["siProperties"]["billingAccountId"]
+            .as_str()
+            .ok_or(DataError::RequiredField(
+                "siProperties.billingAccountId".into(),
+            ))?;
+        let workspace_id = change_set_entry_json["siProperties"]["workspaceId"]
+            .as_str()
+            .ok_or(DataError::RequiredField("siProperties.workspaceId".into()))?;
+        let organization_id = change_set_entry_json["siProperties"]["organizationId"]
+            .as_str()
+            .ok_or(DataError::RequiredField(
+                "siProperties.organizationId".into(),
+            ))?;
+        let type_name = change_set_entry_json["siStorable"]["typeName"]
+            .as_str()
+            .ok_or(DataError::RequiredField("siStorable.typeName".into()))?;
+
+        let message = if type_name.ends_with("entity_event") {
+            let name = change_set_entry_json["inputEntity"]["name"]
+                .as_str()
+                .ok_or(DataError::RequiredField("inputEntity.name".into()))?;
+            let action = change_set_entry_json["actionName"]
+                .as_str()
+                .ok_or(DataError::RequiredField("actionName".into()))?;
+            let type_name = change_set_entry_json["inputEntity"]["siStorable"]["typeName"]
+                .as_str()
+                .ok_or(DataError::RequiredField(
+                    "inputEntity.siStorable.typeName".into(),
+                ))?;
+            format!(
+                "starting to execute change set entry action {} for {} named {}",
+                action, type_name, name,
+            )
+        } else {
+            let name = change_set_entry_json["name"]
+                .as_str()
+                .ok_or(DataError::RequiredField("name".into()))?;
+            format!(
+                "starting to execute change set entry for {} named {}",
+                type_name, name
+            )
+        };
+        let id = change_set_entry_json["id"]
+            .as_str()
+            .ok_or(DataError::RequiredField("id".into()))?;
+
+        let payload_data = change_set_entry_json.to_string();
+
+        let timestamp = Utc::now();
+
+        EventLog::create(
+            db,
+            Some("Change Set Entry Executed".into()),
+            Some("Change Set Entry Executed".into()),
+            Some(EventLogLevel::Info),
+            Some(EventLogSiProperties {
+                billing_account_id: Some(billing_account_id.into()),
+                workspace_id: Some(workspace_id.into()),
+                organization_id: Some(organization_id.into()),
+            }),
+            Some(message),
+            Some(EventLogPayload {
+                kind: Some("change_set_entry".into()),
+                data: Some(payload_data),
+            }),
+            vec![
+                billing_account_id.into(),
+                workspace_id.into(),
+                organization_id.into(),
                 id.into(),
             ],
             Some(timestamp.to_string()),
