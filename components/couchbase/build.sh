9ee5d2142af383ad03c8fdaf50fb172688a6313d
Fletcher Nichol <fnichol@nichol.ca>
2020-11-17 10:53:21 -0700

    chore(db): add `relaunch.sh` for couchbase
    This change adds a `components/couchbase/relaunch.sh` script which will
    destroy any currently running `db` container instance and re-launch a
    fresh/new copy. This is useful when debugging or developing new features
    that dramatically affect the data model. The `build.sh` and `run.sh`
    scripts got a refresh so they could be more easily reused and called
    from `launch.sh`.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/couchbase/build.sh
+++ components/couchbase/build.sh
@@ -1,15 +1,36 @@
-#!/bin/sh -ex
+#!/usr/bin/env sh
+# shellcheck shell=sh disable=SC2039
 
-REPO="systeminit"
-TAG="latest"
-IMAGE="couchbase"
-DOCKER_CONTAINER_NAME="db"
+main() {
+  set -eu
+  if [ -n "${DEBUG:-}" ]; then set -v; fi
+  if [ -n "${TRACE:-}" ]; then set -xv; fi
 
-echo "# Couchbase Memory Configuration" > ./.env
-echo "export DATA_RAM_QUOTA=${DATA_RAM_QUOTA:-2048}" >> ./.env
-echo "export INDEX_RAM_QUOTA=${INDEX_RAM_QUOTA:-512}" >> ./.env
-echo "export FULL_TEXT_RAM_QUOTA=${FULL_TEXT_RAM_QUOTA:-512}" >> ./.env
-echo "export BUCKET_SI_RAM_QUOTA=${BUCKET_SI_RAM_QUOTA:-512}" >> ./.env
-echo "export BUCKET_SI_INTEGRATION_RAM_QUOTA=${BUCKET_SI_INTEGRATION_RAM_QUOTA:-512}" >> ./.env
+  local image="systeminit/couchbase:latest"
 
-docker rmi ${REPO}/${IMAGE}:${TAG} && docker build -t ${REPO}/${IMAGE}:${TAG} . || docker build -t ${REPO}/${IMAGE}:${TAG} .
+  if image_built "$image"; then
+    echo "--- Removing preexisting image '$image'"
+    docker image rm "$image"
+  fi
+
+  # Change to directory containing Dockerfile context
+  cd "${0%/*}"
+
+  echo "--- Building image '$image'"
+  cat <<-EOF >.env
+	# Couchbase Memory Configuration
+	export DATA_RAM_QUOTA=${DATA_RAM_QUOTA:-2048}
+	export INDEX_RAM_QUOTA=${INDEX_RAM_QUOTA:-512}
+	export FULL_TEXT_RAM_QUOTA=${FULL_TEXT_RAM_QUOTA:-512}
+	export BUCKET_SI_RAM_QUOTA=${BUCKET_SI_RAM_QUOTA:-512}
+	export BUCKET_SI_INTEGRATION_RAM_QUOTA=${BUCKET_SI_INTEGRATION_RAM_QUOTA:-512}
+	EOF
+  cat .env
+  docker image build --tag "$image" .
+}
+
+image_built() {
+  [ -n "$(docker image ls --quiet "$1")" ]
+}
+
+main "$@" || exit 1
