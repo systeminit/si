aeda5e5ee629d878baf6bcd50cda97893508af84
Fletcher Nichol <fnichol@nichol.ca>
2020-12-16 17:49:48 -0700

    chore: restore `make release`
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/build/rust.mk
+++ components/build/rust.mk
@@ -8,32 +8,40 @@ RUST_LOG ?= info
 .PHONY: build build_release test run start release container
 
 build:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	cargo build
 
 build_release:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	cargo build --release
 
 test:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	env RUST_BACKTRACE=1 RUST_LOG=debug cargo test -- --nocapture
 
 run:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	cargo run
 
 start: run
 
 clean:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	cargo clean
 
 watch:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	env RUST_LOG=$(RUST_LOG) cargo watch $(foreach path,$(WATCH_PATHS),-w $(path)) -x $(WATCH_TASK)
 
 test_container:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	docker run -t --network=host --volume=$(CURDIR)/../../:/src docker.pkg.github.com/systeminit/si/si-base:latest /bin/bash -c "cd / && if [[ ! -d /src/target ]]; then tar zxf /build-cache/cargo-cache.tgz; fi && if [[ ! -d /src/components/si-web-ui/node_modules ]]; then tar zxf /build-cache/npm-cache.tgz; fi && . /root/.cargo/env && cd /src/components/$(COMPONENT) && make test"
 
 # -t docker.pkg.github.com/systeminit/si/${COMPONENT}-service:latest \
 # -t docker.pkg.github.com/systeminit/si/${COMPONENT}-service:$(RELEASE) \
 
 container:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	env BUILDKIT_PROGRESS=plain DOCKER_BUILDKIT=1 docker build \
 		-f $(CURDIR)/../${COMPONENT}/Dockerfile \
 		-t systeminit/${COMPONENT}:latest \
@@ -42,5 +50,6 @@ container:
 		$(CURDIR)/../../
 
 release: container
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	docker push systeminit/${COMPONENT}:latest
 	docker push systeminit/${COMPONENT}:$(RELEASE)
--- components/build/typescript.mk
+++ components/build/typescript.mk
@@ -2,37 +2,40 @@ include $(CURDIR)/../build/docker.mk
 
 RELEASE := $(shell date +%Y%m%d%H%M%S)
 
-.PHONY: build build_release clean test run install
-
-install: 
-	npm install
+.PHONY: build build_release clean test run node_modules
 
 node_modules: package.json
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	npm install
 
 build: node_modules
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	env NODE_ENV=production npm run build
 
 build_release: build
 
 test: node_modules
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	npm run test
 
 run: start
 
 start: node_modules
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	npm run start
 
 watch: start
-	@ echo "Watching.."
 
 clean:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	rm -rf ./node_modules ./dist ./lib
 
 test_container:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	docker run -t --network=host --volume=$(CURDIR)/../../:/src docker.pkg.github.com/systeminit/si/si-base:latest /bin/bash -c "cd / && if [[ ! -d /src/target ]]; then tar zxf /build-cache/cargo-cache.tgz; fi && if [[ ! -d /src/components/si-web-ui/node_modules ]]; then tar zxf /build-cache/npm-cache.tgz; fi && . /root/.cargo/env && cd /src/components/$(COMPONENT) && make test"
 
 container:
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	env BUILDKIT_PROGRESS=plain DOCKER_BUILDKIT=1 docker build \
 		-f $(CURDIR)/../${COMPONENT}/Dockerfile \
 		-t systeminit/${CONTAINER}:latest \
@@ -40,6 +43,6 @@ container:
 		$(CURDIR)/../../
 
 release: container
+	@echo "--- [$(shell basename ${CURDIR})] $@"
 	docker push systeminit/${CONTAINER}:latest
 	docker push systeminit/${CONTAINER}:$(RELEASE)
-
