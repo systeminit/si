fc6c1dc8a0f808ec8208fa6dfe1155156d6eaceb
Fletcher Nichol <fnichol@nichol.ca>
2021-01-25 12:13:40 -0700

    chore: remove couchbase dependency from project (#269)
    This change finishes the removal of couchbase throughout the codebase,
    mostly thanks to `rg couch`. Thanks for the help Couchbase, we'll catch
    you next time!
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/si-settings/tests/integration.rs
+++ components/si-settings/tests/integration.rs
@@ -7,7 +7,7 @@ use si_settings::Settings;
 lazy_static! {
     pub static ref SETTINGS: Settings = {
         env::set_var("RUN_ENV", "testing");
-        env::set_var("SI_DB__CLUSTER_PASSWORD", "testing");
+        env::set_var("SI_PG__PASSWORD", "testing");
         Settings::new().expect("Failed to load settings")
     };
 }
@@ -19,15 +19,15 @@ fn uses_default_values() {
 
 #[test]
 fn uses_run_env_values() {
-    assert_eq!(SETTINGS.db.cluster_url, "couchbase://127.0.0.2");
+    assert_eq!(SETTINGS.pg.hostname, "127.0.0.2");
 }
 
 #[test]
 fn uses_local_toml_values() {
-    assert_eq!(SETTINGS.db.cluster_user, "monkey");
+    assert_eq!(SETTINGS.pg.user, "monkey");
 }
 
 #[test]
 fn uses_environment_variables() {
-    assert_eq!(SETTINGS.db.cluster_password, "testing");
+    assert_eq!(SETTINGS.pg.password, "testing");
 }
