240e4d85b687b88986b43b40d5adb2923b028029
Alex Ethier <51493434+alex-init@users.noreply.github.com>
2021-05-05 14:37:03 -0700

    feat(si-web-app): implement multi orientation node graphs prototype (#342)
    Prototype:
     - Sockets render according to inputs.
     - Connections are rendered horizontally or vertically based on the schematic orientation.
     - Socket lyout is hardcoded.

--- components/si-web-app/src/organisims/SchematicViewer/Edge.vue
+++ components/si-web-app/src/organisims/SchematicViewer/Edge.vue
@@ -27,28 +27,74 @@
 
 import Vue, { PropType } from "vue";
 
-import { PanelEventBus } from "@/atoms/PanelEventBus";
+import _ from "lodash";
+
+import { RegistryEntry } from "si-registry";
+import { SiEntity } from "si-entity";
+
 import { Cg2dCoordinate } from "@/api/sicg";
 import { Edge } from "@/api/sdf/model/edge";
+import { Entity } from "@/api/sdf/model/entity";
 import { ISchematicNode, Schematic } from "@/api/sdf/model/schematic";
 
-const INPUT_SOCKET_OFFSET = {
-  x: 68.5, // node center.
-  y: 0, // top line of a node
+import { PanelEventBus } from "@/atoms/PanelEventBus";
+
+import { SchematicOrientation } from "@/organisims/SchematicViewer.vue";
+
+const INPUT_SOCKET_OFFSET_VERTICAL = {
+  x: 68.5,
+  y: 0,
 };
 
-const OUTPUT_SOCKET_OFFSET = {
-  x: 68.5, // node center.
-  y: 100, // bottom line of a node
+const INPUT_SOCKET_OFFSET_HORIZONTAL = {
+  x: 0,
+  y: 61,
 };
+
+const OUTPUT_SOCKET_OFFSET_VERTICAL = {
+  x: 68.5,
+  y: 100,
+};
+
+const OUTPUT_SOCKET_OFFSET_HORIZONTAL = {
+  x: 138,
+  y: 36,
+};
+
+const INPUT_SOCKET_ALIGNMENT_OFSET = {
+  x: 0,
+  y: 16,
+};
+
+// ----------------------------------------
+// Connection
+export interface Connection {
+  kind: ConnectionKind;
+  from: Source;
+  to: Source;
+  edge: Edge;
+}
+
+export interface Source {
+  nodeId: string;
+  socketId: string;
+}
+
+export enum ConnectionKind {
+  Deployment = "deployment",
+  Implementation = "implementation",
+}
+// ----------------------------------------
 
 export interface EdgeTemporary {
   id: string;
   headVertex: {
     nodeId: string;
+    socket?: string;
   };
   tailVertex: {
     nodeId: string;
+    socket?: string;
   };
 }
 
@@ -93,6 +139,10 @@ export default Vue.extend({
       required: true,
     },
     positionCtx: String,
+    orientation: {
+      type: String as PropType<SchematicOrientation>,
+      default: "horizontal",
+    },
   },
   data(): IData {
     return {
@@ -128,15 +178,30 @@ export default Vue.extend({
           x: Number(this.sourceNode.node.positions[context].x),
           y: Number(this.sourceNode.node.positions[context].y),
         };
-        return {
-          x: sourceNodePosition.x + OUTPUT_SOCKET_OFFSET.x,
-          y: sourceNodePosition.y + OUTPUT_SOCKET_OFFSET.y,
-        };
+
+        switch (this.orientation) {
+          case SchematicOrientation.Horizontal: {
+            return {
+              x: sourceNodePosition.x + OUTPUT_SOCKET_OFFSET_HORIZONTAL.x,
+              y: sourceNodePosition.y + OUTPUT_SOCKET_OFFSET_HORIZONTAL.y,
+            };
+          }
+
+          case SchematicOrientation.Vertical: {
+            return {
+              x: sourceNodePosition.x + OUTPUT_SOCKET_OFFSET_VERTICAL.x,
+              y: sourceNodePosition.y + OUTPUT_SOCKET_OFFSET_VERTICAL.y,
+            };
+          }
+
+          default: {
+            return undefined;
+          }
+        }
       } else {
         return undefined;
       }
     },
-
     destinationSocketPosition(): Cg2dCoordinate | undefined {
       const context = this.positionCtx;
       if (
@@ -147,10 +212,39 @@ export default Vue.extend({
           x: Number(this.destinationNode.node.positions[context].x),
           y: Number(this.destinationNode.node.positions[context].y),
         };
-        return {
-          x: destinationNodePosition.x + INPUT_SOCKET_OFFSET.x,
-          y: destinationNodePosition.y + INPUT_SOCKET_OFFSET.y,
-        };
+
+        switch (this.orientation) {
+          case SchematicOrientation.Horizontal: {
+            const socketId = this.edge.headVertex.socket;
+            const index = _.findIndex(this.inputs(), function(o) {
+              return o.name == socketId;
+            });
+
+            const positionalOffset = {
+              x: INPUT_SOCKET_ALIGNMENT_OFSET.x,
+              y: INPUT_SOCKET_ALIGNMENT_OFSET.y * index,
+            };
+            return {
+              x:
+                destinationNodePosition.x +
+                (INPUT_SOCKET_OFFSET_HORIZONTAL.x + positionalOffset.x),
+              y:
+                destinationNodePosition.y +
+                (INPUT_SOCKET_OFFSET_HORIZONTAL.y + positionalOffset.y),
+            };
+          }
+
+          case SchematicOrientation.Vertical: {
+            return {
+              x: destinationNodePosition.x + INPUT_SOCKET_OFFSET_VERTICAL.x,
+              y: destinationNodePosition.y + INPUT_SOCKET_OFFSET_VERTICAL.y,
+            };
+          }
+
+          default: {
+            return undefined;
+          }
+        }
       } else {
         return undefined;
       }
@@ -161,9 +255,6 @@ export default Vue.extend({
   },
   beforeDestroy() {
     this.deRegisterEvents();
-  },
-  beforeUpdate: function() {
-    this.updated++;
   },
   methods: {
     registerEvents(): void {
@@ -215,27 +306,84 @@ export default Vue.extend({
       ) {
         const element = document.getElementById(this.lineId) as HTMLElement;
 
-        if (element) {
-          if (event.nodeId == this.sourceNode.node.id) {
-            element.setAttribute(
-              "x1",
-              String(event.nodePosition.x + OUTPUT_SOCKET_OFFSET.x),
-            );
-            element.setAttribute(
-              "y1",
-              String(event.nodePosition.y + OUTPUT_SOCKET_OFFSET.y),
-            );
-          } else if (event.nodeId == this.destinationNode.node.id) {
-            element.setAttribute(
-              "x2",
-              String(event.nodePosition.x + INPUT_SOCKET_OFFSET.x),
-            );
-            element.setAttribute(
-              "y2",
-              String(event.nodePosition.y + INPUT_SOCKET_OFFSET.y),
-            );
+        /* This sections relies on duplicated code from:
+         * - sourceSocketPosition() and destinationSocketPosition().
+         * It should be refactored...
+         */
+        if (element && event.nodeId) {
+          switch (this.orientation) {
+            case SchematicOrientation.Vertical: {
+              console.log("updateSvgLinePosition");
+
+              if (event.nodeId == this.sourceNode.node.id) {
+                const sourceSocketPosition: Cg2dCoordinate = {
+                  x: event.nodePosition.x + OUTPUT_SOCKET_OFFSET_VERTICAL.x,
+                  y: event.nodePosition.y + OUTPUT_SOCKET_OFFSET_VERTICAL.y,
+                };
+
+                console.log(sourceSocketPosition);
+
+                element.setAttribute("x1", String(sourceSocketPosition.x));
+                element.setAttribute("y1", String(sourceSocketPosition.y));
+              } else if (event.nodeId == this.destinationNode.node.id) {
+                const destinationSocketPosition: Cg2dCoordinate = {
+                  x: event.nodePosition.x + INPUT_SOCKET_OFFSET_VERTICAL.x,
+                  y: event.nodePosition.y + INPUT_SOCKET_OFFSET_VERTICAL.y,
+                };
+
+                console.log(destinationSocketPosition);
+
+                element.setAttribute("x2", String(destinationSocketPosition.x));
+                element.setAttribute("y2", String(destinationSocketPosition.y));
+              }
+              break;
+            }
+
+            case SchematicOrientation.Horizontal: {
+              const socketId = this.edge.headVertex.socket;
+              const index = _.findIndex(this.inputs(), function(o) {
+                return o.name == socketId;
+              });
+
+              const positionalOffset = {
+                x: INPUT_SOCKET_ALIGNMENT_OFSET.x,
+                y: INPUT_SOCKET_ALIGNMENT_OFSET.y * index,
+              };
+
+              if (event.nodeId == this.sourceNode.node.id) {
+                const sourceSocketPosition: Cg2dCoordinate = {
+                  x: event.nodePosition.x + OUTPUT_SOCKET_OFFSET_HORIZONTAL.x,
+                  y: event.nodePosition.y + OUTPUT_SOCKET_OFFSET_HORIZONTAL.y,
+                };
+                element.setAttribute("x1", String(sourceSocketPosition.x));
+                element.setAttribute("y1", String(sourceSocketPosition.y));
+              } else if (event.nodeId == this.destinationNode.node.id) {
+                const destinationSocketPosition: Cg2dCoordinate = {
+                  x:
+                    event.nodePosition.x +
+                    (INPUT_SOCKET_OFFSET_HORIZONTAL.x + positionalOffset.x),
+                  y:
+                    event.nodePosition.y +
+                    (INPUT_SOCKET_OFFSET_HORIZONTAL.y + positionalOffset.y),
+                };
+                element.setAttribute("x2", String(destinationSocketPosition.x));
+                element.setAttribute("y2", String(destinationSocketPosition.y));
+              }
+              break;
+            }
           }
         }
+
+        // if (element) {
+        //   if (event.nodeId == this.sourceNode.node.id) {
+        //     element.setAttribute("x1", String(event.nodePosition.x + OUTPUT_SOCKET_OFFSET_HORIZONTAL.x));
+        //     element.setAttribute("y1", String(event.nodePosition.y + OUTPUT_SOCKET_OFFSET_HORIZONTAL.y));
+
+        //   } else if (event.nodeId == this.destinationNode.node.id) {
+        //     element.setAttribute("x2", String(event.nodePosition.x + INPUT_SOCKET_OFFSET_HORIZONTAL.x));
+        //     element.setAttribute("y2", String(event.nodePosition.y + INPUT_SOCKET_OFFSET_HORIZONTAL.y));
+        //   }
+        // }
       }
     },
     getNode(nodeId: string): ISchematicNode | undefined {
@@ -244,6 +392,17 @@ export default Vue.extend({
           return this.schematic.nodes[nodeId];
         }
       }
+    },
+    inputs(): RegistryEntry["inputs"] {
+      const sourceNode = this.schematic.nodes[
+        this.edge.headVertex.nodeId
+      ] as ISchematicNode;
+
+      const sourceNodeEntity = SiEntity.fromJson(sourceNode.object as Entity);
+
+      let inputs = sourceNodeEntity.schema().inputs;
+
+      return inputs;
     },
   },
 });
