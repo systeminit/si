d24ea7b2ab8b79b7c5c9df3959f66a2a807b66a6
Alex Ethier <alex@systeminit.com>
2021-05-26 20:47:15 -0700

    feat(si-web-app): improve panel tree and resource viewer
    Improve panel tree layout:
     - right side panels default to attribute viewer.
     - startup left side panels to right side panels ratio is adjusted.
    
    Improve resource viewer:
     - Implement code mirror
     - update sync icon

---
+++ components/si-web-app/src/organisims/ResourceViewer/ResourceVisualization.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col w-full h-full overflow-hidden">
+    <div class="flex flex-col w-full h-full overflow-auto overscroll-none">
+      <CodeMirror
+        class="w-full h-ful"
+        :value="jsonString"
+        readOnly
+        mode="json"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { Resource } from "@/api/sdf/model/resource";
+import CodeMirror from "@/molecules/CodeMirror.vue";
+
+export default Vue.extend({
+  name: "ResourceVisualization",
+  components: {
+    CodeMirror,
+  },
+  props: {
+    resource: {
+      type: Object as PropType<Resource>,
+    },
+  },
+  computed: {
+    jsonString(): string {
+      return JSON.stringify(this.resource, null, "\t");
+    },
+    jsonMode(): Object {
+      return {
+        name: "javascript",
+        json: "true",
+      };
+    },
+  },
+});
+</script>
