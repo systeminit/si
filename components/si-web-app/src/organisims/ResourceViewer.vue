dd28fc4882a6bc85e7c296f244d89a6d819b3b2c
Alex Ethier <alex@systeminit.com>
2021-05-27 23:52:14 -0700

    feat(si-web-app): improve resource viewer
    Improve resource viewer
     - Implement basic resource card.
     - Animate sync button.
    
    and change code mirror font size.
    
    [ch1355]

--- components/si-web-app/src/organisims/ResourceViewer.vue
+++ components/si-web-app/src/organisims/ResourceViewer.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="flex flex-col w-full" v-if="entity">
     <div
-      class="relative flex flex-row items-center pt-2 pb-2 pl-6 pr-6 text-white property-section-bg-color"
+      class="relative flex flex-row items-center justify-between pt-2 pb-2 pl-6 pr-6 text-white property-section-bg-color"
     >
       <div class="text-lg">
         {{ entity.entityType }} {{ entity.name }} resource
       </div>
-    </div>
 
-    <div
-      class="relative flex flex-row items-center pt-2 pb-2 pl-6 pr-6 text-base bg-black"
-    >
-      <div class="flex">
+      <div class="flex pl-1">
         <button
-          class="flex items-center pl-1 focus:outline-none"
+          class="flex items-center focus:outline-none button"
+          ref="sync"
           v-show="!editMode"
           @click="runSync()"
         >
-          <RefreshCcwIcon size="1x" class="text-sm button" />
+          <RefreshCcwIcon size="1x" class="text-sm sync-button-invert" />
         </button>
       </div>
     </div>
 
-    <div class="flex w-full h-full pt-2 overflow-scroll">
-      <div class="flex w-full h-full text-xs" v-if="resource">
-        <ResourceVisualization :resource="resource" />
+    <div class="flex flex-row">
+      <div class="w-full h-full pt-2">
+        <div class="flex flex-col w-full h-full" v-if="resource">
+          <ResourceVisualization :resource="resource" />
+        </div>
       </div>
     </div>
   </div>
@@ -61,7 +60,6 @@ export default Vue.extend({
     };
   },
   components: {
-    // VueJsonPretty,
     RefreshCcwIcon,
     ResourceVisualization,
   },
@@ -74,9 +72,9 @@ export default Vue.extend({
   },
   methods: {
     async runSync(): Promise<void> {
+      this.animateSyncButton();
       // @ts-ignore
       if (this.system && this.workspace) {
-        this.isSynchronizing = true;
         let reply = await ResourceDal.syncResource({
           entityId: this.entity.id,
           // @ts-ignore
@@ -84,10 +82,21 @@ export default Vue.extend({
           // @ts-ignore
           workspaceId: this.workspace.id,
         });
-        this.isSynchronizing = false;
         if (reply.error) {
           emitEditorErrorMessage(reply.error.message);
         }
+      }
+    },
+    animateSyncButton() {
+      const button = this.$refs.sync as HTMLElement;
+      if (button) {
+        button.animate(
+          [{ transform: "rotate(0deg)" }, { transform: "rotate(180deg)" }],
+          {
+            duration: 225,
+            easing: "ease-out",
+          },
+        );
       }
     },
   },
@@ -121,6 +130,9 @@ $button-brightness: 1.1;
 
 .button:active {
   filter: saturate(1.5) brightness($button-brightness);
-  @apply animate-ping;
+}
+
+.sync-button-invert {
+  transform: scaleX(-1);
 }
 </style>
--- components/si-web-app/src/organisims/ResourceViewer/ResourceVisualization.vue
+++ components/si-web-app/src/organisims/ResourceViewer/ResourceVisualization.vue
@@ -1,30 +1,99 @@
 <template>
-  <div class="flex flex-col w-full h-full overflow-hidden">
-    <div class="flex flex-col w-full h-full overflow-auto overscroll-none">
-      <CodeMirror
-        class="w-full h-ful"
-        :value="jsonString"
-        readOnly
-        mode="json"
-      />
+  <div class="flex flex-col w-full h-full">
+    <div class="flex flex-col mx-4">
+      <div class="flex flex-row py-1 card-title-bar card">
+        <div class="flex flex-col flex-grow">
+          <div class="flex flex-row mx-2 my-1">
+            <div class="text-xs">
+              <HeartIcon size="1.25x" :class="healthColor" />
+            </div>
+
+            <div class="ml-2 text-xs ">
+              {{ resource.timestamp }}
+            </div>
+          </div>
+
+          <div v-show="resource.error" class="flex-grow w-full card ">
+            <div
+              class="px-2 py-2 mx-2 my-2 text-xs whitespace-pre-wrap border content-error card"
+            >
+              {{ resource.error }}
+            </div>
+          </div>
+        </div>
+
+        <button
+          @click="toggleData()"
+          class="mr-2 focus:outline-none"
+          v-show="Object.keys(resource.data).length > 0"
+        >
+          <ChevronDownIcon
+            v-if="isDataExpanded"
+            size="1.1x"
+            class="text-gray-300 "
+          />
+          <ChevronRightIcon size="1.1x" v-else class="text-gray-300 " />
+        </button>
+      </div>
+
+      <div
+        class="flex flex-col justify-start h-full card"
+        v-if="isDataExpanded"
+      >
+        <!-- <CodeMirror
+              class="max-w-md"
+              :value="jsonString"
+              readOnly
+              mode="json"
+            /> -->
+
+        <div
+          v-show="Object.keys(resource.data).length > 0"
+          class="w-full overflow-auto"
+        >
+          <div class="mx-4 my-4 json-code">
+            <VueJsonPretty :data="resource.data" :showDoubleQuotes="false" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
-import { Resource } from "@/api/sdf/model/resource";
-import CodeMirror from "@/molecules/CodeMirror.vue";
+import { Resource, ResourceHealth } from "@/api/sdf/model/resource";
+// import CodeMirror from "@/molecules/CodeMirror.vue";
+import VueJsonPretty from "vue-json-pretty";
+
+import {
+  HeartIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+} from "vue-feather-icons";
+
+interface IData {
+  isDataExpanded: boolean;
+}
 
 export default Vue.extend({
   name: "ResourceVisualization",
   components: {
-    CodeMirror,
+    // CodeMirror,
+    VueJsonPretty,
+    HeartIcon,
+    ChevronRightIcon,
+    ChevronDownIcon,
   },
   props: {
     resource: {
       type: Object as PropType<Resource>,
     },
+  },
+  data(): IData {
+    return {
+      isDataExpanded: false,
+    };
   },
   computed: {
     jsonString(): string {
@@ -36,6 +105,69 @@ export default Vue.extend({
         json: "true",
       };
     },
+    healthColor(): Record<string, any> {
+      let style: Record<string, any> = {};
+      if (this.resource.health == ResourceHealth.Ok) {
+        style["health-ok"] = true;
+      } else if (this.resource.health == ResourceHealth.Warning) {
+        style["health-warning"] = true;
+      } else if (this.resource.health == ResourceHealth.Error) {
+        style["health-error"] = true;
+      } else if (this.resource.health == ResourceHealth.Unknown) {
+        style["health-unknown"] = true;
+      } else {
+        style["health-unknown"] = true;
+      }
+      return style;
+    },
+  },
+  methods: {
+    toggleData() {
+      this.isDataExpanded = !this.isDataExpanded;
+    },
+    whenCodeMirrorIsVisible(): Record<string, any> {
+      let style: Record<string, any> = {};
+      if (this.isDataExpanded) {
+        style["h-full"] = true;
+      }
+      return style;
+    },
   },
 });
 </script>
+
+<style scoped>
+.card {
+  background-color: #1f2122;
+}
+
+.card-title-bar {
+  color: #cccdb1;
+}
+
+.content-error {
+  color: #f08686;
+  border-color: #f08686;
+}
+
+.health-ok {
+  color: #86f0ad;
+  @apply animate-pulse;
+}
+
+.health-warning {
+  color: #f0d286;
+}
+
+.health-error {
+  color: #f08686;
+}
+
+.health-unknown {
+  color: #bbbbbb;
+}
+
+.json-code {
+  width: 24rem;
+}
+</style>
