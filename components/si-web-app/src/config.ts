4d852fc52855c189da51c455cf09997a321e2d6b
Fletcher Nichol <fnichol@nichol.ca>
2021-07-30 10:19:57 -0600

    chore: rebuild Docker images for key services
    This is the first iteration of a rebuild of Docker images for the
    following services:
    
    - si-sdf
    - si-veritech
    - si-web-app
    
    They are not fully complete but with a couple of caveats, are functional
    together. Crucially, the demo flow works aside from the dockerImage
    qualification check as mentioned below.
    
    Currently, they are built to work with Docker Compose, with an example
    in `support/dev`. To build all the images, you can:
    
    ```sh
    cd support/dev
    ./demo-build.sh
    ```
    
    which invokes `docker-compose` in parallel build mode. Some Honeycomb
    credentials are required and the script should fail if they aren't found
    in the current shell environment.
    
    To launch the setup run: `docker-compose up` in the `support/dev`
    directory.
    
    All the relevant ports are exposed out to the host system, meaning that
    all the same local ports are served up--this time via the container
    services. So you can, in theory develop on `si-web-app` as normal and
    have the rest of the stack running with Docker Compose
    
    Caveats
    -------
    
    Currently there are some known issues to address:
    
    - Which Veritech's image has an installation of Docker, it's not yet
      wired and capable of using the outer engine for dockerImage
      qualifications
    - SDF is currently using a local PostgreSQL container and needs to have
      Aurora credentials passed through to the SDF container on start
    - The si-web-app container needs a slightly customized nginx
      configuration to use the `try_file` directive to help with some client
      refreshing/routing back to `/index.html`
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- components/si-web-app/src/config.ts
+++ components/si-web-app/src/config.ts
@@ -7,15 +7,15 @@ export interface Config {
 export const config: Config = {
   routerBase: process.env.BASE_URL,
   sdfBaseUrl: new URL(
-    process.env.VUE_APP_SDF_BASE_URL ||
+    process.env.VUE_APP_SDF_BASE_HTTP_URL ||
       (process.env.NODE_ENV == "production"
-        ? "https://api.systeminit.com/api"
+        ? "https://app.systeminit.com/api"
         : "http://localhost:5156"),
   ),
   sdfBaseWsUrl: new URL(
     process.env.VUE_APP_SDF_BASE_WS_URL ||
       (process.env.NODE_ENV == "production"
-        ? "ws://api.systeminit.com/api/updates"
+        ? "wss://app.systeminit.com/api/updates"
         : "ws://localhost:5156/updates"),
   ),
 };
---
+++ support/dev/demo-build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+# shellcheck shell=sh disable=SC2039
+
+main() {
+  set -eu
+  if [ -n "${DEBUG:-}" ]; then set -v; fi
+  if [ -n "${TRACE:-}" ]; then set -xv; fi
+
+  if [ -z "${HONEYCOMB_TOKEN:-}" ]; then
+    die "HONEYCOMB_TOKEN must be set"
+  fi
+  if [ -z "${HONEYCOMB_DATASET:-}" ]; then
+    die "HONEYCOMB_DATASET must be set"
+  fi
+
+  set -x
+  env \
+    DOCKER_BUILDKIT=1 \
+    BUILDKIT_PROGRESS=plain \
+    COMPOSE_DOCKER_CLI_BUILD=1 \
+    docker-compose build --parallel
+}
+
+die() {
+  echo "" >&2
+  echo "xxx $1" >&2
+  echo "" >&2
+  exit 1
+}
+
+main "$@" || exit 1
