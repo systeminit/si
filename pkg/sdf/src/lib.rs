2ced38189d204e551019151ccc34298e64f0d303
Fletcher Nichol <fnichol@nichol.ca>
2021-10-27 00:49:58 -0600

    chore(sdf): implement primordial server/service
    There aren't any routes yet, but the basic structure is there. Future
    work will add to this and extract common behavior that is currently
    shared via copy pasta with cyclone.
    
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- pkg/sdf/src/lib.rs
+++ pkg/sdf/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#![warn(
+    clippy::unwrap_in_result,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::missing_panics_doc,
+    clippy::panic_in_result_fn
+)]
+#![allow(
+    clippy::missing_errors_doc,
+    clippy::module_inception,
+    clippy::module_name_repetitions
+)]
+
+mod server;
+pub use server::{Config, ConfigError, IncomingStream, Server};
