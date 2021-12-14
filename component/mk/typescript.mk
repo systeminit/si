b60c06ddf20fc97620524cdbe72ffe81c313fe3c
Fletcher Nichol <fnichol@nichol.ca>
2021-12-14 16:18:07 -0700

    chore(ci,web): add back build and release for web
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ component/mk/typescript.mk
@@ -0,0 +1,34 @@
+node_modules: package.json ## Installs the package's dependencies
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	npm install
+.PHONY: node_modules
+
+build: node_modules ## Builds the TypeScript package
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	env NODE_ENV=production npm run build
+.PHONY: build
+
+test: node_modules ## Tests the TypeScript package
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	npm run test
+.PHONY: test
+
+run: start ## Alias for `make start`
+.PHONY: run
+
+start: node_modules ## Runs a dev server for the package/app
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	npm run start
+.PHONY: start
+
+watch: start ## Alias for `make start`
+.PHONY: watch
+
+clean: ## Cleans all build/test temporary work files
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	rm -rf ./node_modules ./dist ./lib
+
+lint: node_modules ## Runs code/style linting
+	@echo "--- [$(shell basename ${CURDIR})] $@"
+	npm run eslint
+.PHONY: lint
