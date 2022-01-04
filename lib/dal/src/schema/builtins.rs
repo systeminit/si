f09386dcd87289e8be56698360982278d5539428
Nick Gerace <nick@systeminit.com>
2022-01-04 12:38:24 -0500

    feat(sdf,dal): add service schema and impl (#588)
    - Add schema for service component [sc-1939]
    - Add implementation for service component (k8s-based) [sc-1940]
    - Fix list_schemas test to be dynamic based on what schemas exist upon
      migration
    - Fix local development Make targets for sdf and web
    - Update README with new local development targets
    - Add shortcut integration to README
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

--- lib/dal/src/schema/builtins.rs
+++ lib/dal/src/schema/builtins.rs
@@ -4,13 +4,14 @@ use si_data::{NatsTxn, PgTxn};
 
 pub async fn migrate(txn: &PgTxn<'_>, nats: &NatsTxn) -> SchemaResult<()> {
     application(txn, nats).await?;
+    service(txn, nats).await?;
+
+    kubernetes_service(txn, nats).await?;
     Ok(())
 }
 
 async fn application(txn: &PgTxn<'_>, nats: &NatsTxn) -> SchemaResult<()> {
-    let tenancy = Tenancy::new_universal();
-    let visibility = Visibility::new_head(false);
-    let history_actor = HistoryActor::SystemInit;
+    let (tenancy, visibility, history_actor) = default_migration_config();
     let mut schema = Schema::new(
         txn,
         nats,
@@ -33,4 +34,64 @@ async fn application(txn: &PgTxn<'_>, nats: &NatsTxn) -> SchemaResult<()> {
         .await?;
 
     Ok(())
+}
+
+async fn kubernetes_service(txn: &PgTxn<'_>, nats: &NatsTxn) -> SchemaResult<()> {
+    let (tenancy, visibility, history_actor) = default_migration_config();
+    let mut schema = Schema::new(
+        txn,
+        nats,
+        &tenancy,
+        &visibility,
+        &history_actor,
+        "kubernetes_service",
+        &SchemaKind::Implementation,
+    )
+    .await?;
+
+    let variant =
+        SchemaVariant::new(txn, nats, &tenancy, &visibility, &history_actor, "v0").await?;
+    variant
+        .set_schema(txn, nats, &visibility, &history_actor, schema.id())
+        .await?;
+
+    schema
+        .set_default_schema_variant_id(txn, nats, &visibility, &history_actor, Some(*variant.id()))
+        .await?;
+
+    Ok(())
+}
+
+async fn service(txn: &PgTxn<'_>, nats: &NatsTxn) -> SchemaResult<()> {
+    let (tenancy, visibility, history_actor) = default_migration_config();
+    let mut schema = Schema::new(
+        txn,
+        nats,
+        &tenancy,
+        &visibility,
+        &history_actor,
+        "service",
+        &SchemaKind::Concept,
+    )
+    .await?;
+
+    let variant =
+        SchemaVariant::new(txn, nats, &tenancy, &visibility, &history_actor, "v0").await?;
+    variant
+        .set_schema(txn, nats, &visibility, &history_actor, schema.id())
+        .await?;
+
+    schema
+        .set_default_schema_variant_id(txn, nats, &visibility, &history_actor, Some(*variant.id()))
+        .await?;
+
+    Ok(())
+}
+
+fn default_migration_config() -> (Tenancy, Visibility, HistoryActor) {
+    (
+        Tenancy::new_universal(),
+        Visibility::new_head(false),
+        HistoryActor::SystemInit,
+    )
 }
