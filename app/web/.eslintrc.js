f76d06e447ed3019afa11df181d635685131bbbd
Alex Ethier <alex@systeminit.com>
2022-01-13 09:20:27 -0800

    build(webapp): improve type and linting checks
     - Add typescript checks to eslint config file
     - Update eslint configuration
     - Update tsc configuration

--- app/web/.eslintrc.js
+++ app/web/.eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     node: true,
   },
   globals: { defineProps: "readonly", defineEmits: "readonly" },
+  plugins: ["vue", "@typescript-eslint"],
   extends: [
-    "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "@vue/typescript",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
     "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
   ],
   rules: {
     camelcase: "off",
@@ -23,7 +25,10 @@ module.exports = {
         varsIgnorePattern: "^_",
       },
     ],
+    "vue/script-setup-uses-vars": "error",
+    "@typescript-eslint/ban-ts-comment": "off",
   },
+  parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
   },
