ce08fb4091b4b11c91730011adb453a52fbd3dd6
Nick Gerace <nick@systeminit.com>
2022-02-18 14:56:43 -0500

    feat(web): add public key use for true encryption
    - Add get public key SDF route using DAL PublicKey
    - Add get public key blocking service in frontend
    - Use get public key within secret creation function
    - Add doc comments for discoveries found during development
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>
    Co-authored-by: Fletcher Nichol <fletcher@systeminit.com>

---
+++ app/web/src/service/secret/get_public_key.ts
@@ -0,0 +1,20 @@
+import { ApiResponse, SDF } from "@/api/sdf";
+import { lastValueFrom } from "rxjs";
+import Bottle from "bottlejs";
+import { PublicKey } from "@/api/sdf/dal/key_pair";
+
+// The response _is_ the "PublicKey" from the DAL. You may need to decode the actual public key via "Base64.toUint8Array"
+// or equivalent.
+export type GetPublicKeyResponse = PublicKey;
+
+// This is a blocking call, unlike most service calls. It does not return an observable.
+export async function getPublicKeyRaw(): Promise<
+  ApiResponse<GetPublicKeyResponse>
+> {
+  const bottle = Bottle.pop("default");
+  const sdf: SDF = bottle.container.SDF;
+  const reply$ = sdf.get<ApiResponse<GetPublicKeyResponse>>(
+    "secret/get_public_key",
+  );
+  return await lastValueFrom(reply$);
+}
