b3d25bf2dfe486b566d6da64144aa8b208d1c4a4
Fletcher Nichol <fletcher@systeminit.com>
2022-02-17 17:48:06 -0700

    fix(web): fix 3 type errors related to edit form
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- app/web/src/organisims/EditForm/SelectWidget.vue
+++ app/web/src/organisims/EditForm/SelectWidget.vue
@@ -17,7 +17,7 @@
         @blur="onBlur"
       >
         <option
-          v-for="option in widget.options.options"
+          v-for="option in widget.options"
           :key="String(option.value)"
           :value="option.value"
         >
