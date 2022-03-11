0ea5c25e97d438560bcbf9d0144be7475b5c8085
Jacob Helwig <jacob@systeminit.com>
2022-03-11 10:09:45 -0800

    feat(dal): Add deculture::IndexMap
    This version of `IndexMap` works with `AttributeValueId`, instead of
    with `AttributeResolverId` to fit into the new schema.

--- lib/dal/src/deculture.rs
+++ lib/dal/src/deculture.rs
@@ -1,6 +1,8 @@
 pub mod attribute;
+pub mod index_map;
 pub mod input_socket;
 
 pub use attribute::context::{
     AttributeContext, AttributeContextBuilderError, AttributeContextError, AttributeReadContext,
 };
+pub use index_map::IndexMap;
