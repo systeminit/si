d1355d3ba510516d7a30e2b8b9075a4329ca6354
Victor Bustamante <victor@systeminit.com>
2022-07-18 20:49:03 -0300

    feat(web): Add node skeleton SVG component and sidebar content raw structure
    Signed-off-by: Victor Bustamante <victor@systeminit.com>

---
+++ app/web/src/organisims/AssetPalette.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="p-2">
+    <input placeholder="search" />
+
+    <p>
+      Get Started by dragging the assets that you wish to include in your
+      application into the canvas to the right
+    </p>
+
+    <ul>
+      <Disclosure v-slot="{ open }" as="li" class="mt-2">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        >
+          <span
+            :class="open ? 'rotate-180 transform' : ''"
+            class="text-purple-500"
+          >
+            v
+          </span>
+          <span>Type 1</span>
+        </DisclosureButton>
+        <DisclosurePanel as="ul" class="px-4 pt-4 pb-2 text-sm text-gray-500">
+          <SiListItem />
+          <SiListItem />
+          <SiListItem />
+        </DisclosurePanel>
+      </Disclosure>
+      <Disclosure v-slot="{ open }" as="li" class="mt-2">
+        <DisclosureButton
+          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        >
+          <span
+            :class="open ? 'rotate-180 transform' : ''"
+            class="text-purple-500"
+          >
+            v
+          </span>
+          <span>Type 2</span>
+        </DisclosureButton>
+        <DisclosurePanel as="ul" class="px-4 pt-4 pb-2 text-sm text-gray-500">
+          <SiListItem />
+          <SiListItem />
+          <SiListItem />
+        </DisclosurePanel>
+      </Disclosure>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SiListItem from "@/molecules/SiListItem.vue";
+import { Disclosure, DisclosurePanel, DisclosureButton } from "@headlessui/vue";
+</script>
---
+++ app/web/src/organisims/SiChangesetForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full border-gray-300 border-b-2 p-2">
+    <SiTextBox2 title="Change Set" class="w-3/4" />
+    <button class="w-1/4">Apply</button>
+    <button>Commit</button>
+    <button>Discard</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SiTextBox2 from "@/atoms/SiTextBox2.vue";
+</script>
