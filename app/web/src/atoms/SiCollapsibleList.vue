c4f0afaee7c2f52c4f3195f95c62bbe4156727f7
Victor Bustamante <victor@systeminit.com>
2022-07-19 16:54:34 -0300

    feat(web): Add full skeleton for left sidebar of the workspace view
    Signed-off-by: Victor Bustamante <victor@systeminit.com>

---
+++ app/web/src/atoms/SiCollapsibleList.vue
@@ -0,0 +1,24 @@
+<template>
+  <Disclosure v-slot="{ open }" :as="as" class="mt-2">
+    <DisclosureButton
+      class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+    >
+      <span :class="open ? 'rotate-180 transform' : ''" class="text-purple-500">
+        v
+      </span>
+      <span>{{ label }}</span>
+    </DisclosureButton>
+    <DisclosurePanel as="ul" class="px-4 pt-4 pb-2 text-sm text-gray-500">
+      <slot />
+    </DisclosurePanel>
+  </Disclosure>
+</template>
+
+<script setup lang="ts">
+import { Disclosure, DisclosurePanel, DisclosureButton } from "@headlessui/vue";
+
+defineProps<{
+  label: string;
+  as?: string;
+}>();
+</script>
--- app/web/src/organisims/AssetPalette.vue
+++ app/web/src/organisims/AssetPalette.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-2">
-    <input placeholder="search" />
+    <SiSearch />
 
     <p>
       Get Started by dragging the assets that you wish to include in your
@@ -8,47 +8,41 @@
     </p>
 
     <ul>
-      <Disclosure v-slot="{ open }" as="li" class="mt-2">
-        <DisclosureButton
-          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-        >
-          <span
-            :class="open ? 'rotate-180 transform' : ''"
-            class="text-purple-500"
-          >
-            v
-          </span>
-          <span>Type 1</span>
-        </DisclosureButton>
-        <DisclosurePanel as="ul" class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          <SiListItem />
-          <SiListItem />
-          <SiListItem />
-        </DisclosurePanel>
-      </Disclosure>
-      <Disclosure v-slot="{ open }" as="li" class="mt-2">
-        <DisclosureButton
-          class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
-        >
-          <span
-            :class="open ? 'rotate-180 transform' : ''"
-            class="text-purple-500"
-          >
-            v
-          </span>
-          <span>Type 2</span>
-        </DisclosureButton>
-        <DisclosurePanel as="ul" class="px-4 pt-4 pb-2 text-sm text-gray-500">
-          <SiListItem />
-          <SiListItem />
-          <SiListItem />
-        </DisclosurePanel>
-      </Disclosure>
+      <SiCollapsibleList
+        v-for="(category, index) in assetCategories"
+        :key="index"
+        as="li"
+        :label="category.name"
+      >
+        <SiListItem v-for="i in 5" :key="i" />
+      </SiCollapsibleList>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import SiListItem from "@/molecules/SiListItem.vue";
-import { Disclosure, DisclosurePanel, DisclosureButton } from "@headlessui/vue";
+import SiListItem from "@/molecules/SiAssetListItem.vue";
+import SiCollapsibleList from "@/atoms/SiCollapsibleList.vue";
+import SiSearch from "@/molecules/SiSearch.vue";
+
+const assetCategories = [
+  {
+    name: "Kubernetes",
+    color: "text-red",
+    assets: [],
+  },
+  {
+    name: "Blip",
+    color: "text-blue",
+    assets: [],
+  },
+  {
+    name: "Something with a really really long name oh no oh no",
+    color: "text-purple",
+    assets: [],
+  },
+];
+// NodeSprite
+// NodeSprite normal hover focus disabled
+// NodeCategory
 </script>
