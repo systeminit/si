f61100f74b8ba4724e56470834daf6305019a8dd
Zachary Hamm <zack@systeminit.com>
2022-08-04 19:20:42 -0500

    fix(web): pass in memo, default to refCount = false

--- app/web/src/service/func/get_func.ts
+++ app/web/src/service/func/get_func.ts
@@ -1,9 +1,9 @@
-import { ApiResponse, SDF } from "@/api/sdf";
-import Bottle from "bottlejs";
-import { combineLatest, Observable } from "rxjs";
-import { switchMap, share } from "rxjs/operators";
-import { standardVisibilityTriggers$ } from "@/observable/visibility";
+import { ApiResponse } from "@/api/sdf";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { Func, FuncBackendKind } from "@/api/sdf/dal/func";
+import { GlobalErrorService } from "@/service/global_error";
+import { memoizedVisibilitySdfPipe } from "@/utils/memoizedVisibilitySdfPipes";
 
 export interface GetFuncArgs {
   id: number;
@@ -11,20 +11,30 @@ export interface GetFuncArgs {
 
 export type GetFuncResponse = Func;
 
-export function getFunc(
-  args: GetFuncArgs,
-): Observable<ApiResponse<GetFuncResponse>> {
-  return combineLatest([standardVisibilityTriggers$]).pipe(
-    switchMap(([[visibility]]) => {
-      const sdf: SDF = Bottle.pop("default").container.SDF;
-      return sdf.get<ApiResponse<GetFuncResponse>>("func/get_func", {
-        ...args,
-        ...visibility,
-      });
-    }),
-    share(),
+const memo: {
+  [key: string]: Observable<GetFuncResponse>;
+} = {};
+
+export const getFunc: (args: GetFuncArgs) => Observable<GetFuncResponse> =
+  memoizedVisibilitySdfPipe(
+    (visibility, sdf, args) =>
+      sdf
+        .get<ApiResponse<GetFuncResponse>>("func/get_func", {
+          ...args,
+          ...visibility,
+        })
+        .pipe(
+          map((response) => {
+            if (response.error) {
+              GlobalErrorService.set(response);
+              return nullFunc;
+            }
+
+            return response as GetFuncResponse;
+          }),
+        ),
+    memo,
   );
-}
 
 export const nullFunc: GetFuncResponse = {
   id: 0,
