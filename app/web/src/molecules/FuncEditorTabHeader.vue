777f08fbddedbb2a81d182a21944593fa5874fa3
Zachary Hamm <zack@systeminit.com>
2022-08-11 12:59:41 -0500

    chore(web): Delete unused FuncEditorTabHeader.vue

--- app/web/src/molecules/FuncEditorTabHeader.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <SiTabHeader v-for="(funcId, index) in funcList" :key="funcId">
-    {{ editingFuncs[index].origFunc.name }}
-    <button class="inline-block rounded-sm" @click="closeFunc(funcId)">
-      <VueFeather type="x-circle" />
-    </button>
-  </SiTabHeader>
-</template>
