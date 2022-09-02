cb4e3a949f618f560e681df36cdece2fce7e6933
wendybujalski <wendydbujalski@gmail.com>
2022-09-02 12:34:26 -0400

    feat(web) - Timestamps of various sizes

--- app/web/src/ui-lib/Timestamp.vue
+++ app/web/src/ui-lib/Timestamp.vue
@@ -4,7 +4,9 @@
 
 <script lang="ts" setup>
 import { computed, PropType } from "vue";
-import { format, formatDistanceToNowStrict } from "date-fns";
+import { format, formatDistanceToNow } from "date-fns";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
 export type TimestampSize = "mini" | "normal" | "extended";
 
@@ -17,21 +19,24 @@ const props = defineProps({
   },
 });
 
+TimeAgo.addLocale(en);
+const timeAgo = new TimeAgo("en-US");
+
 const dateStr = computed(() => {
   const d = props.date;
   if (props.size === "mini") {
     if (props.relative) {
-      return formatDistanceToNowStrict(d);
+      return timeAgo.format(d, "mini-minute-now");
     }
     return format(d, "M/d/y");
   } else if (props.size === "extended") {
     if (props.relative) {
-      return formatDistanceToNowStrict(d);
+      return `${formatDistanceToNow(d)} ago`;
     }
     return `${format(d, "eeee MMMM do, y")} at ${format(d, "h:mm:ss a")}`;
   }
   if (props.relative) {
-    return formatDistanceToNowStrict(d);
+    return timeAgo.format(d);
   }
   return format(d, "MMMM d, y");
 });
