56493d761f5878432e9f2a90d7153ffe275f389d
wendybujalski <wendy@systeminit.com>
2022-10-04 18:51:01 -0400

    feat(web) - WorkspaceFix left panel

---
+++ app/web/src/molecules/FixSprite.vue
@@ -0,0 +1,47 @@
+<template>
+  <SiCollapsible
+    as="li"
+    class="w-full"
+    content-as="ul"
+    :default-open="false"
+    hide-bottom-border
+  >
+    <template #label>
+      <div
+        class="flex flex-row items-center gap-2.5 text-sm relative"
+        :class="classes"
+      >
+        <VormInput type="checkbox" no-label />
+        <Icon name="tools" size="xl" class="text-destructive-500" />
+        <div class="w-full text-ellipsis whitespace-nowrap overflow-hidden">
+          {{ fix.name }}
+        </div>
+      </div>
+    </template>
+    <template #default>
+      <div class="pl-8 pr-2 py-4">
+        <span class="font-bold">Recommendation: </span>{{ fix.recommendation }}
+      </div>
+    </template>
+  </SiCollapsible>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import Icon from "@/ui-lib/Icon.vue";
+import VormInput from "@/ui-lib/forms/VormInput.vue";
+import SiCollapsible from "@/organisms/SiCollapsible.vue";
+
+type Fix = {
+  id: number;
+  name: string;
+  recommendation: string;
+};
+
+const props = defineProps<{
+  fix: Fix;
+  class?: string;
+}>();
+
+const classes = computed(() => props.class);
+</script>
--- app/web/src/organisms/FixPicker.vue
+++ app/web/src/organisms/FixPicker.vue
@@ -7,7 +7,36 @@
       <SiDropdownItem>FIX</SiDropdownItem>
     </template>
     <template #panels>
-      <TabPanel :key="0" class="h-full overflow-auto"> </TabPanel>
+      <TabPanel :key="0" class="h-full overflow-hidden">
+        <SiSearch auto-search placeholder="search fixes" />
+        <div
+          class="w-full text-neutral-400 dark:text-neutral-300 text-sm p-2 border-b dark:border-neutral-600"
+        >
+          Select fixes from the list below to run them.
+        </div>
+        <div
+          class="w-full text-neutral-400 dark:text-neutral-300 text-sm p-2 border-b dark:border-neutral-600 flex flex-row items-center justify-between whitespace-nowrap gap-4 overflow-auto"
+        >
+          <VormInput type="checkbox" label="Select All" no-label
+            >Select All</VormInput
+          >
+          <VButton2 icon="tools" tone="action">Fix Resources</VButton2>
+        </div>
+        <ul class="overflow-y-auto">
+          <SiCollapsible as="li" class="w-full" content-as="ul" default-open>
+            <template #label>
+              <div class="flex flex-row items-center justify-between">
+                <div>Resources</div>
+              </div>
+            </template>
+            <template #default>
+              <li v-for="fix in fixes" :key="fix.id">
+                <FixSprite :fix="fix" />
+              </li>
+            </template>
+          </SiCollapsible>
+        </ul>
+      </TabPanel>
     </template>
   </SiTabGroup>
 </template>
@@ -17,4 +46,24 @@ import { TabPanel } from "@headlessui/vue";
 import SiTabGroup from "@/molecules/SiTabGroup.vue";
 import SiTabHeader from "@/molecules/SiTabHeader.vue";
 import SiDropdownItem from "@/atoms/SiDropdownItem.vue";
+import SiSearch from "@/molecules/SiSearch.vue";
+import Icon from "@/ui-lib/Icon.vue";
+import VormInput from "@/ui-lib/forms/VormInput.vue";
+import VButton2 from "@/ui-lib/VButton2.vue";
+import FixSprite from "@/molecules/FixSprite.vue";
+import SiCollapsible from "./SiCollapsible.vue";
+
+const fixes = [
+  {
+    id: 1,
+    name: "This is a fix!",
+    recommendation:
+      "this is what we recommend you do - just fix this thing and you will be all good",
+  },
+  {
+    id: 2,
+    name: "Also a fix.",
+    recommendation: "honestly idk, you figure it out",
+  },
+];
 </script>
