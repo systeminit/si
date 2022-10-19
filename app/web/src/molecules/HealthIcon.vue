c5e4955468d79321d2a9b521f0e486c9ecd41161
wendybujalski <wendy@systeminit.com>
2022-10-19 17:56:48 -0400

    fix(web) - restored HealthIcon since it's still being used

---
+++ app/web/src/molecules/HealthIcon.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    v-if="health === ResourceHealth.Ok"
+    class="flex flex-row whitespace-nowrap items-center text-sm"
+  >
+    <Icon
+      name="check-circle"
+      class="text-success-500"
+      :class="!removeRightPadding ? 'pr-2' : ''"
+      :size="size"
+    />
+    <span v-if="!hideText">Health: {{ health }}</span>
+  </div>
+  <div
+    v-else-if="health === ResourceHealth.Warning"
+    class="flex flex-row whitespace-nowrap items-center text-sm"
+  >
+    <Icon
+      name="exclamation-circle"
+      class="text-warning-300"
+      :class="!removeRightPadding ? 'pr-2' : ''"
+      :size="size"
+    />
+    <span v-if="!hideText">Health: {{ health }}</span>
+  </div>
+  <div
+    v-else-if="health === ResourceHealth.Error"
+    class="flex flex-row whitespace-nowrap items-center text-sm"
+  >
+    <Icon
+      name="x-circle"
+      class="text-destructive-500"
+      :class="!removeRightPadding ? 'pr-2' : ''"
+      :size="size"
+    />
+    <span v-if="!hideText">Health: {{ health }}</span>
+  </div>
+  <div v-else class="flex flex-row whitespace-nowrap items-center text-sm">
+    <Icon
+      name="help-circle"
+      class="text-neutral-300"
+      :class="!removeRightPadding ? 'pr-2' : ''"
+      :size="size"
+    />
+    <span v-if="!hideText">Health: {{ health }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+import Icon, { IconSizes } from "@/ui-lib/Icon.vue";
+import { ResourceHealth } from "@/api/sdf/dal/resource";
+
+export type WorkflowStatus = "running" | "success" | "failure";
+defineProps({
+  health: {
+    type: String as PropType<ResourceHealth>,
+    required: true,
+  },
+  hideText: { type: Boolean, default: false },
+  size: {
+    type: String as PropType<IconSizes>,
+    default: "lg",
+  },
+  removeRightPadding: { type: Boolean, default: false },
+});
+</script>
