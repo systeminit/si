2560a4e85ac3913ec331c2760b9674e6a1ee8643
Fletcher Nichol <fletcher@systeminit.com>
2022-11-04 12:12:29 -0600

    chore(veritech): update logging modules for Veritech server
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- bin/veritech/src/main.rs
+++ bin/veritech/src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<()> {
         .service_name("veritech")
         .service_namespace("si")
         .log_env_var_prefix("SI")
-        .app_modules(vec!["veritech_cli", "veritech"])
+        .app_modules(vec!["veritech_cli", "veritech_server"])
         .build()?;
     let telemetry = telemetry_application::init(config)?;
     let args = args::parse();
