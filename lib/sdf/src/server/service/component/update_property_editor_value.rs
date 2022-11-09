48015dd79c990cd89ab891ca55096496481ffa92
Zachary Hamm <zack@systeminit.com>
2022-11-09 14:12:45 -0600

    Revert "fix(sdf): update_property_editor_value should lookup value by context first"
    This reverts commit 24f4cd3e0e0e5af1784a7e1a6feebd54c6507727.

--- lib/sdf/src/server/service/component/update_property_editor_value.rs
+++ lib/sdf/src/server/service/component/update_property_editor_value.rs
@@ -1,8 +1,5 @@
 use axum::Json;
-use dal::{
-    AttributeContext, AttributeReadContext, AttributeValue, AttributeValueId, ExternalProviderId,
-    InternalProviderId, StandardModel, SystemId, Visibility, WsEvent,
-};
+use dal::{AttributeContext, AttributeValue, AttributeValueId, Visibility, WsEvent};
 use serde::{Deserialize, Serialize};
 
 use super::ComponentResult;
@@ -33,44 +30,10 @@ pub async fn update_property_editor_value(
 ) -> ComponentResult<Json<UpdatePropertyEditorValueResponse>> {
     let ctx = builder.build(request_ctx.build(request.visibility)).await?;
 
-    let read_context = AttributeReadContext {
-        prop_id: Some(request.attribute_context.prop_id()),
-        internal_provider_id: Some(InternalProviderId::NONE),
-        external_provider_id: Some(ExternalProviderId::NONE),
-        schema_id: Some(request.attribute_context.schema_id()),
-        schema_variant_id: Some(request.attribute_context.schema_variant_id()),
-        component_id: Some(request.attribute_context.component_id()),
-        system_id: Some(SystemId::NONE),
-    };
-
-    // When we set the value for the first time, we might only have the schema variant specific
-    // value id since there may not yet be a value for the component context. Let's make sure a
-    // value does not already exist for this context first. And if it does, set *that* value
-    // instead.
-    let (av_id, maybe_pav_id) =
-        if let Some(av) = AttributeValue::find_for_context(&ctx, read_context).await? {
-            if *av.id() != request.attribute_value_id {
-                (
-                    *av.id(),
-                    av.parent_attribute_value(&ctx).await?.map(|pav| *pav.id()),
-                )
-            } else {
-                (
-                    request.attribute_value_id,
-                    request.parent_attribute_value_id,
-                )
-            }
-        } else {
-            (
-                request.attribute_value_id,
-                request.parent_attribute_value_id,
-            )
-        };
-
     let (_, _) = AttributeValue::update_for_context(
         &ctx,
-        av_id,
-        maybe_pav_id,
+        request.attribute_value_id,
+        request.parent_attribute_value_id,
         request.attribute_context,
         request.value,
         request.key,
