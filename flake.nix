42ea76cf6cb33fd0a7495dfc3c399515c074495e
Jacob Helwig <jacob@technosorcery.net>
2023-02-08 22:59:04 +0000

    feat: Create base Nix development environment

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Development environment for System Initiative";
+
+  # Flake inputs
+  inputs = {
+    # Unstable nixpkgs to get pnpm
+    nixpkgs.url = "github:NixOS/nixpkgs/master"; # also valid: "nixpkgs"
+    rust-overlay.url = "github:oxalica/rust-overlay"; # A helper for Rust + Nix
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      # Overlays enable you to customize the Nixpkgs attribute set
+      overlays = [
+        # Makes a `rust-bin` attribute available in Nixpkgs
+        (import rust-overlay)
+        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to 
+        # create a Rust environment
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default;
+        })
+      ];
+
+      # Helper to provide system-specific attributes
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          buildInputs = (with pkgs; [
+            rustToolchain
+
+            automake
+            awscli
+            bash
+            butane
+            coreutils
+            docker-compose
+            gcc
+            git
+            kubeval
+            libtool
+            gnumake
+            protobuf
+            skopeo
+            jq
+            nodejs-16_x
+            nodePackages.pnpm
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+
+          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
+        };
+      });
+    };
+}
