17874dec5e59e0c33b061e8f1f1ce1c5bbedafae
Zachary Hamm <zack@systeminit.com>
2023-03-01 13:57:08 -0600

    feat(sdf): actually install sipkgs

--- lib/sdf/src/server/service/pkg/install_pkg.rs
+++ lib/sdf/src/server/service/pkg/install_pkg.rs
@@ -1,7 +1,7 @@
 use super::{get_pkgs_path, pkg_lookup, PkgError, PkgResult};
 use crate::server::extract::{AccessBuilder, HandlerContext};
 use axum::Json;
-use dal::{installed_pkg::InstalledPkg, StandardModel, Visibility, WsEvent};
+use dal::{installed_pkg::InstalledPkg, pkg::import_pkg, StandardModel, Visibility, WsEvent};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -25,11 +25,14 @@ pub async fn install_pkg(
 ) -> PkgResult<Json<InstallPkgResponse>> {
     let ctx = builder.build(request_ctx.build(request.visibility)).await?;
 
-    let real_pkg_file_name = pkg_lookup(get_pkgs_path(&builder).await?, &request.name).await?;
+    let real_pkg_path = pkg_lookup(get_pkgs_path(&builder).await?, &request.name).await?;
 
-    let real_pkg_file_name = match real_pkg_file_name {
+    let (real_pkg_file_name, real_pkg_path) = match real_pkg_path {
         None => return Err(PkgError::PackageNotFound(request.name)),
-        Some(real_pkg_file_name) => real_pkg_file_name,
+        Some(real_pkg_path) => match real_pkg_path.file_name() {
+            None => unreachable!(),
+            Some(file_name) => (file_name.to_string_lossy().to_string(), real_pkg_path),
+        },
     };
 
     if !InstalledPkg::find_by_attr(&ctx, "name", &real_pkg_file_name)
@@ -39,7 +42,8 @@ pub async fn install_pkg(
         return Err(PkgError::PackageAlreadyInstalled(real_pkg_file_name));
     }
 
-    InstalledPkg::new(&ctx, &request.name, None, None).await?;
+    let pkg = import_pkg(&ctx, &real_pkg_path).await?;
+    InstalledPkg::new(&ctx, &request.name, Some(pkg.hash()?.to_string()), None).await?;
 
     WsEvent::change_set_written(&ctx)
         .await?
