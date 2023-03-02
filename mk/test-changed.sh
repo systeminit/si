6f69503aa60138b6e0242fa755eca6d67cc0af67
Fletcher Nichol <fletcher@systeminit.com>
2023-03-02 16:34:49 -0700

    fix(ci): run all Rust targets only when necessary
    This change updates the intention of the `mk/test-changed.sh` script to
    run all Rust tests only when a global config file such as
    `Cargo.toml`/`Cargo.lock`/etc. has been modified in a Git change set.
    
    The prior implementation caused some PRs to run all tests for trivial
    changes (like *.md files in the root) or to die when a component was
    living under `component/` (which don't have a `Makefile`).
    
    Now, when the following files are present in the list of changed files,
    all Rust `test//*` Make tasks will be run:
    
    - `Cargo.lock`
    - `Cargo.toml`
    - `rust-toolchain`
    
    Additionally, when the following files are present in the list of
    changed files, all Rust `check//*` Make tasks will be run:
    
    - `rustfmt.toml`
    - `clippy.toml`
    
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- mk/test-changed.sh
+++ mk/test-changed.sh
@@ -4,13 +4,44 @@ set -eu
 BEFORE_SHA="${1:-"main"}"
 AFTER_SHA="${2:-"HEAD"}"
 
+changed_files() {
+  git --no-pager diff --name-only "$BEFORE_SHA...$AFTER_SHA"
+}
+
 CHANGED_COMPONENTS="$(
-  git --no-pager diff --name-only "$BEFORE_SHA...$AFTER_SHA" \
+  changed_files \
     | xargs dirname \
-    | grep '^app/\|lib/\|bin/\|.' \
+    | grep '^app/\|lib/\|bin/' \
     | awk -F"/" '{print $1 "/" $2 }' \
     | sort -u
 )"
+
+changed_files_contains_global_rust_config_for_test() {
+  changed_files | grep -q -E '^(Cargo\.(toml|lock)|rust-toolchain)$'
+}
+
+changed_files_contains_global_rust_config_for_check() {
+  changed_files | grep -q -E '^(clippy.toml|rustfmt.toml)$'
+}
+
+cargo_workspace_member_paths() {
+  local root
+  root="$(cargo metadata --offline --locked --no-deps --quiet \
+    | jq -r .workspace_root)"
+
+  cargo metadata --offline --locked --no-deps --quiet \
+    | jq -r .workspace_members[] \
+    | sed -e "s|^.*(path+file://$root/\(.*\))$|\1|g" \
+    | sort
+}
+
+all_rust_test_targets() {
+  cargo_workspace_member_paths | sed 's|^|test//|'
+}
+
+all_rust_check_targets() {
+  cargo_workspace_member_paths | sed 's|^|check//|'
+}
 
 echo "::group::Changed Components"
 echo "$CHANGED_COMPONENTS" | tr '[:space:]' '\n'
@@ -18,12 +49,14 @@ echo "::endgroup::"
 
 if [ -z "${SKIP_CHECK:-}" ]; then
   check_targets="$(while IFS= read -r line; do
-    if [[ $line = "./" ]]; then
-      echo "check"
-    elif [[ $line != "./" && -f "$line/Makefile" ]]; then
+    if [[ -f "$line/Makefile" ]]; then
       echo "check//$line"
     fi
   done <<<"$CHANGED_COMPONENTS")"
+  if changed_files_contains_global_rust_config_for_check; then
+    check_targets="$check_targets $(all_rust_check_targets)"
+  fi
+
   echo "::group::make $check_targets"
   set -x
   make CI=true "CI_FROM_REF=$BEFORE_SHA" "CI_TO_REF=$AFTER_SHA" $check_targets
@@ -32,12 +65,16 @@ if [ -z "${SKIP_CHECK:-}" ]; then
 fi
 
 test_targets="$(while IFS= read -r line; do
-  if [[ $line = "./" ]]; then
-    echo "test"
-  elif [[ $line != "./" && -f "$line/Makefile" ]]; then
+  if [[ -f "$line/Makefile" ]]; then
     echo "test//$line"
   fi
 done <<<"$CHANGED_COMPONENTS")"
+# If a global Rust config file has changed then we want to test all Rust
+# components in the Cargo workspace
+if changed_files_contains_global_rust_config_for_test; then
+  test_targets="$test_targets $(all_rust_test_targets)"
+fi
+
 echo "::group::make $test_targets"
 set -x
 make CI=true "CI_FROM_REF=$BEFORE_SHA" "CI_TO_REF=$AFTER_SHA" $test_targets
