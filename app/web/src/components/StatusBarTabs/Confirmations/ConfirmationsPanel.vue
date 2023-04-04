7e426a481c756edf386bd4730cd130f865e34b1a
wendybujalski <wendydbujalski@gmail.com>
2023-04-04 13:17:44 -0400

    fix(web) - fixed issues with component selection on the ConfirmationsPanel

--- app/web/src/components/StatusBarTabs/Confirmations/ConfirmationsPanel.vue
+++ app/web/src/components/StatusBarTabs/Confirmations/ConfirmationsPanel.vue
@@ -2,10 +2,19 @@
   <div class="flex flex-row h-full w-full">
     <ConfirmationsResourceList />
     <div
-      v-if="!hasSelectedResource"
+      v-if="
+        !componentsStore.selectedComponentId ||
+        !componentIds.includes(componentsStore.selectedComponentId)
+      "
       class="flex flex-row items-center text-center w-full h-full bg-shade-100"
     >
-      <p class="w-full text-3xl text-neutral-500">No Resource Selected</p>
+      <p class="w-full text-3xl text-neutral-500">
+        {{
+          !componentsStore.selectedComponentId
+            ? "No Component Selected"
+            : "Selected Component Does Not Have A Resource"
+        }}
+      </p>
     </div>
     <ConfirmationViewerMultiple v-else />
     <!-- <div
@@ -21,12 +30,11 @@
 import * as _ from "lodash-es";
 import { computed } from "vue";
 import { useComponentsStore } from "@/store/components.store";
-
+import { useFixesStore } from "@/store/fixes.store";
 import ConfirmationViewerMultiple from "./ConfirmationViewerMultiple.vue";
 import ConfirmationsResourceList from "./ConfirmationsResourceList.vue";
 
+const fixesStore = useFixesStore();
+const componentIds = computed(() => fixesStore.allComponents);
 const componentsStore = useComponentsStore();
-const hasSelectedResource = computed(
-  () => !!componentsStore.selectedComponentId,
-);
 </script>
