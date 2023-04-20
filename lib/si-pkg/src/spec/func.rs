62c0c86f055bacde57b97a72c2b67feb04736397
Zachary Hamm <zack@systeminit.com>
2023-04-20 18:50:04 -0500

    chore(si-pkg): break spec up into modules

---
+++ lib/si-pkg/src/spec/func.rs
@@ -0,0 +1,169 @@
+use derive_builder::Builder;
+use object_tree::Hash;
+use serde::{Deserialize, Serialize};
+use strum_macros::{AsRefStr, Display, EnumIter, EnumString};
+use url::Url;
+
+use super::SpecError;
+
+#[derive(
+    Deserialize,
+    Serialize,
+    AsRefStr,
+    Display,
+    EnumIter,
+    EnumString,
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+)]
+#[serde(rename_all = "camelCase")]
+pub enum FuncArgumentKind {
+    Array,
+    Boolean,
+    Integer,
+    Object,
+    String,
+    Map,
+    Any,
+}
+
+#[derive(Builder, Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[builder(build_fn(error = "SpecError"))]
+pub struct FuncArgumentSpec {
+    #[builder(setter(into))]
+    pub name: String,
+    #[builder(setter(into))]
+    pub kind: FuncArgumentKind,
+    #[builder(setter(into))]
+    pub element_kind: Option<FuncArgumentKind>,
+}
+
+impl FuncArgumentSpec {
+    pub fn builder() -> FuncArgumentSpecBuilder {
+        FuncArgumentSpecBuilder::default()
+    }
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, AsRefStr, Display, EnumIter, EnumString)]
+#[serde(rename_all = "camelCase")]
+pub enum FuncSpecBackendKind {
+    JsAttribute,
+    JsWorkflow,
+    JsCommand,
+    JsValidation,
+    Json,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, AsRefStr, Display, EnumIter, EnumString)]
+#[serde(rename_all = "camelCase")]
+pub enum FuncSpecBackendResponseType {
+    Array,
+    Boolean,
+    Integer,
+    Map,
+    Object,
+    Qualification,
+    CodeGeneration,
+    Confirmation,
+    String,
+    Json,
+    Validation,
+    Workflow,
+    Command,
+}
+
+pub type FuncUniqueId = Hash;
+
+#[derive(Builder, Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[builder(build_fn(error = "SpecError"))]
+pub struct FuncSpec {
+    #[builder(setter(into))]
+    pub name: String,
+    #[builder(setter(into, strip_option), default)]
+    pub display_name: Option<String>,
+    #[builder(setter(into, strip_option), default)]
+    pub description: Option<String>,
+    #[builder(setter(into))]
+    pub handler: String,
+    #[builder(setter(into))]
+    pub code_base64: String,
+    #[builder(setter(into))]
+    pub backend_kind: FuncSpecBackendKind,
+    #[builder(setter(into))]
+    pub response_type: FuncSpecBackendResponseType,
+    #[builder(setter(into))]
+    pub hidden: bool,
+    #[builder(field(type = "FuncUniqueId", build = "self.build_func_unique_id()"))]
+    pub unique_id: FuncUniqueId,
+
+    #[builder(setter(into, strip_option), default)]
+    pub link: Option<Url>,
+
+    #[builder(setter(each(name = "argument"), into), default)]
+    pub arguments: Vec<FuncArgumentSpec>,
+}
+
+impl FuncSpec {
+    #[must_use]
+    pub fn builder() -> FuncSpecBuilder {
+        FuncSpecBuilder::default()
+    }
+}
+
+impl FuncSpecBuilder {
+    #[allow(unused_mut)]
+    pub fn try_link<V>(&mut self, value: V) -> Result<&mut Self, V::Error>
+    where
+        V: TryInto<Url>,
+    {
+        let converted: Url = value.try_into()?;
+        Ok(self.link(converted))
+    }
+
+    fn build_func_unique_id(&self) -> Hash {
+        // Not happy about all these clones and unwraps...
+        let mut bytes = vec![];
+        bytes.extend(self.name.clone().unwrap_or("".to_string()).as_bytes());
+        bytes.extend(
+            self.display_name
+                .clone()
+                .unwrap_or(Some("".to_string()))
+                .unwrap_or("".to_string())
+                .as_bytes(),
+        );
+        bytes.extend(
+            self.description
+                .clone()
+                .unwrap_or(Some("".to_string()))
+                .unwrap_or("".to_string())
+                .as_bytes(),
+        );
+        bytes.extend(self.handler.clone().unwrap_or("".to_string()).as_bytes());
+        bytes.extend(
+            self.code_base64
+                .clone()
+                .unwrap_or("".to_string())
+                .as_bytes(),
+        );
+        bytes.extend(
+            self.backend_kind
+                .unwrap_or(FuncSpecBackendKind::Json)
+                .to_string()
+                .as_bytes(),
+        );
+        bytes.extend(
+            self.response_type
+                .unwrap_or(FuncSpecBackendResponseType::Json)
+                .to_string()
+                .as_bytes(),
+        );
+        bytes.extend(&[self.hidden.unwrap_or(false).into()]);
+
+        Hash::new(&bytes)
+    }
+}
---
+++ lib/si-pkg/src/spec/validation.rs
@@ -0,0 +1,151 @@
+use derive_builder::UninitializedFieldError;
+use serde::{Deserialize, Serialize};
+use strum_macros::{Display, EnumIter, EnumString};
+
+use object_tree::Hash;
+
+use super::SpecError;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(tag = "kind", rename_all = "camelCase")]
+pub enum ValidationSpec {
+    IntegerIsBetweenTwoIntegers {
+        lower_bound: i64,
+        upper_bound: i64,
+    },
+    StringEquals {
+        expected: String,
+    },
+    StringHasPrefix {
+        expected: String,
+    },
+    StringInStringArray {
+        expected: Vec<String>,
+        display_expected: bool,
+    },
+    StringIsValidIpAddr,
+    StringIsHexColor,
+    StringIsNotEmpty,
+    CustomValidation {
+        func_unique_id: Hash,
+    },
+}
+
+impl ValidationSpec {
+    pub fn builder() -> ValidationSpecBuilder {
+        ValidationSpecBuilder::default()
+    }
+}
+
+#[derive(
+    Clone, Copy, Debug, Eq, Hash, PartialEq, EnumIter, EnumString, Display, Serialize, Deserialize,
+)]
+pub enum ValidationSpecKind {
+    IntegerIsBetweenTwoIntegers,
+    StringEquals,
+    StringHasPrefix,
+    StringInStringArray,
+    StringIsValidIpAddr,
+    StringIsHexColor,
+    StringIsNotEmpty,
+    CustomValidation,
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct ValidationSpecBuilder {
+    kind: Option<ValidationSpecKind>,
+    upper_bound: Option<i64>,
+    lower_bound: Option<i64>,
+    expected_string: Option<String>,
+    expected_string_array: Option<Vec<String>>,
+    display_expected: Option<bool>,
+    func_unique_id: Option<Hash>,
+}
+
+impl ValidationSpecBuilder {
+    pub fn kind(&mut self, kind: ValidationSpecKind) -> &mut Self {
+        self.kind = Some(kind);
+        self
+    }
+
+    pub fn upper_bound(&mut self, upper_bound: i64) -> &mut Self {
+        self.upper_bound = Some(upper_bound);
+        self
+    }
+
+    pub fn lower_bound(&mut self, lower_bound: i64) -> &mut Self {
+        self.lower_bound = Some(lower_bound);
+        self
+    }
+
+    pub fn expected_string(&mut self, expected_string: String) -> &mut Self {
+        self.expected_string = Some(expected_string);
+        self
+    }
+
+    pub fn expected_string_array(&mut self, expected_string_array: Vec<String>) -> &mut Self {
+        self.expected_string_array = Some(expected_string_array);
+        self
+    }
+
+    pub fn display_expected(&mut self, display_expected: bool) -> &mut Self {
+        self.display_expected = Some(display_expected);
+        self
+    }
+
+    pub fn func_unique_id(&mut self, func_unique_id: Hash) -> &mut Self {
+        self.func_unique_id = Some(func_unique_id);
+        self
+    }
+
+    pub fn build(&self) -> Result<ValidationSpec, SpecError> {
+        Ok(match self.kind {
+            Some(kind) => match kind {
+                ValidationSpecKind::IntegerIsBetweenTwoIntegers => {
+                    ValidationSpec::IntegerIsBetweenTwoIntegers {
+                        lower_bound: self
+                            .lower_bound
+                            .ok_or(UninitializedFieldError::from("lower_bound"))?,
+                        upper_bound: self
+                            .upper_bound
+                            .ok_or(UninitializedFieldError::from("lower_bound"))?,
+                    }
+                }
+                ValidationSpecKind::StringEquals => ValidationSpec::StringEquals {
+                    expected: self
+                        .expected_string
+                        .as_ref()
+                        .ok_or(UninitializedFieldError::from("expected_string"))?
+                        .to_string(),
+                },
+                ValidationSpecKind::StringHasPrefix => ValidationSpec::StringHasPrefix {
+                    expected: self
+                        .expected_string
+                        .as_ref()
+                        .ok_or(UninitializedFieldError::from("expected_string"))?
+                        .to_string(),
+                },
+                ValidationSpecKind::StringInStringArray => ValidationSpec::StringInStringArray {
+                    display_expected: self
+                        .display_expected
+                        .ok_or(UninitializedFieldError::from("display_expected"))?,
+                    expected: self
+                        .expected_string_array
+                        .clone()
+                        .ok_or(UninitializedFieldError::from("expected_string"))?,
+                },
+                ValidationSpecKind::StringIsValidIpAddr => ValidationSpec::StringIsValidIpAddr,
+                ValidationSpecKind::StringIsHexColor => ValidationSpec::StringIsHexColor,
+                ValidationSpecKind::StringIsNotEmpty => ValidationSpec::StringIsNotEmpty,
+                ValidationSpecKind::CustomValidation => ValidationSpec::CustomValidation {
+                    func_unique_id: self
+                        .func_unique_id
+                        .ok_or(UninitializedFieldError::from("func_unique_id"))?,
+                },
+            },
+            None => {
+                return Err(UninitializedFieldError::from("kind").into());
+            }
+        })
+    }
+}
