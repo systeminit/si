079bd84197c8911b7372f858382431a22575cb41
stack72 <public@paulstack.co.uk>
2023-06-07 21:23:32 +0300

    feat(web): Make the Assets tab the default in the customize screen

--- app/web/src/components/CustomizeTabs.vue
+++ app/web/src/components/CustomizeTabs.vue
@@ -4,14 +4,14 @@
       :start-selected-tab-slug="tabContentSlug"
       @update:selected-tab="onTabChange"
     >
+      <TabGroupItem slug="assets" label="ASSETS">
+        <slot v-if="tabContentSlug === 'assets'" />
+      </TabGroupItem>
       <TabGroupItem slug="functions" label="FUNCTIONS">
         <slot v-if="tabContentSlug === 'functions'" />
       </TabGroupItem>
       <TabGroupItem slug="packages" label="MODULES">
         <slot v-if="tabContentSlug === 'packages'" />
-      </TabGroupItem>
-      <TabGroupItem slug="assets" label="ASSETS">
-        <slot v-if="tabContentSlug === 'assets'" />
       </TabGroupItem>
     </TabGroup>
   </div>
@@ -27,7 +27,7 @@ const route = useRoute();
 
 defineProps({
   tabContentSlug: {
-    type: String as PropType<"functions" | "packages" | "assets">,
+    type: String as PropType<"assets" | "functions" | "packages">,
     required: true,
   },
 });
