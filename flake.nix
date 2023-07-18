311cd854dd0c686e1787977a26e44e6e9e4e25f9
Fletcher Nichol <fletcher@systeminit.com>
2023-07-17 22:53:43 -0600

    style(nix): fmt flake using alejandra
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- flake.nix
+++ flake.nix
@@ -15,47 +15,55 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [
-          (self: super: {
-            nodejs = super.nodejs-18_x;
-            pnpm = super.nodePackages.pnpm;
-          })
-          (import rust-overlay)
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (self: super: {
+          nodejs = super.nodejs-18_x;
+          pnpm = super.nodePackages.pnpm;
+        })
+        (import rust-overlay)
+      ];
 
-        pkgs = import nixpkgs { inherit overlays system; };
+      pkgs = import nixpkgs {inherit overlays system;};
 
-        rustVersion = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-        rust-toolchain = rustVersion.override {
-          extensions = [ "rust-analyzer" "rust-src" ];
-        };
-
-      in with pkgs; {
+      rustVersion = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+      rust-toolchain = rustVersion.override {
+        extensions = ["rust-analyzer" "rust-src"];
+      };
+    in
+      with pkgs; {
         devShells.default = mkShell {
-          packages = [
-            bash
-            buck2
-            clang
-            docker-compose
-            gh
-            jq
-            lld
-            nodejs
-            pgcli
-            pnpm
-            protobuf
-            python3
-            reindeer
-            rust-toolchain
-            tilt
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            libiconv
-            darwin.apple_sdk.frameworks.Security
-            darwin.apple_sdk.frameworks.SystemConfiguration
-          ];
+          packages =
+            [
+              alejandra
+              bash
+              buck2
+              clang
+              docker-compose
+              gh
+              jq
+              lld
+              nodejs
+              pgcli
+              pnpm
+              protobuf
+              python3
+              reindeer
+              rust-toolchain
+              tilt
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              libiconv
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
           depsTargetTarget = [
             awscli
             butane
@@ -63,5 +71,7 @@
             skopeo
           ];
         };
+
+        formatter = alejandra;
       });
 }
