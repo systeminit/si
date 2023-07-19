3dd8b8d1f688ee11b64c3fd26e4d5d531b8c9c26
Fletcher Nichol <fletcher@systeminit.com>
2023-07-18 18:24:28 -0600

    build(nix): create package impls for bin components
    This change updates the Nix flake to add a package description for
    buildable/runnable components found under the `bin/` directory in the
    monorepo. Importantly, the following components have package
    definitions:
    
    - `bin/council` (built with `nix build .#council`)
    - `bin/cyclone` (built with `nix build .#cyclone`)
    - `bin/lang-js` (built with `nix build .#lang-js`)
    - `bin/module-index` (built with `nix build .#module-index`)
    - `bin/pinga` (built with `nix build .#pinga`)
    - `bin/sdf` (built with `nix build .#sdf`)
    - `bin/si` (built with `nix build .#si`)
    - `bin/veritech` (built with `nix build .#veritech`)
    
    Future changes will drive these Nix package builds in a Dockerfile to
    produce reasonably isolated builds.
    
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Development environment for System Initiative";
 
-  # Flake inputs
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,7 +13,6 @@
     };
   };
 
-  # Flake outputs
   outputs = {
     self,
     nixpkgs,
@@ -37,39 +35,121 @@
       rust-toolchain = rustVersion.override {
         extensions = ["rust-analyzer" "rust-src"];
       };
+
+      buck2NativeBuildInputs = with pkgs;
+        [
+          bash
+          buck2
+          cacert
+          clang
+          gitMinimal
+          lld
+          nodejs
+          pnpm
+          protobuf
+          python3
+          ripgrep
+          rust-toolchain
+
+          # breakpointHook
+        ]
+        ++ lib.optionals pkgs.stdenv.isDarwin [
+          darwin.sigtool
+        ]
+        ++ lib.optionals pkgs.stdenv.isLinux [
+          autoPatchelfHook
+        ];
+
+      buck2BuildInputs = with pkgs;
+        []
+        ++ lib.optionals pkgs.stdenv.isLinux [
+          glibc
+          glibc.libgcc
+        ]
+        ++ lib.optionals pkgs.stdenv.isDarwin [
+          libiconv
+          darwin.apple_sdk.frameworks.Security
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+
+      buck2Derivation = {
+        pathPrefix,
+        pkgName,
+      }:
+        pkgs.stdenv.mkDerivation rec {
+          name = pkgName;
+          buck2_target = "//${pathPrefix}/${pkgName}";
+          __impure = true;
+          src = ./.;
+          nativeBuildInputs = buck2NativeBuildInputs;
+          buildInputs = buck2BuildInputs;
+          runtimeDependencies = map pkgs.lib.getLib buck2BuildInputs;
+          postPatch = with pkgs; ''
+            rg -l '#!(/usr/bin/env|/bin/bash|/bin/sh)' prelude prelude-si \
+              | while read -r file; do
+                patchShebangs --build "$file"
+              done
+
+            rg -l '(/usr/bin/env|/bin/bash)' prelude prelude-si \
+              | while read -r file; do
+                substituteInPlace "$file" \
+                  --replace /usr/bin/env "${coreutils}/bin/env" \
+                  --replace /bin/bash "${bash}/bin/bash"
+              done
+          '';
+          buildPhase = ''
+            export HOME="$(dirname $(pwd))/home"
+            buck2 build "$buck2_target" --verbose 3 --out "$name-$system"
+          '';
+          installPhase = ''
+            mkdir -p "$out/bin"
+            cp -p "$name-$system" "$out/bin/$name"
+          '';
+        };
+
+      binDerivation = pkgName: (buck2Derivation {
+        pathPrefix = "bin";
+        inherit
+          pkgName
+          ;
+      });
     in
-      with pkgs; {
+      with pkgs; rec {
+        packages = {
+          council = binDerivation "council";
+          cyclone = binDerivation "cyclone";
+          lang-js = binDerivation "lang-js";
+          module-index = binDerivation "module-index";
+          pinga = binDerivation "pinga";
+          sdf = binDerivation "sdf";
+          si = binDerivation "si";
+          veritech = binDerivation "veritech";
+        };
+
         devShells.default = mkShell {
           packages =
             [
               alejandra
-              bash
-              buck2
-              clang
               docker-compose
               gh
               jq
-              lld
-              nodejs
               pgcli
-              pnpm
-              protobuf
-              python3
               reindeer
-              rust-toolchain
               tilt
+
+              awscli
+              butane
+              kubeval
+              skopeo
             ]
-            ++ lib.optionals pkgs.stdenv.isDarwin [
-              libiconv
-              darwin.apple_sdk.frameworks.Security
-              darwin.apple_sdk.frameworks.SystemConfiguration
-            ];
-          depsTargetTarget = [
-            awscli
-            butane
-            kubeval
-            skopeo
-          ];
+            # Directly add the build depndencies for the packages rather than
+            # use: `inputsFrom = lib.attrValues packages;`.
+            #
+            # This tweak means our flake doesn't require `impure-derivations`
+            # and `ca-derivations` experimental features by default--only when
+            # attempting to build the packages directly.
+            ++ buck2NativeBuildInputs
+            ++ buck2BuildInputs;
         };
 
         formatter = alejandra;
