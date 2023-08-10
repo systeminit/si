c77e3c628dafe2c20c279fa280915e1ffb52d167
stack72 <public@paulstack.co.uk>
2023-08-11 01:47:38 +0300

    feat(si): Block SI Launch from opening web ui until SDF is ready
    Co-authored-by: britmyerss <brit@systeminit.com>

--- lib/si-cli/src/cmd/launch.rs
+++ lib/si-cli/src/cmd/launch.rs
@@ -1,14 +1,17 @@
 use crate::key_management::get_user_email;
 use crate::state::AppState;
 use crate::{CliResult, SiCliError};
+use indicatif::{ProgressBar, ProgressStyle};
+use std::thread;
+use std::time::Duration;
 
 impl AppState {
     pub async fn launch(&self, launch_metrics: bool) -> CliResult<()> {
+        invoke(launch_metrics).await?;
         self.track(
             get_user_email().await?,
             serde_json::json!({"command-name": "launch-ui"}),
         );
-        invoke(launch_metrics).await?;
         Ok(())
     }
 }
@@ -20,12 +23,52 @@ async fn invoke(launch_metrics: bool) -> CliResult<()> {
         "http://localhost:8080"
     };
 
+    if path == "http://localhost:8080" {
+        check_sdf().await?;
+    }
+
     println!("Opening URL: {}", path);
     match open::that(path) {
         Ok(()) => Ok(()),
         Err(_err) => Err(SiCliError::FailToLaunch(path.to_string())),
     }
     .expect("issue opening url");
+
+    Ok(())
+}
+
+async fn check_sdf() -> CliResult<()> {
+    let spinner_style = ProgressStyle::with_template("{prefix:.bold.dim} {spinner} {wide_msg}")
+        .unwrap()
+        .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ");
+    let sdf_path = "http://localhost:5156/api/";
+
+    let mut is_ready = false;
+    let h = tokio::spawn(async move {
+        let count = 200;
+        let pb = ProgressBar::new(count);
+        pb.set_style(spinner_style.clone());
+        while !is_ready {
+            loop {
+                match reqwest::get(sdf_path).await {
+                    Ok(x) => {
+                        if x.status().as_u16() == 200 {
+                            is_ready = true;
+                            break;
+                        }
+                    }
+                    Err(_e) => {
+                        pb.set_message("{e}");
+                        pb.set_message("Almost ready, waiting for migrations to complete...");
+                        pb.inc(1);
+                        thread::sleep(Duration::new(10, 0));
+                    }
+                }
+            }
+        }
+    });
+
+    h.await?;
 
     Ok(())
 }
