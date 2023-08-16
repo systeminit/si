3dea9b003cb5dbcb90c2650bbf8d33bdbb0a74fb
stack72 <public@paulstack.co.uk>
2023-08-16 16:05:40 +0300

    fix(si): Ensure we are comparing the container version vs release digest from the auth-api for updates

--- lib/si-cli/src/containers.rs
+++ lib/si-cli/src/containers.rs
@@ -18,6 +18,7 @@ pub struct DockerReleaseInfo {
     pub git_sha: String,
     pub created_at: String,
     pub image: String,
+    pub version: String,
 }
 
 #[derive(Clone, Debug)]
@@ -72,6 +73,11 @@ impl DockerClient {
                 git_sha: container
                     .labels
                     .get("org.opencontainers.image.revision")
+                    .unwrap()
+                    .to_string(),
+                version: container
+                    .labels
+                    .get("org.opencontainers.image.version")
                     .unwrap()
                     .to_string(),
                 created_at: container
