6e3dfe979f30dc0614bc1db23b65f47fa4d295c8
Nick Gerace <nick@systeminit.com>
2023-08-23 13:52:38 -0400

    Ensure we can produce messages to RabbitMQ
    - Ensure we can produce messages to RabbitMQ by setting the advertised
      host to localhost (otherwise, the initial environment connection smoke
      test will work, but the stream client connection will fail)
    - Re-word manager, client and connection naming under the "environment"
      umbrella, which reflects how producer and consumer are named in the
      RabbitMQ wrapper crate
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>
    Co-authored-by: Zachary Hamm <zack@systeminit.com>

--- lib/si-rabbitmq/src/environment.rs
+++ lib/si-rabbitmq/src/environment.rs
@@ -1,35 +1,35 @@
 use rabbitmq_stream_client::types::ByteCapacity;
-use rabbitmq_stream_client::Environment;
+use rabbitmq_stream_client::Environment as UpstreamEnvironment;
 
 use crate::error::RabbitResult;
 
 /// A connection to a RabbitMQ node.
 #[allow(missing_debug_implementations)]
-pub struct StreamManager {
-    environment: Environment,
+pub struct Environment {
+    inner: UpstreamEnvironment,
 }
 
-impl StreamManager {
-    /// Creates a new [`StreamManager`], which contains a connection to a RabbitMQ node.
+impl Environment {
+    /// Creates a new [`Environment`], which contains a connection to a RabbitMQ node.
     pub async fn new() -> RabbitResult<Self> {
-        let environment = Environment::builder()
+        let inner = UpstreamEnvironment::builder()
             .host("localhost")
             .username("guest")
             .password("guest")
             .port(5552)
             .build()
             .await?;
-        Ok(Self { environment })
+        Ok(Self { inner })
     }
 
     /// Returns the inner data structure handling the connection.
-    pub fn inner(&self) -> &Environment {
-        &self.environment
+    pub fn inner(&self) -> &UpstreamEnvironment {
+        &self.inner
     }
 
     pub async fn create_stream(&self, stream: impl AsRef<str>) -> RabbitResult<()> {
         Ok(self
-            .environment
+            .inner
             .stream_creator()
             .max_length(ByteCapacity::KB(400))
             .create(stream.as_ref())
@@ -37,6 +37,6 @@ impl StreamManager {
     }
 
     pub async fn delete_stream(&self, stream: impl AsRef<str>) -> RabbitResult<()> {
-        Ok(self.environment.delete_stream(stream.as_ref()).await?)
+        Ok(self.inner.delete_stream(stream.as_ref()).await?)
     }
 }
