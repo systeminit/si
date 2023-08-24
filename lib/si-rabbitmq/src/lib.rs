fa0f2e8a1dce2923c53481e52d9cc0e1deb9e038
Nick Gerace <nick@systeminit.com>
2023-08-24 17:27:07 -0400

    Add gobbler client/server architecture
    This commit adds the gobbler client/server architecture. Primarily, it
    operates on three crates:
    
    - si-rabbitmq (modified): the crate for interacting with RabbitMQ that
      SI services will use or are using
    - gobbler-server (modified): the crate for running an outer management
      consumer loop as well as inner core consumer loops
    - gobbler-client (new): the crate for interfacing with the
      gobbler-server and testing the communication between the two
    
    The addition of gobbler-client is at the center of this commit.
    Essentially, it is not only being used as the interface to
    gobbler-server, but it is also being used as the entrypoint for testing
    the gobbler-server lifecycle.
    
    The outer and inner consumer loops provide the ability to manage gobbler
    singletons as well as run the gobbler singletons.
    The outer loop consumers on the "gobbler-management" stream and its
    lifecycle is owned by the gobbler-server. The inner loops are created on
    a per-change-set basis and are only spun up and down by requests from
    gobbler-clients. The inner loops provide the core work for the
    "gobbler". However, in this commit, all the inner loops do is directly
    return the message they were sent. This is done for testing purposes to
    ensure that all the scaffolding is there and sound.
    
    As far as the inner RabbitMQ communications go, this commit exposes
    "OffsetSpecification" for consumers. It is unclear if this will be a
    temporary change or not. We should know the exact offset of type u64 in
    the future, but for now, we are using what works for testing purposes.
    For context, these changes were driven by tests in the gobbler-client library.
    
    As a bonus, this commit also adds si-rabbitmq testin to protect against
    regressions. It's minimal for now and may remain that way.
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

--- lib/si-rabbitmq/src/lib.rs
+++ lib/si-rabbitmq/src/lib.rs
@@ -24,12 +24,74 @@
 )]
 
 mod consumer;
+mod delivery;
 mod environment;
 mod error;
 mod producer;
 
 pub use consumer::Consumer;
+pub use consumer::ConsumerHandle;
+pub use consumer::ConsumerOffsetSpecification;
+pub use delivery::Delivery;
 pub use environment::Environment;
 pub use error::RabbitError;
 pub use error::RabbitResult;
 pub use producer::Producer;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rabbitmq_stream_client::types::OffsetSpecification;
+    use tokio::test;
+
+    #[test]
+    async fn round_trip() {
+        let environment = Environment::new()
+            .await
+            .expect("could not create environment");
+
+        let stream = "test-stream";
+
+        environment
+            .delete_stream(stream)
+            .await
+            .expect("could not delete stream");
+        environment
+            .create_stream(stream)
+            .await
+            .expect("could not create stream");
+
+        let mut producer = Producer::new(&environment, "producer", stream)
+            .await
+            .expect("could not create producer");
+
+        let mut consumer = Consumer::new(&environment, stream, OffsetSpecification::Next)
+            .await
+            .expect("could not create consumer");
+
+        let message = "starfield";
+        producer
+            .send_single(message, None)
+            .await
+            .expect("could not send message");
+
+        let delivery = consumer
+            .next()
+            .await
+            .expect("could not consume")
+            .expect("empty delivery");
+        let found_contents: String =
+            serde_json::from_value(delivery.message_contents.expect("message contents empty"))
+                .expect("could not deserialize");
+
+        assert_eq!(message, &found_contents);
+
+        producer.close().await.expect("could not close producer");
+        let handle = consumer.handle();
+        handle.close().await.expect("could not close consumer");
+        environment
+            .delete_stream(stream)
+            .await
+            .expect("could not delete stream");
+    }
+}
