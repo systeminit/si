56f16006f6ed0a3c13edfe7fbf4fd9411a44be1f
Nick Gerace <nick@systeminit.com>
2023-08-24 17:27:07 -0400

    Add gobbler client/server architecture
    This commit adds the gobbler client/server architecture. Primarily, it
    operates on three crates:
    
    - si-rabbitmq (modified): the crate for interacting with RabbitMQ that
      SI services will use or are using
    - gobbler-server (modified): the crate for running an outer management
      consumer loop as well as inner core consumer loops
    - gobbler-client (new): the crate for interfacing with the
      gobbler-server and testing the communication between the two
    
    The addition of gobbler-client is at the center of this commit.
    Essentially, it is not only being used as the interface to
    gobbler-server, but it is also being used as the entrypoint for testing
    the gobbler-server lifecycle.
    
    The outer and inner consumer loops provide the ability to manage gobbler
    singletons as well as run the gobbler singletons.
    The outer loop consumers on the "gobbler-management" stream and its
    lifecycle is owned by the gobbler-server. The inner loops are created on
    a per-change-set basis and are only spun up and down by requests from
    gobbler-clients. The inner loops provide the core work for the
    "gobbler". However, in this commit, all the inner loops do is directly
    return the message they were sent. This is done for testing purposes to
    ensure that all the scaffolding is there and sound.
    
    As far as the inner RabbitMQ communications go, this commit exposes
    "OffsetSpecification" for consumers. It is unclear if this will be a
    temporary change or not. We should know the exact offset of type u64 in
    the future, but for now, we are using what works for testing purposes.
    For context, these changes were driven by tests in the gobbler-client library.
    
    As a bonus, this commit also adds si-rabbitmq testin to protect against
    regressions. It's minimal for now and may remain that way.
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

--- lib/gobbler-server/src/lib.rs
+++ lib/gobbler-server/src/lib.rs
@@ -1,25 +1,62 @@
+//! This crate provides the gobbler [`Server`].
+
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true,
+    clippy::missing_panics_doc
+)]
+
 mod config;
-pub mod server;
+mod server;
 
-pub use crate::{
-    config::{
-        detect_and_configure_development, Config, ConfigBuilder, ConfigError, ConfigFile,
-        StandardConfig, StandardConfigFile,
-    },
-    server::{Server, ServerError},
-};
+pub use config::Config;
+pub use config::ConfigBuilder;
+pub use config::ConfigError;
+pub use config::ConfigFile;
+pub use server::Server;
+pub use si_settings::StandardConfig;
+pub use si_settings::StandardConfigFile;
 
-const NATS_JOBS_DEFAULT_SUBJECT: &str = "gobbler-jobs";
-const NATS_JOBS_DEFAULT_QUEUE: &str = "gobbler";
+use serde::{Deserialize, Serialize};
+use ulid::Ulid;
 
-pub fn nats_jobs_subject(prefix: Option<&str>) -> String {
-    nats_subject(prefix, NATS_JOBS_DEFAULT_SUBJECT)
+/// Stream to manage gobbler consumer loops.
+pub const GOBBLER_MANAGEMENT_STREAM: &str = "gobbler-management";
+
+/// Stream prefix for gobbler consumer loops.
+pub const GOBBLER_STREAM_PREFIX: &str = "gobbler";
+
+/// The action for the gobbler management loop.
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ManagementMessageAction {
+    /// Close the inner gobbler loop for a change set. If it has already been closed, this is a
+    /// no-op.
+    Close,
+    /// Open the inner gobbler loop for a change set. If one already exists, it is a no-op.
+    Open,
 }
 
-pub fn nats_subject(prefix: Option<&str>, suffix: impl AsRef<str>) -> String {
-    let suffix = suffix.as_ref();
-    match prefix {
-        Some(prefix) => format!("{prefix}.{suffix}"),
-        None => suffix.to_string(),
-    }
+/// The message that the gobbler management consumer expects in the server.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ManagementMessage {
+    /// The ID of the change set wishing to be operated on.
+    pub change_set_id: Ulid,
+    /// The action to instruct the management loop to perform.
+    pub action: ManagementMessageAction,
 }
---
+++ lib/si-rabbitmq/src/delivery.rs
@@ -0,0 +1,37 @@
+use rabbitmq_stream_client::types::Delivery as UpstreamDelivery;
+use serde_json::Value;
+
+use crate::RabbitError;
+
+/// This type is a deconstruction of the upstream
+/// [`Delivery`](rabbitmq_stream_client::types::Delivery) type.
+#[derive(Debug)]
+pub struct Delivery {
+    /// The contents of the message.
+    pub message_contents: Option<Value>,
+    /// The contents of the "reply_to" field from the message properties.
+    pub reply_to: Option<String>,
+}
+
+impl TryFrom<UpstreamDelivery> for Delivery {
+    type Error = RabbitError;
+
+    fn try_from(value: UpstreamDelivery) -> Result<Self, Self::Error> {
+        let message = value.message();
+
+        let contents: Option<Value> = match message.data() {
+            Some(data) => serde_json::from_slice(data)?,
+            None => None,
+        };
+
+        let reply_to = match message.properties() {
+            Some(properties) => properties.reply_to.clone(),
+            None => None,
+        };
+
+        Ok(Self {
+            message_contents: contents,
+            reply_to,
+        })
+    }
+}
