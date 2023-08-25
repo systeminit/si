68ef3791b57c5f5c09bf6804a71e345c9d22d721
wendybujalski <wendydbujalski@gmail.com>
2023-08-25 17:41:21 -0400

    feat(web) - work on secrets, popover component

---
+++ app/web/src/components/AddSecretForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="overflow-y-auto flex flex-col gap-sm w-full h-full p-sm">
+    <VormInput v-model="secretFormData.name" type="text" label="Name">
+      <template #instructions>
+        <div class="text-neutral-700 dark:text-neutral-400 italic">
+          The display name for this secret within System Initiative
+        </div>
+      </template>
+    </VormInput>
+    <VormInput
+      v-model="secretFormData.description"
+      type="textarea"
+      label="Description"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { VormInput } from "@si/vue-lib/design-system";
+import { reactive } from "vue";
+
+const secretFormData = reactive({
+  name: "",
+  description: "",
+});
+</script>
