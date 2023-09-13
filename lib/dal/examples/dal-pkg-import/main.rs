d9cc90dd1b55e7ce30753a63e741821017a33f94
Zachary Hamm <zack@systeminit.com>
2023-09-13 09:24:36 -0500

    fix(dal): special case si:resourcePayloadToValue
    Because new exports will have a different hash with the added values to
    support workspaces, si:resourcePayloadToValue should temporarily be
    treated like an intrinsic on import and only imported once.

--- lib/dal/examples/dal-pkg-import/main.rs
+++ lib/dal/examples/dal-pkg-import/main.rs
@@ -1,12 +1,14 @@
 use std::{env, path::Path, sync::Arc};
 
 use buck2_resources::Buck2Resources;
+use dal::generate_unique_id;
 use dal::{
-    pkg::import_pkg, DalContext, JobQueueProcessor, NatsProcessor, ServicesContext, Tenancy,
-    Workspace,
+    pkg::import_pkg_from_pkg, ChangeSet, DalContext, JobQueueProcessor, NatsProcessor,
+    ServicesContext, Tenancy, Workspace,
 };
 use si_data_nats::{NatsClient, NatsConfig};
 use si_data_pg::{PgPool, PgPoolConfig};
+use si_pkg::SiPkg;
 use veritech_client::{Client as VeritechClient, EncryptionKey};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + 'static>>;
@@ -17,11 +19,30 @@ async fn main() -> Result<()> {
     let tar_file = args.nth(1).expect("usage: program <PKG_FILE>");
 
     let mut ctx = ctx().await?;
-    let workspace = Workspace::builtin(&ctx).await?;
+
+    let workspace = match Workspace::find_first_user_workspace(&ctx).await? {
+        Some(workspace) => workspace,
+        None => Workspace::builtin(&ctx).await?,
+    };
+
     ctx.update_tenancy(Tenancy::new(*workspace.pk()));
 
-    println!("--- Importing pkg: {tar_file}");
-    import_pkg(&ctx, Path::new(&tar_file)).await?;
+    let pkg = SiPkg::load_from_file(Path::new(&tar_file)).await?;
+    let metadata = pkg.metadata()?;
+    let change_set_name = format!(
+        "pkg - {} ({}) {}",
+        metadata.name(),
+        metadata.version(),
+        generate_unique_id(4)
+    );
+    let change_set = ChangeSet::new(&ctx, &change_set_name, None).await?;
+    let ctx = ctx.clone_with_new_visibility(ctx.visibility().to_change_set(change_set.pk));
+
+    println!(
+        "--- Importing pkg: {tar_file} into change set \"{change_set_name}\" in workspace \"{}\"",
+        workspace.name()
+    );
+    import_pkg_from_pkg(&ctx, &pkg, &tar_file, None).await?;
 
     println!("--- Committing database transaction");
     ctx.commit().await?;
