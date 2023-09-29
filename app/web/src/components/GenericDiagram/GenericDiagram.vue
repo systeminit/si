684f17578439d1d5875bde1e207ee8cd7c803111
wendybujalski <wendydbujalski@gmail.com>
2023-09-29 14:44:47 -0400

    feat(web) - GenericDiagram hotkeys for zoom

--- app/web/src/components/GenericDiagram/GenericDiagram.vue
+++ app/web/src/components/GenericDiagram/GenericDiagram.vue
@@ -461,6 +461,14 @@ function onKeyDown(e: KeyboardEvent) {
   // handle opening the help modal
   if (e.key === "?" || e.key === "/") helpModalRef.value?.open();
 
+  // handle zoom hotkeys
+  if (e.key === "=" || e.key === "+") {
+    setZoom(zoomLevel.value + 0.1);
+  }
+  if (e.key === "-" || e.key === "_") {
+    setZoom(zoomLevel.value - 0.1);
+  }
+
   // handle arrow keys - nudge and alignment
   if (!props.readOnly && e.key.startsWith("Arrow")) {
     const direction = convertArrowKeyToDirection(e.key);
