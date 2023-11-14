9cdd21e093fe05bb7afe3b9633faa9e5037bc26c
John Watson <john@familywatson.co.uk>
2023-11-14 19:40:36 +0000

    chore: vsock lib for cyclone-server

---
+++ lib/cyclone-server/src/vsock.rs
@@ -0,0 +1,58 @@
+use std::{
+    path::{Path, PathBuf},
+    task::{Context, Poll},
+};
+
+use futures::ready;
+use hyper::server::accept::Accept;
+use thiserror::Error;
+use tokio::{
+    fs,
+    net::{UnixListener, UnixStream},
+};
+
+use vsock::{VsockAddr,VsockListener,VsockStream};
+
+#[remain::sorted]
+#[derive(Debug, Error)]
+pub enum VsockIncomingStreamError {
+
+    #[error("failed to bind to vsock: {1}")]
+    Bind(#[source] std::io::Error, VsockAddr),
+
+    //#[error("failed to create parent path for unix domain socket")]
+    // CreateParentPath(#[source] std::io::Error),
+    //#[error("IO error")]
+    //IO(#[from] std::io::Error),
+    //#[error("parent path not found for unix domain socket: {0}")]
+    //ParentPathNotFound(PathBuf),
+}
+
+type Result<T> = std::result::Result<T, VsockIncomingStreamError>;
+
+pub struct VsockIncomingStream {
+    vsock: VsockListener,
+}
+
+// Change this to Port, so that Vsock can pick up the port and translate onto the host v.sock
+impl VsockIncomingStream {
+    pub async fn create(vSockAddr: VsockAddr) -> Result<Self> {
+                                      //addr: &impl SockaddrLike
+        let vsock = VsockListener::bind(vSockAddr)
+            .map_err(|err| VsockIncomingStreamError::Bind(err, vSockAddr))?;
+
+        Ok(Self { vsock })
+    }
+}
+
+impl Accept for VsockIncomingStream {
+    type Conn = VsockStream;
+    type Error = VsockIncomingStreamError;
+
+    fn poll_accept(
+        self: std::pin::Pin<&mut Self>
+    ) -> Poll<Option<Result<Self::Conn>>> {
+        let (stream, _addr) = ready!(self.vsock.accept())?;
+        Poll::Ready(Some(Ok(stream)))
+    }
+}
