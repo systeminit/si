12a8d777df5828fd6c58e032a914e6ccf254c52e
wendybujalski <wendydbujalski@gmail.com>
2023-12-05 14:09:21 -0500

    fix(web) - debug panel font size

--- app/web/src/components/DebugViewItem.vue
+++ app/web/src/components/DebugViewItem.vue
@@ -4,7 +4,7 @@
       <template v-if="title">{{ title }}</template>
       <slot v-else name="title" />
     </dt>
-    <dd class="p-2 my-2 border-2 border-opacity-10 overflow-x-auto">
+    <dd class="p-2 my-2 border-2 border-opacity-10 overflow-x-auto text-xs">
       <pre><template v-if="data">{{ data }}</template><slot v-else name="data" /></pre>
     </dd>
   </div>
@@ -14,7 +14,11 @@
 import { PropType } from "vue";
 
 defineProps({
-  data: { type: Object as PropType<object | string | number | boolean | null> },
+  data: {
+    type: [Object, String, Number, Boolean, null] as PropType<
+      object | string | number | boolean | null
+    >,
+  },
   title: { type: String },
 });
 </script>
