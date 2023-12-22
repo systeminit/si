858d4c7db57ffeb81b879f82ea5cf735dcb36c96
Theo Ephraim <theo@systeminit.com>
2023-12-22 14:40:11 -0800

    feat(sdf) open new changeset if adding action on head

--- lib/sdf-server/src/server/service/change_set/add_action.rs
+++ lib/sdf-server/src/server/service/change_set/add_action.rs
@@ -2,7 +2,8 @@ use super::ChangeSetResult;
 use crate::server::extract::{AccessBuilder, HandlerContext, PosthogClient};
 use crate::server::tracking::track;
 use axum::extract::{Json, OriginalUri};
-use dal::{Action, ActionPrototypeId, ComponentId, StandardModel, Visibility, WsEvent};
+use axum::response::IntoResponse;
+use dal::{Action, ActionPrototypeId, ChangeSet, ComponentId, StandardModel, Visibility, WsEvent};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -20,8 +21,24 @@ pub async fn add_action(
     HandlerContext(builder): HandlerContext,
     AccessBuilder(request_ctx): AccessBuilder,
     Json(request): Json<AddActionRequest>,
-) -> ChangeSetResult<Json<()>> {
-    let ctx = builder.build(request_ctx.build(request.visibility)).await?;
+) -> ChangeSetResult<impl IntoResponse> {
+    let mut ctx = builder.build(request_ctx.build(request.visibility)).await?;
+
+    let mut force_changeset_pk = None;
+    if ctx.visibility().is_head() {
+        let change_set = ChangeSet::new(&ctx, ChangeSet::generate_name(), None).await?;
+
+        let new_visibility = Visibility::new(change_set.pk, request.visibility.deleted_at);
+
+        ctx.update_visibility(new_visibility);
+
+        force_changeset_pk = Some(change_set.pk);
+
+        WsEvent::change_set_created(&ctx, change_set.pk)
+            .await?
+            .publish_on_commit(&ctx)
+            .await?;
+    };
 
     let action = Action::new(&ctx, request.prototype_id, request.component_id).await?;
     let prototype = action.prototype(&ctx).await?;
@@ -49,5 +66,9 @@ pub async fn add_action(
 
     ctx.commit().await?;
 
-    Ok(Json(()))
+    let mut response = axum::response::Response::builder();
+    if let Some(force_changeset_pk) = force_changeset_pk {
+        response = response.header("force_changeset_pk", force_changeset_pk.to_string());
+    }
+    Ok(response.body(axum::body::Empty::new())?)
 }
