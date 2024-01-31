c9c14fc2a2a1d0d75d13a8c8aa3e861f3d5c1ab4
Scott Prutton <scott@systeminit.com>
2024-01-31 11:31:54 -0500

    feat: add vector for veritech logs

--- bin/veritech/scripts/userdata.sh
+++ bin/veritech/scripts/userdata.sh
@@ -38,31 +38,57 @@ function create_md_array {
   echo $((30*1024)) > /proc/sys/dev/raid/speed_limit_min
 }
 
-# create raid array from local volumes
+POOL_SIZE=${1:-500}
+NATS=${2:-tls://connect.ngs.global}
+CYCLONE_ENCRYPTION_KEY_SECRET=${3:-tools-encryption-key}
+NATS_CREDS_SECRET=${4:-tools-prod-nats-creds}
+HONEYCOMB_API_SECRET=${5:-tools-honeycomb-api-key}
+
+# create raid array from local volumes for veritech
 VOLUME="/dev/md0"
 create_md_array
 mkfs -t xfs -f $VOLUME
 mkdir -p /srv/jailer
 mount $VOLUME /srv/jailer
 
-###################################################################
-################       Application Start           ################
-###################################################################
-
-POOL_SIZE=${1:-500}
-NATS=${2:-tls://connect.ngs.global}
-CYCLONE_ENCRYPTION_KEY_SECRET=${3:-tools-encryption-key}
-NATS_CREDS_SECRET=${3:-tools-prod-nats-creds}
-HONEYCOMB_API_SECRET=${4:-tools-honeycomb-api-key}
-
+# install latest app
 wget https://artifacts.systeminit.com/veritech/stable/omnibus/linux/$(arch)/veritech-stable-omnibus-linux-$(arch).tar.gz -O - | tar -xzvf - -C /
 
-# Awkward install of the aws cli
+# add vector repo
+wget https://repositories.timber.io/public/vector/gpg.3543DB2D0A2BC4B8.key -O - | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/timber-vector.list
+deb https://repositories.timber.io/public/vector/deb/ubuntu focal main
+deb-src https://repositories.timber.io/public/vector/deb/ubuntu focal main
+EOF
+
+# install deps
 sudo apt update
-sudo apt install unzip jq -y
+sudo DEBIAN_FRONTEND=noninteractive apt install unzip jq vector -y
+
+# Awkward install of the aws cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# configure vector
+cat <<EOF > /etc/vector/vector.yaml
+sources:
+  veritech-journal:
+    type: "journald"
+    include_units: ["veritech"]
+
+sinks:
+  cloudwatch:
+    type: "aws_cloudwatch_logs"
+    inputs: ["veritech-journal"]
+    compression: "gzip"
+    encoding:
+      codec: "json"
+    region: "us-east-1"
+    group_name: "/ec2/tools-veritech"
+    stream_name: "{{ host }}"
+EOF
+systemctl restart vector
 
 # create a volume for our friend the decryption key
 KEY_VOLUME=/firecracker-data/decrypt_key.ext4
@@ -76,6 +102,7 @@ aws secretsmanager get-secret-value --region us-east-1 --secret-id $CYCLONE_ENCR
 chmod 777 $KEY_MOUNT/decryption.key
 umount $KEY_VOLUME
 
+# get nats creds
 aws secretsmanager get-secret-value --region us-east-1 --secret-id $NATS_CREDS_SECRET | jq -r '.SecretString' >> /tmp/nats-creds
 
 # Install + run docker with otel on 4317 on the host interface
@@ -88,6 +115,7 @@ docker run \
  -p 4317:4317 \
  -d systeminit/otelcol:stable
 
+# create and start systemd service
 cat << EOF > /etc/systemd/system/veritech.service
 
 [Unit]
