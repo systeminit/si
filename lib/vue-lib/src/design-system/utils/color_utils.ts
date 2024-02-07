88c065bfaaa33826165554a89b710248bacc94f1
wendybujalski <wendydbujalski@gmail.com>
2024-02-06 19:02:46 -0500

    feat(web) - more compact AssetPalette

--- lib/vue-lib/src/design-system/utils/color_utils.ts
+++ lib/vue-lib/src/design-system/utils/color_utils.ts
@@ -9,42 +9,54 @@ const TONES = {
   action: {
     colorHex: COLOR_PALETTE.action[500],
     bgColorClass: tw`bg-action-500`,
-    textColorClass: tw`text-action-500`,
+    textColorClassLight: tw`text-action-500`,
+    textColorClassDark: tw`text-action-300`,
+    borderColorClassLight: tw`border-action-500`,
+    borderColorClassDark: tw`border-action-300`,
   },
   info: {
     colorHex: COLOR_PALETTE.action[500],
     bgColorClass: tw`bg-action-500`,
-    textColorClass: tw`text-action-500`,
+    textColorClassLight: tw`text-action-500`,
+    textColorClassDark: tw`text-action-300`,
+    borderColorClassLight: tw`border-action-500`,
+    borderColorClassDark: tw`border-action-300`,
   },
   destructive: {
     colorHex: COLOR_PALETTE.destructive[500],
     bgColorClass: tw`bg-destructive-500`,
     textColorClass: tw`text-destructive-500`,
+    borderColorClass: tw`border-destructive-500`,
   },
   error: {
     colorHex: COLOR_PALETTE.destructive[500],
     bgColorClass: tw`bg-destructive-500`,
     textColorClass: tw`text-destructive-500`,
+    borderColorClass: tw`border-destructive-500`,
   },
   success: {
     colorHex: COLOR_PALETTE.success[500],
     bgColorClass: tw`bg-success-500`,
     textColorClass: tw`text-success-500`,
+    borderColorClass: tw`border-success-500`,
   },
   warning: {
     colorHex: COLOR_PALETTE.warning[500],
     bgColorClass: tw`bg-warning-500`,
     textColorClass: tw`text-warning-500`,
+    borderColorClass: tw`border-warning-500`,
   },
   neutral: {
     colorHex: COLOR_PALETTE.neutral[500],
     bgColorClass: tw`bg-neutral-500`,
     textColorClass: tw`text-neutral-500`,
+    borderColorClass: tw`border-neutral-500`,
   },
   empty: {
     colorHex: "#00000000",
-    bgColorClass: tw`border-transparent`,
+    bgColorClass: tw`bg-transparent`,
     textColorClass: tw`text-transparent border-transparent bg-transparent border-0 border-hidden opacity-0`,
+    borderColorClass: tw`border-transparent`,
   },
   shade: {
     colorHexLight: COLOR_PALETTE.shade[0],
@@ -53,6 +65,8 @@ const TONES = {
     bgColorClassDark: tw`bg-shade-100`,
     textColorClassLight: tw`text-shade-100`,
     textColorClassDark: tw`text-shade-0`,
+    borderColorClassLight: tw`bg-shade-0`,
+    borderColorClassDark: tw`bg-shade-100`,
   },
 };
 
@@ -74,6 +88,15 @@ export function getToneTextColorClass(tone: Tones) {
   if (theme.value === "dark")
     return toneSettings.textColorClassDark || toneSettings.textColorClass;
   return toneSettings.textColorClassLight || toneSettings.textColorClass;
+}
+
+export function getToneBorderColorClass(tone: Tones) {
+  const { theme } = useTheme();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const toneSettings = TONES[tone] as any;
+  if (theme.value === "dark")
+    return toneSettings.borderColorClassDark || toneSettings.borderColorClass;
+  return toneSettings.borderColorClassLight || toneSettings.borderColorClass;
 }
 
 export function getToneColorHex(tone: Tones) {
