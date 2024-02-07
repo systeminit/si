af6490dead608405828a36e597fe0d4f65725b05
Scott Prutton <scott@systeminit.com>
2024-02-07 12:15:58 -0500

    fix: jail clean flakiness

--- bin/veritech/scripts/stop.sh
+++ bin/veritech/scripts/stop.sh
@@ -4,33 +4,23 @@ set -euo pipefail
 
 SB_ID="${1:-null}"
 
-JAILER_BINARY="/usr/bin/jailer"
-TAP_DEV="fc-${SB_ID}-tap0"
-
-# Kill the firecracker process
-ps aux | grep "firecracke[r] --id $SB_ID" | awk '{ print $2 }' | xargs kill -9 || true
-
-# Remove TAP device
-ip link del "$TAP_DEV" 2> /dev/null || true
-
-# Remove veth devices
-ip link del veth-main$SB_ID 2> /dev/null || true
-ip link del veth-jailer$SB_ID 2> /dev/null || true
-
-# Remove iptables rules
-ip netns exec jailer-$SB_ID iptables -t nat -D POSTROUTING -o veth-jailer$SB_ID -j MASQUERADE || true
-ip netns exec jailer-$SB_ID iptables -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT || true
-ip netns exec jailer-$SB_ID iptables -D FORWARD -i fc-$SB_ID-tap0 -o veth-jailer$SB_ID -j ACCEPT || true
-
-# Remove network namespace
-ip netns del jailer-$SB_ID || true
-
-# Remove user and group
-userdel jailer-$SB_ID || true
+# Kill the firecracker process if it exists
+FIRECRACKER_PID=$(pgrep -f "firecracker --id $SB_ID") || true
+if [ -n "${FIRECRACKER_PID}" ]; then
+  kill -9 $FIRECRACKER_PID
+fi
 
 # Remove directories and files
-umount /srv/jailer/firecracker/$SB_ID/root/rootfs.ext4 || true
-dmsetup remove --retry rootfs-overlay-$SB_ID
+DISK="/srv/jailer/firecracker/$SB_ID/root/rootfs.ext4"
+OVERLAY="rootfs-overlay-$SB_ID"
+
+until ! $(mount | grep -q $DISK); do
+  umount -dl "$DISK"
+done
+
+until ! dmsetup status "$OVERLAY" &> /dev/null; do
+  dmsetup remove --force --retry $OVERLAY
+done
 
 # We are not currently creating these.
 # umount /srv/jailer/firecracker/$SB_ID/root/image-kernel.bin || true
