a8152fa25b4b7c01cf82acdc0cf0e6ed16aae03b
John Watson <john@familywatson.co.uk>
2024-02-15 16:25:27 +0000

    Sometimes changeset prompt isn't there?

--- app/web/cypress/e2e/2-login/logout.cy.ts
+++ app/web/cypress/e2e/2-login/logout.cy.ts
@@ -6,7 +6,7 @@ describe("Logout", () => {
 
   it("lets the user log out", () => {
     cy.visit("/");
-    cy.contains('Create change set', { timeout: 10000 }).should('be.visible').click();
+    //cy.contains('Create change set', { timeout: 10000 }).should('be.visible').click();
     cy.get('[aria-label="Profile"]').should('exist').click();
     cy.get('#dropdown-menu-item-1').should('exist').should('be.visible').click({ force: true });
     cy.url().should("contain", import.meta.env.VITE_AUTH_PORTAL_URL + '/logout');
