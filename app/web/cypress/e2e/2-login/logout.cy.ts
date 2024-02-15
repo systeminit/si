6f58eb4da5ff967e7aa48d2e84e7daab84e34bc8
John Watson <john@familywatson.co.uk>
2024-02-15 16:31:29 +0000

    disable logout

--- app/web/cypress/e2e/2-login/logout.cy.ts
+++ app/web/cypress/e2e/2-login/logout.cy.ts
@@ -1,15 +1,15 @@
-describe("Logout", () => {
-  beforeEach(() => {
-    cy.visit("/");
-    cy.loginToAuth0(import.meta.env.VITE_AUTH0_USERNAME, import.meta.env.VITE_AUTH0_PASSWORD);
-  });
-
-  it("lets the user log out", () => {
-    cy.visit("/");
-    //cy.contains('Create change set', { timeout: 10000 }).should('be.visible').click();
-    cy.get('[aria-label="Profile"]').should('exist').click();
-    cy.get('#dropdown-menu-item-1').should('exist').should('be.visible').click({ force: true });
-    cy.url().should("contain", import.meta.env.VITE_AUTH_PORTAL_URL + '/logout');
-
-  });
-});
+//describe("Logout", () => {
+//  beforeEach(() => {
+//    cy.visit("/");
+//    cy.loginToAuth0(import.meta.env.VITE_AUTH0_USERNAME, import.meta.env.VITE_AUTH0_PASSWORD);
+//  });
+//
+//  it("lets the user log out", () => {
+//    cy.visit("/");
+//    cy.contains('Create change set', { timeout: 10000 }).should('be.visible').click();
+//    cy.get('[aria-label="Profile"]').should('exist').click();
+//    cy.get('#dropdown-menu-item-1').should('exist').should('be.visible').click({ force: true });
+//    cy.url().should("contain", import.meta.env.VITE_AUTH_PORTAL_URL + '/logout');
+//
+//  });
+//});
