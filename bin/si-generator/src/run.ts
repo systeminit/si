e821d4200379594ad95070353989e6075ef28e23
Adam Jacob <adam@systeminit.com>
2024-02-16 12:43:07 -0800

    feat(si-generator): add codegen and create funcs
    The generator can now produce codeGen and createFuncs.
    
    In theory, there should only need to be 1 codeGen func created, and it
    should just be re-used across every asset. It should be named
    'si:genericAwsCreate', which will then be re-used in the create
    functions.

--- bin/si-generator/src/run.ts
+++ bin/si-generator/src/run.ts
@@ -1,6 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 import { awsGenerate } from "./asset_generator.ts";
-import { renderAsset } from "./render.ts";
+import { renderAsset, renderCodeGen, renderCreate } from "./render.ts";
 
 export async function run() {
   const command = new Command()
@@ -20,6 +20,20 @@ export async function run() {
       const props = awsGenerate(awsService, awsCommand);
       const result = await renderAsset(props, "aws");
       console.log(result);
+    })
+    .command("codeGen")
+    .description("generate a codeGen definition for an aws asset to create")
+    .action(async (_options) => {
+      const result = await renderCodeGen("aws");
+      console.log(result);
+    })
+    .command("create")
+    .description("generate a create function from an aws cli skeleton")
+    .arguments("<awsService:string> <awsCommand:string>")
+    .action(async (_options, awsService, awsCommand) => {
+      const result = await renderCreate({ provider: "aws", awsService, awsCommand });
+      console.log(result);
     });
+
   const _result = await command.parse(Deno.args);
 }
