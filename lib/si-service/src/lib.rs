1b183beed943752156669c6c54040051cd6c809c
Fletcher Nichol <fletcher@systeminit.com>
2024-02-26 12:36:18 -0700

    feat: create `si-service` crate with generic graceful shutdown
    This change adds a `lib/si-service` crate which is intended to contain
    common server/service-related behavior, when creating a service in
    following the SI engineering patterns. Currently the only added behavior
    is a graceful shutdown function which provides signal-handling logic,
    optional timeout duration, and proper telemetry shutdown logic.
    
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

---
+++ lib/si-service/src/lib.rs
@@ -0,0 +1,19 @@
+//! System Initiative common service/server support.
+
+#![warn(
+    clippy::unwrap_in_result,
+    clippy::indexing_slicing,
+    clippy::arithmetic_side_effects,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::missing_panics_doc,
+    clippy::panic_in_result_fn,
+    missing_docs
+)]
+#![allow(
+    clippy::missing_errors_doc,
+    clippy::module_inception,
+    clippy::module_name_repetitions
+)]
+
+pub mod shutdown;
