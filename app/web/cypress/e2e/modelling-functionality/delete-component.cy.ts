84f00c43799b3ee133f8cd01a937f1fae74bc110
John Watson <john@familywatson.co.uk>
2024-02-28 10:43:48 +0000

    fix(cypress): amend create component to be working with new UI

--- app/web/cypress/e2e/modelling-functionality/delete-component.cy.ts
+++ app/web/cypress/e2e/modelling-functionality/delete-component.cy.ts
@@ -22,7 +22,7 @@ Cypress._.times(import.meta.env.VITE_SI_CYPRESS_MULTIPLIER ? import.meta.env.VIT
       cy.url().should('not.include', 'head', { timeout: 10000 });
 
       // Find the AWS Credential
-      cy.get('[data-cy="asset_card', { timeout: 30000 }).contains('AWS Credential', { timeout: 30000 }).should('be.visible').as('awsCred')
+      cy.get('div[class="tree-node"]', { timeout: 30000 }).contains('AWS Credential').should('be.visible').as('awsCred')
 
       // Find the canvas to get a location to drag to
       cy.get('canvas').first().as('konvaStage');
