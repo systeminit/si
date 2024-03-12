83857b7c333121f10ea8fdbbb0933d2b54213c59
Nick Gerace <nick@systeminit.com>
2024-03-12 16:34:43 -0400

    Remove references and usages of "providers" in app/web
    This commit removes references and usages of "providers" in app/web
    everywhere except for function authoring. That's because we need further
    changes to fully restore the attribute prototype parts of the function
    authoring and attachment experience.
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

--- app/web/src/api/sdf/dal/attribute.ts
+++
@@ -1,6 +0,0 @@
-export interface AttributeContext {
-  attribute_context_prop_id: string;
-  attribute_context_internal_provider_id: string;
-  attribute_context_external_provider_id: string;
-  attribute_context_component_id: string;
-}
--- app/web/src/api/sdf/dal/diagram.ts
+++ app/web/src/api/sdf/dal/diagram.ts
@@ -4,28 +4,14 @@ export type DiagramKind = "configuration";
 
 export type DiagramProviderMetadata = string;
 
-export interface DiagramOutputProvider {
-  id: string;
-  ty: DiagramProviderMetadata;
-}
-
 export interface DiagramOutputSocket {
   id: string;
   name: string;
-  diagramKind: DiagramKind;
-  provider: DiagramOutputProvider;
-}
-
-export interface DiagramInputProvider {
-  id: string;
-  ty: DiagramProviderMetadata;
 }
 
 export interface DiagramInputSocket {
   id: string;
   name: string;
-  diagramKind: DiagramKind;
-  provider: DiagramInputProvider;
 }
 
 export interface DiagramSchemaVariant {
