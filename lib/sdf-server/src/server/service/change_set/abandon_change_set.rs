8b79100289960b1f4be2b6077b985f79fe3b1b5b
stack72 <public@paulstack.co.uk>
2024-03-13 20:24:56 +0000

    feat(dal, sdf-server): Restore Abandon ChangeSet Route

--- lib/sdf-server/src/server/service/change_set/abandon_change_set.rs
+++ lib/sdf-server/src/server/service/change_set/abandon_change_set.rs
@@ -4,7 +4,8 @@ use crate::server::service::change_set::ChangeSetError;
 use crate::server::tracking::track;
 use axum::extract::OriginalUri;
 use axum::Json;
-use dal::{ChangeSet, ChangeSetPk, HistoryActor, User, WsEvent};
+use dal::change_set_pointer::ChangeSetPointer;
+use dal::{ChangeSetPk, ChangeSetStatus};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -16,7 +17,7 @@ pub struct AbandonChangeSetRequest {
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct AbandonChangeSetResponse {
-    pub change_set: ChangeSet,
+    pub change_set: ChangeSetPointer,
 }
 
 pub async fn abandon_change_set(
@@ -25,13 +26,17 @@ pub async fn abandon_change_set(
     PosthogClient(posthog_client): PosthogClient,
     OriginalUri(original_uri): OriginalUri,
     Json(request): Json<AbandonChangeSetRequest>,
-) -> ChangeSetResult<Json<AbandonChangeSetResponse>> {
+) -> ChangeSetResult<()> {
     let mut ctx = builder.build_head(access_builder).await?;
 
-    let mut change_set = ChangeSet::get_by_pk(&ctx, &request.change_set_pk)
+    let mut change_set = ChangeSetPointer::find(&ctx, request.change_set_pk.into())
         .await?
         .ok_or(ChangeSetError::ChangeSetNotFound)?;
-    change_set.abandon(&mut ctx).await?;
+    ctx.update_visibility_and_snapshot_to_visibility_no_editing_change_set(&change_set)
+        .await?;
+    change_set
+        .update_status(&ctx, ChangeSetStatus::Abandoned)
+        .await?;
 
     track(
         &posthog_client,
@@ -43,22 +48,20 @@ pub async fn abandon_change_set(
         }),
     );
 
-    ctx.blocking_commit().await?;
+    // let user = match ctx.history_actor() {
+    //     HistoryActor::User(user_pk) => User::get_by_pk(&ctx, *user_pk)
+    //         .await?
+    //         .ok_or(ChangeSetError::InvalidUser(*user_pk))?,
+    //
+    //     HistoryActor::SystemInit => return Err(ChangeSetError::InvalidUserSystemInit),
+    // };
 
-    let user = match ctx.history_actor() {
-        HistoryActor::User(user_pk) => User::get_by_pk(&ctx, *user_pk)
-            .await?
-            .ok_or(ChangeSetError::InvalidUser(*user_pk))?,
+    // WsEvent::change_set_abandoned(&ctx, change_set.changeset_pk(), Some(user.pk()))
+    //     .await?
+    //     .publish_on_commit(&ctx)
+    //     .await?;
 
-        HistoryActor::SystemInit => return Err(ChangeSetError::InvalidUserSystemInit),
-    };
+    ctx.commit_no_rebase().await?;
 
-    WsEvent::change_set_abandoned(&ctx, change_set.pk, Some(user.pk()))
-        .await?
-        .publish_on_commit(&ctx)
-        .await?;
-
-    ctx.commit().await?;
-
-    Ok(Json(AbandonChangeSetResponse { change_set }))
+    Ok(())
 }
