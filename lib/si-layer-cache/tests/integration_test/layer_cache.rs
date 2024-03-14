c07c6b1ff93fa620ed349c0df812798e8c1e2a8b
Zachary Hamm <zack@systeminit.com>
2024-03-14 16:37:45 -0500

    chore(si-layer-cache): add randomness to get_bulk test

--- lib/si-layer-cache/tests/integration_test/layer_cache.rs
+++ lib/si-layer-cache/tests/integration_test/layer_cache.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use si_layer_cache::layer_cache::LayerCache;
 
 async fn make_layer_cache(db_name: &str) -> LayerCache<&'static str, String> {
@@ -109,7 +111,7 @@ async fn get_bulk_inserts_to_memory() {
 async fn get_bulk_from_db() {
     let layer_cache = make_layer_cache("get_bulk").await;
 
-    let values = [
+    let mut values = [
         "skid row",
         "kid scrow",
         "march for macragge",
@@ -117,18 +119,23 @@ async fn get_bulk_from_db() {
         "steppa pig",
     ];
 
-    for value in values {
-        let _ = layer_cache.pg().insert(value, "cas", value.as_ref()).await;
-    }
+    let mut rng = thread_rng();
 
-    let get_values = layer_cache
-        .pg()
-        .get_many(values.as_ref())
-        .await
-        .expect("should get bulk")
-        .expect("should have results");
+    for _i in 0..5 {
+        values.shuffle(&mut rng);
+        for value in values {
+            let _ = layer_cache.pg().insert(value, "cas", value.as_ref()).await;
+        }
 
-    for value in values {
-        assert_eq!(value.as_bytes(), get_values[value]);
+        let get_values = layer_cache
+            .pg()
+            .get_many(values.as_ref())
+            .await
+            .expect("should get bulk")
+            .expect("should have results");
+
+        for value in values {
+            assert_eq!(value.as_bytes(), get_values[value]);
+        }
     }
 }
