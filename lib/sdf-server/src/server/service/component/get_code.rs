626d2f935179cafc486049261607dbfd970c0415
stack72 <public@paulstack.co.uk>
2024-04-11 23:56:19 +0100

    chore(sdf): Restore posthog tracking for component routes

--- lib/sdf-server/src/server/service/component/get_code.rs
+++ lib/sdf-server/src/server/service/component/get_code.rs
@@ -1,10 +1,12 @@
+use axum::extract::OriginalUri;
 use axum::{extract::Query, Json};
 use dal::code_view::CodeView;
 use dal::{Component, ComponentId, Visibility};
 use serde::{Deserialize, Serialize};
 
 use super::ComponentResult;
-use crate::server::extract::{AccessBuilder, HandlerContext};
+use crate::server::extract::{AccessBuilder, HandlerContext, PosthogClient};
+use crate::server::tracking::track;
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -22,6 +24,8 @@ pub struct GetCodeResponse {
 }
 
 pub async fn get_code(
+    OriginalUri(original_uri): OriginalUri,
+    PosthogClient(posthog_client): PosthogClient,
     HandlerContext(builder): HandlerContext,
     AccessBuilder(request_ctx): AccessBuilder,
     Query(request): Query<GetCodeRequest>,
@@ -29,6 +33,20 @@ pub async fn get_code(
     let ctx = builder.build(request_ctx.build(request.visibility)).await?;
 
     let (code_views, has_code) = Component::list_code_generated(&ctx, request.component_id).await?;
+
+    track(
+        &posthog_client,
+        &ctx,
+        &original_uri,
+        "get_code",
+        serde_json::json!({
+            "how": "/component/get_code",
+            "component_id": request.component_id.clone(),
+            "code_views": code_views.clone().len(),
+            "has_code": has_code.clone(),
+            "change_set_id": ctx.change_set_id(),
+        }),
+    );
 
     Ok(Json(GetCodeResponse {
         code_views,
--- lib/sdf-server/src/server/service/component/restore_default_function.rs
+++ lib/sdf-server/src/server/service/component/restore_default_function.rs
@@ -1,11 +1,8 @@
-use axum::{response::IntoResponse, Json};
-use serde::{Deserialize, Serialize};
-
-use dal::{AttributeValue, AttributeValueId, ChangeSet, Visibility};
-
-use crate::server::extract::{AccessBuilder, HandlerContext};
-
 use super::ComponentResult;
+use crate::server::extract::{AccessBuilder, HandlerContext};
+use axum::{response::IntoResponse, Json};
+use dal::{AttributeValue, AttributeValueId, ChangeSet, Visibility};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -25,42 +22,6 @@ pub async fn restore_default_function(
     let force_change_set_id = ChangeSet::force_new(&mut ctx).await?;
 
     AttributeValue::use_default_prototype(&ctx, request.attribute_value_id).await?;
-
-    // Track
-    // {
-    //     let attribute_value = AttributeValue::get_by_id(&ctx, &request.attribute_value_id)
-    //         .await?
-    //         .ok_or_else(|| {
-    //             AttributeValueError::NotFound(request.attribute_value_id, *ctx.visibility())
-    //         })?;
-    //     let component = Component::get_by_id(&ctx, &attribute_value.context.component_id())
-    //         .await?
-    //         .ok_or_else(|| ComponentError::NotFound(attribute_value.context.component_id()))?;
-    //
-    //     let component_schema = component
-    //         .schema(&ctx)
-    //         .await?
-    //         .ok_or(ComponentError::SchemaNotFound)?;
-    //
-    //     let prop = Prop::get_by_id(&ctx, &attribute_value.context.prop_id())
-    //         .await?
-    //         .ok_or(ComponentError::PropNotFound(
-    //             attribute_value.context.prop_id(),
-    //         ))?;
-    //
-    //     track(
-    //         &posthog_client,
-    //         &ctx,
-    //         &original_uri,
-    //         "default_function_restored",
-    //         serde_json::json!({
-    //             "component_id": component.id(),
-    //             "component_schema_name": component_schema.name(),
-    //             "prop_id": prop.id(),
-    //             "prop_name": prop.name(),
-    //         }),
-    //     );
-    // }
 
     ctx.commit().await?;
 
