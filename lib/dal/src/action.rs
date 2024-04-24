d0eb14ecc7ba354a0c8dbd65b265d42d462da53f
Jacob Helwig <jacob@technosorcery.net>
2024-04-24 09:47:58 -0700

    Calculate dependency graph of Actions

--- lib/dal/src/action.rs
+++ lib/dal/src/action.rs
@@ -1,3 +1,4 @@
+use petgraph::prelude::*;
 use serde::{Deserialize, Serialize};
 use si_events::ulid::Ulid;
 use strum::EnumDiscriminants;
@@ -5,14 +6,17 @@ use telemetry::prelude::*;
 use thiserror::Error;
 
 use crate::{
+    action::dependency_graph::ActionDependencyGraph,
     id, implement_add_edge_to,
     workspace_snapshot::node_weight::{
         category_node_weight::CategoryNodeKind, ActionNodeWeight, NodeWeight, NodeWeightError,
     },
-    ChangeSetError, ChangeSetId, ComponentId, DalContext, EdgeWeightError, EdgeWeightKind,
-    EdgeWeightKindDiscriminants, HelperError, TransactionsError, WorkspaceSnapshotError,
+    ChangeSetError, ChangeSetId, ComponentError, ComponentId, DalContext, EdgeWeightError,
+    EdgeWeightKind, EdgeWeightKindDiscriminants, HelperError, TransactionsError,
+    WorkspaceSnapshotError,
 };
 
+pub mod dependency_graph;
 pub mod prototype;
 
 #[remain::sorted]
@@ -20,6 +24,8 @@ pub mod prototype;
 pub enum ActionError {
     #[error("Change Set error: {0}")]
     ChangeSet(#[from] ChangeSetError),
+    #[error("Component error: {0}")]
+    Component(#[from] ComponentError),
     #[error("Edge Weight error: {0}")]
     EdgeWeight(#[from] EdgeWeightError),
     #[error("Helper error: {0}")]
@@ -146,5 +152,83 @@ impl Action {
             .into();
 
         Ok(new_action)
+    }
+
+    /// Sort the dependency graph of [`Actions`][Action] topologically, breaking ties by listing
+    /// [`Actions`][Action] sorted by their ID (oldest first thanks to ULID sorting).
+    pub async fn list_topologically(ctx: &DalContext) -> ActionResult<Vec<ActionId>> {
+        // TODO: Grab all "running" & "failed" Actions to list first?
+        let mut result = Vec::new();
+
+        let mut action_dependency_graph = ActionDependencyGraph::for_workspace(ctx).await?;
+
+        loop {
+            let mut independent_actions = action_dependency_graph.independent_actions();
+            if independent_actions.is_empty() {
+                break;
+            }
+
+            independent_actions.sort();
+            for action_id in independent_actions {
+                action_dependency_graph.remove_action(action_id);
+                result.push(action_id);
+            }
+        }
+
+        // If there is a cycle in the dependencies for some reason, we still want to know that the
+        // actions exist, even though they'll never start executing.
+        let mut actions_in_cycle = action_dependency_graph.remaining_actions();
+        actions_in_cycle.sort();
+        result.extend(&actions_in_cycle);
+
+        Ok(result)
+    }
+
+    pub async fn component_id(
+        ctx: &DalContext,
+        action_id: ActionId,
+    ) -> ActionResult<Option<ComponentId>> {
+        for (_, _tail_node_idx, head_node_idx) in ctx
+            .workspace_snapshot()?
+            .edges_directed_for_edge_weight_kind(
+                action_id,
+                Outgoing,
+                EdgeWeightKindDiscriminants::Use,
+            )
+            .await?
+        {
+            if let NodeWeight::Component(component_node_weight) = ctx
+                .workspace_snapshot()?
+                .get_node_weight(head_node_idx)
+                .await?
+            {
+                return Ok(Some(component_node_weight.id().into()));
+            }
+        }
+
+        Ok(None)
+    }
+
+    pub async fn all_ids(ctx: &DalContext) -> ActionResult<Vec<ActionId>> {
+        let mut result = Vec::new();
+
+        let action_category_node_index = ctx
+            .workspace_snapshot()?
+            .get_category_node(None, CategoryNodeKind::Action)
+            .await?;
+        for (_, _, action_node_index) in ctx
+            .workspace_snapshot()?
+            .edges_directed(action_category_node_index, Outgoing)
+            .await?
+        {
+            let action_id = ctx
+                .workspace_snapshot()?
+                .get_node_weight(action_node_index)
+                .await?
+                .id();
+            result.push(action_id.into());
+        }
+
+        Ok(result)
     }
 }
