b7fdac22e1d52562a9a62173c721410e9b87ed43
stack72 <public@paulstack.co.uk>
2024-05-01 21:41:46 +0100

    fix(dal, sdf): Ensure update_asset works in all scenarios
    In order to support workspace level imports, the import code had a Changeset based thingmap. That thingmap was causing issues in variant export because some funcs were being added to the map with the incorrect changeset id
    
    We no longer use the import_workspace code path so we only ever import a single changeset at a time
    
    We have no rid the world of a Changeset scoped Thingmap and this means the update_variant is a happier world and we can now happily upgrade variants!

--- lib/sdf-server/src/server/service/module/export_module.rs
+++ lib/sdf-server/src/server/service/module/export_module.rs
@@ -83,7 +83,6 @@ pub async fn export_module(
         request.description.as_ref(),
         &created_by_email,
         schema_ids,
-        ctx.get_workspace_default_change_set_id().await?,
     );
 
     let module_payload = exporter.export_as_bytes(&ctx).await?;
