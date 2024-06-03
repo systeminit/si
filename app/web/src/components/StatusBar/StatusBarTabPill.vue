a8bc2dd177d67c68e03be496639a36b3191c99e2
wendybujalski <wendydbujalski@gmail.com>
2024-06-03 18:40:19 -0400

    fix(web) - StatusBar has a fixed height of 40px

--- app/web/src/components/StatusBar/StatusBarTabPill.vue
+++ app/web/src/components/StatusBar/StatusBarTabPill.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex place-content-center items-center rounded-md min-w-fit border px-1 py-px text-xs gap-0.5"
+    class="flex place-content-center items-center rounded-md min-w-fit border px-1 py-px text-xs gap-0.5 truncate"
   >
     <slot />
   </div>
