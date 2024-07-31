2ce6a80489bc6c34338ab1d2352b8af9a0b412e1
Scott Prutton <scott@systeminit.com>
2024-07-31 16:33:56 -0400

    feat: metrics macro and function metric tracking

--- lib/telemetry-utils-rs/src/lib.rs
+++ lib/telemetry-utils-rs/src/lib.rs
@@ -1,6 +1,12 @@
 #[macro_export]
 macro_rules! metric {
-    ($counter:expr, $value:expr) => {
-        info!(metrics = true, "{} = {}", $counter, $value);
+    ($key:ident = $value:expr) => {
+        info!(metrics = true, $key = $value);
+    };
+    ($key:literal = $value:expr) => {
+        info!(metrics = true, $key = $value);
+    };
+    ($($key:ident).+ = $value:expr) => {
+        info!(metrics = true, $($key).+ = $value);
     };
 }
