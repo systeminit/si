4834dd64a607bfb632c2a319fb91e5dfe9ff28cd
Scott Prutton <scott@systeminit.com>
2024-08-26 16:55:14 -0400

    feat: run a one-shot SDF to perform migrations on deploy

--- component/toolbox/scripts/supporting-funcs/ssm-funcs.sh
+++ component/toolbox/scripts/supporting-funcs/ssm-funcs.sh
@@ -6,6 +6,7 @@
 upgrade_check_script="si-check-node-upgrade"
 service_maintenance_script="si-service-maintenance"
 service_state_script="si-service-state"
+sdf_migrate_script="si-migrate-sdf"
 
 # Function to start SSM session
 start_and_track_ssm_session() {
