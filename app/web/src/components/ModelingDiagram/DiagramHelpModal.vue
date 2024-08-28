6b5618171f88f014a9f33406ab1d4b3ebbc82bc9
Mag <i+gh@aopab.art>
2024-08-28 18:12:18 +0200

    add to tooltip

--- app/web/src/components/ModelingDiagram/DiagramHelpModal.vue
+++ app/web/src/components/ModelingDiagram/DiagramHelpModal.vue
@@ -8,7 +8,7 @@
       <li>Option/Alt + Shift - close/open both side panels</li>
       <li>Trackpad pinch - adjust zoom</li>
       <li>Arrow keys - Nudge selection, hold shift to nudge further</li>
-      <li>Spacebar - hold to activate drag-to-scroll</li>
+      <li>Spacebar / v - hold to activate drag-to-scroll</li>
       <li>Esc - clear selection</li>
       <li>Backspace / delete - delete selected component</li>
     </ul>
