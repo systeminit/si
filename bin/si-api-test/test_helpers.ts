af04c64ba40b3d95eab3a013b70f093fdc80a9d2
Scott Prutton <scott@systeminit.com>
2024-09-10 12:24:43 -0400

    fix: add soak markers, console logs

--- bin/si-api-test/test_helpers.ts
+++ bin/si-api-test/test_helpers.ts
@@ -3,30 +3,34 @@ import assert from "node:assert";
 
 export function sleep(ms: number) {
   const natural_ms = Math.max(0, Math.floor(ms));
+  console.log(`Sleeping for ${natural_ms} ms`);
   return new Promise((resolve) => setTimeout(resolve, natural_ms));
 }
 
 // Run fn n times, with increasing intervals between tries
 export async function retryWithBackoff(
   fn: () => Promise<void>,
-  retries = 6,
-  backoffFactor = 2,
+  retries = 60,
+  backoffFactor = 1.2,
   initialDelay = 2, /// in seconds
 ) {
+  const maxDelay = 10;
   let latest_err;
   let try_count = 0;
   let delay = initialDelay;
 
+  console.log("Running retry_with_backoff block");
   do {
     try_count++;
     latest_err = undefined;
+    console.log(`try number ${try_count}`);
 
     try {
       await fn();
     } catch (e) {
       latest_err = e;
       await sleep(delay * 1000);
-      delay = Math.min(delay * backoffFactor, 30);
+      delay = Math.min(delay * backoffFactor, maxDelay);
     }
   } while (latest_err && try_count < retries);
 
