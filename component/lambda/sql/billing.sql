86a6d4880b4fc94f8d1404920c205bfd8389dedc
John Keiser <john@johnkeiser.com>
2024-10-14 14:09:54 -0700

    Replace "track resource counts" lambda with a view.
    This is slower to run (10-11 seconds vs 3), but reports fresher data
    and has fewer moving parts to fail. Performance can be improved later as
    needed with indexes and materialized views.
    
    Also exposes per-workspace resource counts for each
    resource-hour, for diagnostic purposes and so that
    we can (eventually) segment invoices by workspace.

--- component/lambda/sql/billing.sql
+++ component/lambda/sql/billing.sql
@@ -1,16 +1,7 @@
 create external schema spectrum_schema
 from data catalog
 database 'si-prod-data'
-iam_role 'arn:aws:iam::944151301248:role/si-prod-redshift,arn:aws:iam::798856999590:role/si-prod-glue-data-bucket';
-
-CREATE TABLE IF NOT EXISTS workspace_operations.lago_resource_hours_upload_record (
-    lago_organization_id INT NOT NULL,
-    upload_start_time TIMESTAMP NOT NULL,
-    upload_end_time TIMESTAMP NOT NULL,
-    hour_start TIMESTAMP NOT NULL,
-    uploaded_records TIMESTAMP NOT NULL,
-    error TEXT,
-)
+iam_role 'arn:aws:iam::$REDSHIFT_ACCOUNT:role/si-prod-redshift,arn:aws:iam::$S3_ACCOUNT:role/si-prod-glue-data-bucket';
 
 -- Status of workspace_update_events ingestion, including when we consider events to be complete
 CREATE OR REPLACE VIEW workspace_operations.workspace_update_events_summary AS
@@ -25,7 +16,7 @@ CREATE OR REPLACE VIEW workspace_operations.workspace_update_events_summary AS
 
 
 -- The resource counts for each workspace after each time it changes
-CREATE OR REPLACE VIEW workspace_operations.workspace_resource_counts_view AS
+CREATE OR REPLACE VIEW workspace_operations.workspace_resource_counts AS
     WITH
         -- Get only events with actual resource_counts recorded (plus previous resource count for next step)
         workspace_resource_count_events AS (
@@ -35,17 +26,14 @@ CREATE OR REPLACE VIEW workspace_operations.workspace_resource_counts_view AS
         ),
         -- Once we have only events with resource counts, get the ones where it changed (plus next_event_timestamp)
         workspace_resource_count_change_events AS (
-            SELECT *, LEAD(event_timestamp) OVER (PARTITION BY workspace_id ORDER BY event_timestamp)
+            SELECT *, LEAD(event_timestamp) OVER (PARTITION BY workspace_id ORDER BY event_timestamp) AS next_event_timestamp
               FROM workspace_resource_count_events
-             WHERE resource_count != prev_resource_count
+             WHERE resource_count != prev_resource_count OR prev_resource_count IS NULL
         )
-    SELECT workspace_id, event_timestamp, resource_count, next_event_timestamp
+    SELECT workspace_id, event_timestamp::timestamp, resource_count, next_event_timestamp::timestamp
       FROM workspace_resource_count_change_events
     WITH NO SCHEMA BINDING;
 
-
--- Make it so we don't have to type schema prefixes for our tables / views
-SET search_path TO workspace_operations;
 
 -- Generates a list of all hours from Sep. 2024 until now
 CREATE OR REPLACE VIEW workspace_operations.si_hours AS
@@ -59,71 +47,78 @@ CREATE OR REPLACE VIEW workspace_operations.si_hours AS
     )
     SELECT hour_start FROM hour_generator;
 
-
--- The resource counts for an owner after each time it changes
-CREATE OR REPLACE VIEW workspace_operations.owner_resource_counts AS
+-- Get the resource counts for all workspaces anytime they change *and* at least once per hour
+CREATE OR REPLACE VIEW workspace_operations.workspace_hourly_samples AS
     WITH
         -- Get all times at which any workspace for an owner has changed
-        event_times AS (
-            SELECT DISTINCT owner_pk, event_timestamp
-            FROM workspace_resource_counts
-            JOIN workspace_owners USING (workspace_id)
+        workspace_change_events AS (
+            SELECT owner_pk, event_timestamp
+            FROM workspace_operations.workspace_resource_counts
+            JOIN workspace_operations.workspace_owners USING (workspace_id)
         ),
-        -- For each event time, get the resource count for all resources at that time
-        event_resource_counts AS (
-            SELECT event_times.owner_pk, event_times.event_timestamp, workspace_owners.workspace_id, resource_count
-            FROM event_times
-            JOIN workspace_owners USING (owner_pk)
-            JOIN workspace_resource_counts USING (workspace_id)
-            WHERE workspace_resource_counts.event_timestamp <= event_times.event_timestamp
-                AND (next_event_timestamp IS NULL OR event_times.event_timestamp < next_event_timestamp)
+        -- List of all workspace owners
+        owners AS (SELECT DISTINCT owner_pk FROM workspace_operations.workspace_owners),
+        -- Get times at which we want to sample workspace resource counts
+        --
+        -- Using UNION instead of UNION ALL removes duplicates introduced when two
+        -- workspaces change at the same time, as well as when a workspace changes at
+        -- the exact start of an hour
+        sample_times (owner_pk, hour_start, sample_time) AS (
+            (SELECT owner_pk, DATE_TRUNC('hour', event_timestamp) AS hour_start, event_timestamp FROM workspace_change_events)
+            UNION
+            (SELECT owner_pk, hour_start, hour_start FROM workspace_operations.si_hours, owners)
         )
-    SELECT owner_pk, event_timestamp, SUM(resource_count) AS resource_count, (LEAD(event_timestamp) OVER (PARTITION BY owner_pk ORDER BY event_timestamp)) AS next_event_timestamp
-      FROM event_resource_counts
-     GROUP BY owner_pk, event_timestamp;
+    SELECT owner_pk, hour_start, sample_time, workspace_id, event_timestamp, resource_count
+      FROM sample_times
+      JOIN workspace_operations.workspace_owners USING (owner_pk)
+      JOIN workspace_operations.workspace_resource_counts USING (workspace_id)
+     WHERE sample_time >= event_timestamp
+       AND (next_event_timestamp IS NULL OR sample_time < next_event_timestamp)
+WITH NO SCHEMA BINDING;
 
--- Maximum resource counts for each and every hour a user has had workspaces
+-- Get the maximum resource count for each owner at each hour (and the time at which it occurred)
 CREATE OR REPLACE VIEW workspace_operations.owner_resource_hours AS
-    WITH
-        -- Get resource counts at the start of the hour
-        at_hour_start AS (
-            SELECT owner_pk, hour_start, SUM(resource_count) AS resource_count
-              FROM owner_resource_counts
-              JOIN si_hours
-                ON event_timestamp <= hour_start
-                 AND (next_event_timestamp IS NULL OR hour_start < next_event_timestamp)
-             GROUP BY owner_pk, hour_start
-        ),
-        -- Get resource counts in the middle of the hour
-        on_change AS (
-            SELECT owner_pk, DATE_TRUNC('hour', event_timestamp) AS hour_start, resource_count
-                FROM owner_resource_counts
-        )
-        SELECT owner_pk, hour_start, MAX(resource_count) AS resource_count
-            FROM (
-                (SELECT * FROM at_hour_start)
-                UNION ALL
-                (SELECT * FROM on_change)
-            )
-            GROUP BY owner_pk, hour_start;
+WITH
+    owner_samples AS (
+        SELECT owner_pk,
+               hour_start,
+               sample_time,
+               SUM(resource_count) AS owner_resource_count,
+               ROW_NUMBER() OVER (PARTITION BY owner_pk, hour_start ORDER BY owner_resource_count DESC) AS resource_count_order
+          FROM workspace_operations.workspace_hourly_samples
+         GROUP BY owner_pk, hour_start, sample_time
+    )
+    SELECT owner_pk, hour_start, sample_time, owner_resource_count AS resource_count
+      FROM owner_samples
+      WHERE resource_count_order = 1
+WITH NO SCHEMA BINDING;
 
+-- Get the resource counts for all workspaces at the point of the maximum resource count of
+-- their owner (owner_resource_hours)
+CREATE OR REPLACE VIEW workspace_operations.workspace_resource_hours AS
+    SELECT workspace_hourly_samples.*
+      FROM workspace_operations.owner_resource_hours
+      JOIN workspace_operations.workspace_hourly_samples USING (owner_pk, sample_time)
+WITH NO SCHEMA BINDING;
 
 -- Get the latest subscription records for each owner (the ones with MAX(record_timestamp))
 CREATE OR REPLACE VIEW workspace_operations.latest_owner_subscriptions AS
     WITH
       -- Find the latest time we've downloaded subscription data for each owner
       latest_subscription_record_timestamps AS (
-        SELECT owner_pk, MAX(record_timestamp) AS latest_record_timestamp FROM workspace_owner_subscriptions GROUP BY owner_pk
+        SELECT owner_pk, MAX(record_timestamp) AS latest_record_timestamp
+          FROM workspace_operations.workspace_owner_subscriptions
+         GROUP BY owner_pk
       ),
       -- If the user's subscription data has changed, we only want the latest (MAX(record_timestamp))
       latest_subscription_records AS (
         SELECT workspace_owner_subscriptions.*
-            FROM workspace_owner_subscriptions
-            JOIN latest_subscription_record_timestamps
+          FROM workspace_operations.workspace_owner_subscriptions
+          JOIN latest_subscription_record_timestamps
             USING (owner_pk)
             WHERE latest_record_timestamp = record_timestamp
       ),
-      owners AS (SELECT DISTINCT owner_pk FROM workspace_owners)
+      owners AS (SELECT DISTINCT owner_pk FROM workspace_operations.workspace_owners)
       SELECT owners.owner_pk,
              free_trial.external_id AS free_trial_external_subscription_id,
              free_trial.subscription_start_date AS free_trial_start_date,
@@ -148,12 +143,13 @@ CREATE OR REPLACE VIEW workspace_operations.owner_resource_hours_with_subscripti
                 WHEN hour_start >= DATE_TRUNC('hour', free_trial_start_date) THEN free_trial_external_subscription_id
                 ELSE NULL
             END) AS external_subscription_id
-        FROM owner_resource_hours
-        LEFT OUTER JOIN latest_owner_subscriptions USING (owner_pk)
+        FROM workspace_operations.owner_resource_hours
+        LEFT OUTER JOIN workspace_operations.latest_owner_subscriptions USING (owner_pk)
     )
     -- Don't include data before SI launched unless it is attached to a subscription
     SELECT * FROM all_owner_resource_hours_with_subscriptions
      WHERE hour_start >= '2024-09-25'::timestamp OR external_subscription_id IS NOT NULL
+    WITH NO SCHEMA BINDING;
 
 
 -- Totals and max count for the month
@@ -164,8 +160,9 @@ CREATE OR REPLACE VIEW workspace_operations.owner_billing_months AS
         MAX(resource_count) AS max_resource_count,
         SUM(resource_count) AS resource_hours,
         (MAX(resource_count) <= 100) AS is_free
-    FROM owner_resource_hours
-    GROUP BY owner_pk, DATE_TRUNC('month', hour_start);
+    FROM workspace_operations.owner_resource_hours
+    GROUP BY owner_pk, DATE_TRUNC('month', hour_start)
+    WITH NO SCHEMA BINDING;
 
 --
 -- Test queries to verify things went off correctly
@@ -206,5 +203,6 @@ CREATE OR REPLACE VIEW workspace_verifications.owners_with_orphaned_resource_hou
     FROM owner_resource_hours_without_subscriptions
     JOIN owner_summary USING (owner_pk)
     LEFT OUTER JOIN latest_owner_subscriptions USING (owner_pk)
-    ORDER BY first_event DESC;
+    ORDER BY first_event DESC
+    WITH NO SCHEMA BINDING;
 
