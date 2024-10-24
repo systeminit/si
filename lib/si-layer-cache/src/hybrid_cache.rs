f278bdcfb482091dff62dc5a6b7f7c036a9e902e
Scott Prutton <scott@systeminit.com>
2024-10-24 16:51:35 -0400

    more

--- lib/si-layer-cache/src/hybrid_cache.rs
+++ lib/si-layer-cache/src/hybrid_cache.rs
@@ -1,16 +1,17 @@
-use foyer::{DirectFsDeviceOptions, Engine, HybridCache, HybridCacheBuilder};
+use foyer::{DirectFsDeviceOptions, Engine, EvictionConfig, HybridCache, HybridCacheBuilder};
 use std::sync::Arc;
 use std::{hash::Hash, path::PathBuf};
+use telemetry::tracing::error;
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
+use crate::db::serialize;
 use crate::error::LayerDbResult;
-use crate::LayerDbError;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
 enum MaybeDeserialized<V>
 where
-    V: Serialize + DeserializeOwned + Clone + Hash + Eq + PartialEq + Send + Sync + 'static,
+    V: Serialize + Clone + Hash + Eq + PartialEq + Send + Sync + 'static,
 {
     RawBytes(Vec<u8>),
     DeserializedValue(V),
@@ -38,6 +39,7 @@ where
     V: Serialize + DeserializeOwned + Clone + Hash + Eq + PartialEq + Send + Sync + 'static,
 {
     pub async fn new(config: HybridCacheConfig) -> LayerDbResult<Self> {
+        // todo: unwrapping
         Ok(Self {
             cache: HybridCacheBuilder::new()
                 .with_name(&config.name)
@@ -49,54 +51,53 @@ where
                 .unwrap(),
         })
     }
+
+    pub async fn get(&self, key: &str) -> Option<V> {
+        match self.cache.obtain(key.into()).await {
+            Ok(Some(entry)) => match entry.value() {
+                // todo: bad clone here
+                MaybeDeserialized::DeserializedValue(v) => Some(v.clone()),
+                MaybeDeserialized::RawBytes(bytes) => {
+                    // If we fail to deserialize the raw bytes for some reason, pretend that we never
+                    // had the key in the first place, and also remove it from the cache.
+                    match serialize::from_bytes_async::<V>(bytes).await {
+                        Ok(deserialized) => {
+                            self.insert(key.into(), deserialized.clone());
+                            Some(deserialized)
+                        }
+                        Err(e) => {
+                            error!(
+                        "Failed to deserialize stored bytes from memory cache for key ({:?}): {}",
+                        key,
+                        e
+                    );
+                            self.remove(key);
+                            None
+                        }
+                    }
+                }
+            },
+
+            _ => None,
+        }
+    }
+
+    pub fn insert(&self, key: Arc<str>, value: V) {
+        self.cache
+            .insert(key, MaybeDeserialized::DeserializedValue(value));
+    }
+    //
+    //     pub async fn insert_raw_bytes(&self, key: Arc<str>, raw_bytes: Vec<u8>) {
+    //         self.cache
+    //             .insert(key, MaybeDeserialized::RawBytes(raw_bytes))
+    //             .await;
+    //     }
+
+    pub fn remove(&self, key: &str) {
+        self.cache.remove(key);
+    }
 }
 
-//     pub async fn get(&self, key: &str) -> Option<V> {
-//         match self.cache.get(key).await {
-//             Some(MaybeDeserialized::DeserializedValue(value)) => {
-//                 metric!(counter.layer_cache.hit.memory = 1);
-//                 Some(value)
-//             }
-//             Some(MaybeDeserialized::RawBytes(bytes)) => {
-//                 // If we fail to deserialize the raw bytes for some reason, pretend that we never
-//                 // had the key in the first place, and also remove it from the cache.
-//                 match serialize::from_bytes_async::<V>(&bytes).await {
-//                     Ok(deserialized) => {
-//                         self.insert(key.into(), deserialized.clone()).await;
-//                         metric!(counter.layer_cache.hit.memory = 1);
-//                         Some(deserialized)
-//                     }
-//                     Err(e) => {
-//                         error!(
-//                             "Failed to deserialize stored bytes from memory cache for key ({:?}): {}",
-//                             key,
-//                             e
-//                         );
-//                         self.remove(key).await;
-//                         None
-//                     }
-//                 }
-//             }
-//             None => None,
-//         }
-//     }
-//
-//     pub async fn insert(&self, key: Arc<str>, value: V) {
-//         self.cache
-//             .insert(key, MaybeDeserialized::DeserializedValue(value))
-//             .await;
-//     }
-//
-//     pub async fn insert_raw_bytes(&self, key: Arc<str>, raw_bytes: Vec<u8>) {
-//         self.cache
-//             .insert(key, MaybeDeserialized::RawBytes(raw_bytes))
-//             .await;
-//     }
-//
-//     pub async fn remove(&self, key: &str) {
-//         self.cache.remove(key).await;
-//     }
-//
 //     pub fn contains(&self, key: &str) -> bool {
 //         self.cache.contains_key(key)
 //     }
