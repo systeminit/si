e86f27a29e0d9f453c57a194960b6706558c6989
Meg Watson <megan.watson@matillion.com>
2024-12-02 10:56:43 +0000

    feat: add remainder of changset routes to API client

--- bin/si-api-test/tests/6-get_head_changeset.ts
+++ bin/si-api-test/tests/6-get_head_changeset.ts
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import { SdfApiClient } from "../sdf_api_client.ts";
 
 export default async function get_head_changeset(sdfApiClient: SdfApiClient) {
-  const data = await sdfApiClient.call({ route: "open_change_sets" });
+  const data = await sdfApiClient.call({ route: "list_open_change_sets" });
 
   assert(data.headChangeSetId, "Expected headChangeSetId");
   const head = data.changeSets.find((c) => c.id === data.headChangeSetId);
