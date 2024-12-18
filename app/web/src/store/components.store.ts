b6aeeb72705767cf25c0f628deb07fa1403027e5
John Obelenus <jobelenus@systeminit.com>
2024-12-18 18:03:10 -0500

    Fix: load all components at the right time
    After first paint, but before the data is required by other things!

--- app/web/src/store/components.store.ts
+++ app/web/src/store/components.store.ts
@@ -1427,29 +1427,11 @@ export const useComponentsStore = (forceChangeSetId?: ChangeSetId) => {
             ],
           );
 
-          realtimeStore.subscribe(
-            `${this.$id}-workspace`,
-            `workspace/${workspaceId}`,
-            [
-              {
-                eventType: "ChangeSetApplied",
-                callback: (data) => {
-                  // If the applied change set has rebased into this change set,
-                  // then refetch (i.e. there might be updates!)
-                  if (data.toRebaseChangeSetId === changeSetId) {
-                    this.FETCH_ALL_COMPONENTS();
-                  }
-                },
-              },
-            ],
-          );
-
           const actionUnsub = this.$onAction(handleStoreError);
 
           return () => {
             actionUnsub();
             realtimeStore.unsubscribe(`${this.$id}-changeset`);
-            realtimeStore.unsubscribe(`${this.$id}-workspace`);
           };
         },
       },
