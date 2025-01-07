1ef3f5eccbf51691a94b1f2f96da6ccaec269d34
Scott Prutton <scott@systeminit.com>
2025-01-07 13:08:15 -0500

    feat: working tests for lang-js deno

--- bin/lang-js/tests/functions/beforeFuncs.ts
+++ bin/lang-js/tests/functions/beforeFuncs.ts
@@ -1,30 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// deno-lint-ignore-file
 // @ts-nocheck
 function main() {
   console.log("Running Main");
-  const b1 = requestStorage.getItem("b1");
-  const b2 = requestStorage.getItem("b2");
-  const b3 = requestStorage.getItem("b3");
+  const b1 = requestStorage.getEnv("b1");
+  const b2 = requestStorage.getEnv("b2");
+  const b3 = requestStorage.getEnv("b3");
 
-  const keys = requestStorage.getKeys();
+  const keys = requestStorage.getEnvKeys();
 
-  console.log(`Before function 1 set b1="${b1}", Before function 2 said "${b3}", keys are ${keys}`);
+  console.log(
+    `Before function 1 set b1="${b1}", Before function 2 said "${b3}", keys are ${keys}`,
+  );
 
   return {
     status: b1 && b2 === undefined && typeof b3 === "string" ? "ok" : "error",
   };
-}
-
-function before1(arg) {
-  console.log("Running Before 1");
-  console.log(`My arg is ${arg}`);
-  requestStorage.setItem("b1", true);
-  requestStorage.setItem("b2", true);
-}
-
-function before2(arg) {
-  console.log("Running Before 2");
-  console.log(`My arg is ${arg}`);
-  requestStorage.deleteItem("b2");
-  requestStorage.setItem("b3", "I'm a string");
 }
