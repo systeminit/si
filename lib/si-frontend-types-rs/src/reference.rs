f55be9b5467eddbbf36c2f8cc4cb8d8bd0ed8263
Jacob Helwig <jacob@technosorcery.net>
2025-02-07 08:14:06 -0800

    Begin convenience traits for more easily deriving behavior later on
    If we're going to be able to derive "here's how to ship this to the
    frontend with type/id/checksum information" we'll want most everything
    to be able to report its own checksum without having to annotate
    everything with how to get a `&[u8]` from it.

--- lib/si-frontend-types-rs/src/reference.rs
+++ lib/si-frontend-types-rs/src/reference.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
+use si_events::workspace_snapshot::{Checksum, ChecksumHasher};
+
+use crate::change_set::FrontendChecksum;
 
 #[remain::sorted]
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Deserialize, Serialize, strum::Display)]
 #[serde(rename_all = "camelCase")]
 pub enum ReferenceKind {
     ChangeSetList,
@@ -10,14 +13,46 @@ pub enum ReferenceKind {
 #[derive(Clone, Debug, Eq, PartialEq, Serialize)]
 pub struct ReferenceId<T>(pub T)
 where
-    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize;
+    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize + std::fmt::Display;
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize)]
 pub struct Reference<T>
 where
-    T: Serialize + Eq + PartialEq + Clone + std::fmt::Debug,
+    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize + std::fmt::Display,
 {
     pub kind: ReferenceKind,
     pub id: ReferenceId<T>,
     pub checksum: String,
+}
+
+impl<T> std::fmt::Display for ReferenceId<T>
+where
+    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize + std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+
+impl<T> FrontendChecksum for ReferenceId<T>
+where
+    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize + std::fmt::Display,
+{
+    fn checksum(&self) -> Checksum {
+        todo!()
+    }
+}
+
+impl<T> FrontendChecksum for Reference<T>
+where
+    T: Eq + PartialEq + Clone + std::fmt::Debug + Serialize + std::fmt::Display,
+{
+    fn checksum(&self) -> Checksum {
+        let mut hasher = ChecksumHasher::new();
+        hasher.update(FrontendChecksum::checksum(&self.kind.to_string()).as_bytes());
+        hasher.update(FrontendChecksum::checksum(&self.id.to_string()).as_bytes());
+        hasher.update(FrontendChecksum::checksum(&self.checksum).as_bytes());
+
+        hasher.finalize()
+    }
 }
