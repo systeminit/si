3b44eca27391b6c91acb2d34bd010081613f5e6e
stack72 <public@paulstack.co.uk>
2025-02-07 17:50:44 +0000

    chore(auth-portal): Cleanup CHANGE_USER_ROLE feature flag

--- app/auth-portal/src/store/feature_flags.store.ts
+++ app/auth-portal/src/store/feature_flags.store.ts
@@ -11,14 +11,12 @@ export const useFeatureFlagsStore = () => {
       state: () => ({
         ADMIN_PAGE: false,
         ON_DEMAND_ASSETS: false,
-        CHANGE_USER_ROLE: false,
         AUTOMATION_API: false,
       }),
       onActivated() {
         posthog.onFeatureFlags((flags) => {
           this.ADMIN_PAGE = flags.includes("auth_portal_admin_page");
           this.ON_DEMAND_ASSETS = flags.includes("on_demand_assets");
-          this.CHANGE_USER_ROLE = flags.includes("change_user_role");
           this.AUTOMATION_API = flags.includes("automation_api");
         });
       },
