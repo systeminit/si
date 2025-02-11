ba22e8eaf670e9615776183abba624de5431a872
Victor Bustamante <victor@systeminit.com>
2025-02-11 19:28:34 -0300

    fix(Clover): Don't create duplicated output sockets
    Signed-off-by: Victor Bustamante <victor@systeminit.com>

--- bin/clover/src/pipeline-steps/generateOutputSocketsFromProps.ts
+++ bin/clover/src/pipeline-steps/generateOutputSocketsFromProps.ts
@@ -1,9 +1,7 @@
 import _ from "lodash";
 import { bfsPropTree } from "../spec/props.ts";
 import {
-  createOutputSocketFromProp,
-  ExpandedSocketSpec,
-  getSocketOnVariant,
+  getOrCreateOutputSocketFromProp,
   setAnnotationOnSocket,
 } from "../spec/sockets.ts";
 import { ExpandedPkgSpec, ExpandedSchemaVariantSpec } from "../spec/pkgs.ts";
@@ -17,12 +15,10 @@ export function generateOutputSocketsFromProps(
     const [schema] = spec.schemas;
     const [schemaVariant] = schema.variants;
 
-    schemaVariant.sockets = [
-      ...schemaVariant.sockets,
-      ...createSocketsFromResource(schemaVariant),
-      ...createSocketsFromPrimaryIdentifier(schemaVariant),
-      ...createSocketsForCommonProps(schemaVariant),
-    ];
+    // These funcs modify the variant directly
+    createSocketsFromResource(schemaVariant);
+    createSocketsFromPrimaryIdentifier(schemaVariant);
+    createSocketsForCommonProps(schemaVariant);
 
     newSpecs.push(spec);
   }
@@ -32,15 +28,14 @@ export function generateOutputSocketsFromProps(
 
 function createSocketsFromResource(
   variant: ExpandedSchemaVariantSpec,
-): ExpandedSocketSpec[] {
+) {
   const resource = variant.resourceValue;
 
   if (resource.kind !== "object") throw "Resource prop is not object";
 
-  const sockets: ExpandedSocketSpec[] = [];
   for (const prop of resource.entries) {
     if (!["array", "object"].includes(prop.kind)) {
-      const socket = createOutputSocketFromProp(prop);
+      const socket = getOrCreateOutputSocketFromProp(variant, prop);
       // if this socket is an arn, we want to make sure that all input sockets
       // that might also be arns can take this value
       if (socket.name.toLowerCase().endsWith("arn")) {
@@ -49,42 +44,34 @@ function createSocketsFromResource(
           setAnnotationOnSocket(socket, { tokens: [token] });
         }
       }
-      sockets.push(socket);
     }
   }
-  return sockets;
 }
 
 function createSocketsFromPrimaryIdentifier(
   variant: ExpandedSchemaVariantSpec,
-): ExpandedSocketSpec[] {
+) {
   const domain = variant.domain;
 
   if (domain.kind !== "object") throw "Domain prop is not object";
-
-  const sockets: ExpandedSocketSpec[] = [];
 
   bfsPropTree(domain, (prop) => {
     // We don't check if the socket already exists before adding, since on the other func
     // we only look at resourceValue props
     if (prop.metadata.primaryIdentifier) {
-      sockets.push(createOutputSocketFromProp(prop));
+      getOrCreateOutputSocketFromProp(variant, prop);
     }
   }, {
     skipTypeProps: true,
   });
-
-  return sockets;
 }
 
 // VariantName, VariantId props should always have sockets
 function createSocketsForCommonProps(
   variant: ExpandedSchemaVariantSpec,
-): ExpandedSocketSpec[] {
+) {
   const { domain } = variant;
   const variantName = variant.data.displayName;
-
-  const sockets: ExpandedSocketSpec[] = [];
 
   bfsPropTree(domain, (prop) => {
     if (
@@ -94,12 +81,8 @@ function createSocketsForCommonProps(
     ) return;
 
     // Don't duplicate sockets
-    if (getSocketOnVariant(variant, prop.name, "output")) return;
-
-    sockets.push(createOutputSocketFromProp(prop));
+    getOrCreateOutputSocketFromProp(variant, prop);
   }, {
     skipTypeProps: true,
   });
-
-  return sockets;
 }
