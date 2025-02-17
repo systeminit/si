c579009223e160f4c620ae497230554ee8885e17
stack72 <public@paulstack.co.uk>
2025-02-17 19:21:28 +0000

    feat(clover): Adding an override for the secrets manager secret
    This is the first part of this work. We now need to ensure that we can use this prop as part of the payload we send to AWS
    This isn't specific to SecretManager:;Secret, this is a pattern that we will need to replicate with other assets like KmsKey and RDS DBInstance USername/Password

--- bin/clover/src/pipeline-steps/assetSpecificOverrides.ts
+++ bin/clover/src/pipeline-steps/assetSpecificOverrides.ts
@@ -93,7 +93,7 @@ const overrides = new Map<string, OverrideFn>([
   ],
   ["ContainerDefinitions Secrets", (spec: ExpandedPkgSpec) => {
     const variant = spec.schemas[0].variants[0];
-    
+
     const prop = variant.domain.entries.find((p: ExpandedPropSpec) =>
       p.name === "ValueFrom"
     );
