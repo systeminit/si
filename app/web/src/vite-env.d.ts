2f363b2501b46ac739f9815cd0ce44625320ef08
John Watson <john@familywatson.co.uk>
2025-02-21 14:32:54 +0000

    feat: prevent exfiltration of otel variables via web

--- app/web/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_OTEL_EXPORTER_OTLP_ENDPOINT: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
