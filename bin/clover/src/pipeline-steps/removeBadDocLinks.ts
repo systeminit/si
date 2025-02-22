c968951333eb40add3a7109afcaa0a235b315218
John Keiser <john@johnkeiser.com>
2025-02-21 18:52:51 -0800

    Check individual props to see if the #ref works

--- bin/clover/src/pipeline-steps/removeBadDocLinks.ts
+++ bin/clover/src/pipeline-steps/removeBadDocLinks.ts
@@ -1,11 +1,13 @@
 import _logger from "../logger.ts";
 import { ExpandedPkgSpec } from "../spec/pkgs.ts";
-import { bfsPropTree } from "../spec/props.ts";
+import { bfsPropTree, ExpandedPropSpec } from "../spec/props.ts";
 import { existsSync } from "node:fs";
 
 const logger = _logger.ns("siSpecs").seal();
 
 const SAVE_INTERVAL = 5;
+const DOC_START =
+  "https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/";
 
 // This is a failsafe that checks if we've generated any bad links to AWS docs for props. If we
 // have, it removes them from the generated specs so we don't link off to the aether.
@@ -13,63 +15,102 @@ export async function removeBadDocLinks(
   specs: ExpandedPkgSpec[],
   docLinkCache: string,
 ) {
+  const linkCache = await loadLinkCache(docLinkCache);
+
   // Gather the unique doc links
-  const foundLinks = new Set<string>();
+  const foundLinks = {} as Record<
+    string,
+    { fragment: string; prop: ExpandedPropSpec }[]
+  >;
   for (const { schemas: [{ variants: [variant] }] } of specs) {
-    // Gather the links
+    // Add the top level link
+    if (variant.data.link) {
+      foundLinks[variant.data.link] ??= [];
+    }
+
+    // Gather prop links
     bfsPropTree([
       variant.domain,
       variant.resourceValue,
       variant.secrets,
       variant.secretDefinition,
     ], (prop) => {
-      const docLink = prop.data.docLink?.split("#")?.[0];
-      if (docLink) foundLinks.add(docLink);
+      if (!prop.data.docLink) return;
+      const [docLink, fragment] = prop.data.docLink.split("#", 2);
+      if (docLink) {
+        foundLinks[docLink] ??= [];
+        foundLinks[docLink].push({ fragment, prop });
+      }
     });
   }
 
-  // Fetch the docs to see if they are there (if they aren't already in cache)
-  const linkCache = await loadLinkCache(docLinkCache);
+  // Fetch the docs to see if they are there
   let lastSaved = Date.now();
-  for (const link of foundLinks) {
-    if (!(link in linkCache)) {
-      logger.info(`Checking doc link: ${link} ...`);
-      linkCache[link] = (await fetch(link, { redirect: "manual" })).status;
+  for (const [docLink, propRefs] of Object.entries(foundLinks)) {
+    if (!docLink.startsWith(DOC_START)) {
+      throw new Error(`Unexpected doc link ${docLink}`);
     }
+    const page = docLink.slice(DOC_START.length);
+    if (!(page in linkCache)) {
+      logger.info(`Checking doc link: ${docLink} ...`);
+      const response = await fetch(docLink, { redirect: "manual" });
+      const { status } = response;
+      const fragments = propRefs.map(({ fragment }) => fragment);
+      if (status === 200) {
+        // Search the doc to make sure the fragments link somewhere
+        const doc = await response.text();
+        if (fragments.length == 0) {
+          linkCache[page] = {};
+        } else {
+          linkCache[page] = {
+            missing: fragments.filter((fragment) =>
+              !doc.includes(`id="${fragment}"`)
+            ),
+          };
+        }
+      } else {
+        // It's not 200, so everything is missing
+        linkCache[page] = { error: status, missing: fragments };
+      }
+    }
+
     // Save periodically in case there are issues
     if ((Date.now() - lastSaved) > SAVE_INTERVAL) {
       await saveLinkCache(linkCache, docLinkCache);
       lastSaved = Date.now();
     }
   }
-  await saveLinkCache(linkCache, docLinkCache);
 
-  // Remove the doc links from anything that links to a bad spot
+  // Actually remove the links
   for (const { schemas: [{ variants: [variant] }] } of specs) {
-    // Gather the links
-    bfsPropTree([
-      variant.domain,
-      variant.resourceValue,
-      variant.secrets,
-      variant.secretDefinition,
-    ], (prop) => {
-      const docLink = prop.data.docLink?.split("#")?.[0];
-      if (docLink && linkCache[docLink] !== 200) {
+    if (variant.data.link && linkCache[variant.data.link].error) {
+      variant.data.link = null;
+    }
+  }
+
+  for (const [docLink, propRefs] of Object.entries(foundLinks)) {
+    const page = docLink.split("/").pop()!;
+    for (const { fragment, prop } of propRefs) {
+      if (linkCache[page].missing?.includes(fragment)) {
         prop.data.docLink = null;
       }
-    });
+    }
   }
+
+  await saveLinkCache(linkCache, docLinkCache);
 
   return specs;
 }
 
-type LinkCache = Record<string, number>;
+type LinkCache = {
+  [docLink in string]: { error?: number; missing?: string[] };
+};
 
 async function loadLinkCache(docLinkCache: string): Promise<LinkCache> {
-  if (!existsSync(docLinkCache)) {
-    return {};
-  }
-  return JSON.parse(await Deno.readTextFile(docLinkCache));
+  if (!existsSync(docLinkCache)) return {};
+  const result = JSON.parse(await Deno.readTextFile(docLinkCache)) as LinkCache;
+  if (typeof Object.values(result)[0] !== "object") return {};
+  return result as LinkCache;
 }
 
 async function saveLinkCache(linkCache: LinkCache, docLinkCache: string) {
