8e58adc6ca82f3dd2acf06f836f35c03749e6dcd
John Obelenus <jobelenus@systeminit.com>
2025-03-03 13:47:05 -0500

    Obs Fix: bad toString for objects

--- lib/vue-lib/src/pinia/pinia_api_tools.ts
+++ lib/vue-lib/src/pinia/pinia_api_tools.ts
@@ -182,7 +182,11 @@ const describePattern = (pattern: URLPattern): [string, string] => {
       _urlName.push(p);
     } else {
       const vals = Object.values(p);
-      if (!vals[0]) throw Error(`Bad URLPattern ${pattern} with: ${p}`);
+      if (!vals[0])
+        throw Error(
+          `Bad URLPattern ${JSON.stringify(pattern)} with: ${JSON.stringify(p)}
+        `,
+        );
       else _url.push(vals[0]); // url gets the value
       const keys = Object.keys(p);
       if (keys.length > 0) _urlName.push(`:${keys[0]}`); // name gets the str
