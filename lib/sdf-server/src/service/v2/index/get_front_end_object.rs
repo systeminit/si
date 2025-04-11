d624916c0c82d38de054c07a01d9992e3920cba4
Nick Gerace <nick@systeminit.com>
2025-04-11 17:04:01 -0400

    Add rebuild change set index route
    This commit adds the rebuild change set index route. There is no
    corresponding button or action that can trigger it in the frontend yet,
    but it is available.
    
    This commit also standardizes the two rebuild functions' trace naming
    schemes and moves them to the root index module. In addition to these
    changes, all routes are now in their own submodules.
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

---
+++ lib/sdf-server/src/service/v2/index/get_front_end_object.rs
@@ -0,0 +1,50 @@
+use serde::{Deserialize, Serialize};
+
+use axum::{
+    extract::{Path, Query},
+    Json,
+};
+use dal::{ChangeSet, ChangeSetId, WorkspacePk};
+
+use crate::extract::{FriggStore, HandlerContext};
+
+use super::{AccessBuilder, FrontEndObjectMeta, IndexError, IndexResult};
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct FrontendObjectRequest {
+    pub kind: String,
+    pub id: String,
+    pub checksum: Option<String>,
+}
+
+pub async fn get_front_end_object(
+    HandlerContext(builder): HandlerContext,
+    AccessBuilder(access_builder): AccessBuilder,
+    FriggStore(frigg): FriggStore,
+    Path((workspace_pk, change_set_id)): Path<(WorkspacePk, ChangeSetId)>,
+    Query(request): Query<FrontendObjectRequest>,
+) -> IndexResult<Json<FrontEndObjectMeta>> {
+    let ctx = builder
+        .build(access_builder.build(change_set_id.into()))
+        .await?;
+    let change_set = ChangeSet::get_by_id(&ctx, change_set_id).await?;
+
+    let obj;
+    if let Some(checksum) = request.checksum {
+        obj = frigg
+            .get_object(workspace_pk, &request.kind, &request.id, &checksum)
+            .await?
+            .ok_or(IndexError::IndexNotFound(workspace_pk, change_set_id))?;
+    } else {
+        obj = frigg
+            .get_current_object(workspace_pk, change_set_id, &request.kind, &request.id)
+            .await?
+            .ok_or(IndexError::IndexNotFound(workspace_pk, change_set_id))?;
+    }
+
+    Ok(Json(FrontEndObjectMeta {
+        workspace_snapshot_address: change_set.workspace_snapshot_address,
+        front_end_object: obj,
+    }))
+}
