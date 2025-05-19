7ff8a7d5dfd42dcfb5cfdcb3712c5972fc4ccca7
Jacob Helwig <jacob@systeminit.com>
2025-05-19 14:34:20 +0000

    Do not send empty PatchBatch
    We already suppress individual empty patches to prevent needless
    messaging, but if the entire PatchBatch has no patches, we were still
    sending it. We now make sure that there are patches in the PatchBatch
    before sending the batch out to listening clients.

--- lib/edda-server/src/data_cache.rs
+++ lib/edda-server/src/data_cache.rs
@@ -19,6 +19,10 @@ pub(crate) struct DataCache;
 impl DataCache {
     #[instrument(name = "data_cache.publish_patch_batch", level = "info", skip_all)]
     pub async fn publish_patch_batch(ctx: &DalContext, patch_batch: PatchBatch) -> Result<()> {
+        if patch_batch.patches.is_empty() {
+            return Ok(());
+        }
+
         ctx.txns()
             .await?
             .nats()
