a7cb4508846b38467f7b19ad461857df9d184832
Aaron Dernley <aaron@systeminit.com>
2025-06-04 00:07:35 +0100

    fix: ec2 instance attribute function and qualification

--- bin/clover/src/cloud-control-funcs/overrides/AWS::EC2::Instance/attribute/base64EncodeUserData.ts
+++ bin/clover/src/cloud-control-funcs/overrides/AWS::EC2::Instance/attribute/base64EncodeUserData.ts
@@ -1,6 +1,8 @@
 async function main(input: Input): Promise<Output> {
   const data = input.data;
 
+  if (!data) return "";
+
   try {
     const decoded = Buffer.from(data, 'base64').toString('utf8');
     const reEncoded = Buffer.from(decoded, 'utf8').toString('base64');
