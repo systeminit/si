f61fc8bdbb4218369ea6efb7db02416023dc8d6f
Jacob Helwig <jacob@systeminit.com>
2025-06-03 21:19:46 +0000

    Create multi_mjolnir API endpoint for builk retrieval of objects

--- lib/sdf-server/src/service/v2/index.rs
+++ lib/sdf-server/src/service/v2/index.rs
@@ -84,6 +84,10 @@ pub fn v2_change_set_routes() -> Router<AppState> {
         .route("/", get(get_change_set_index::get_change_set_index))
         .route("/mjolnir", get(get_front_end_object::get_front_end_object))
         .route(
+            "/multi_mjolnir",
+            post(get_front_end_object::get_multiple_front_end_objects),
+        )
+        .route(
             "/rebuild",
             post(rebuild_change_set_index::rebuild_change_set_index),
         )
--- lib/sdf-server/src/service/v2/index/get_front_end_object.rs
+++ lib/sdf-server/src/service/v2/index/get_front_end_object.rs
@@ -13,6 +13,7 @@ use serde::{
     Deserialize,
     Serialize,
 };
+use telemetry::prelude::*;
 
 use super::{
     AccessBuilder,
@@ -25,9 +26,9 @@ use crate::extract::{
     HandlerContext,
 };
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
-pub struct FrontendObjectRequest {
+pub struct FrontEndObjectRequest {
     pub kind: String,
     pub id: String,
     pub checksum: Option<String>,
@@ -38,12 +39,64 @@ pub async fn get_front_end_object(
     AccessBuilder(access_builder): AccessBuilder,
     FriggStore(frigg): FriggStore,
     Path((workspace_pk, change_set_id)): Path<(WorkspacePk, ChangeSetId)>,
-    Query(request): Query<FrontendObjectRequest>,
+    Query(request): Query<FrontEndObjectRequest>,
 ) -> IndexResult<Json<FrontEndObjectMeta>> {
     let _ctx = builder
         .build(access_builder.build(change_set_id.into()))
         .await?;
 
+    Ok(Json(
+        front_end_object_meta(&frigg, workspace_pk, change_set_id, request).await?,
+    ))
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct MultipleFrontEndObjectRequest {
+    pub requests: Vec<FrontEndObjectRequest>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct MultipleFrontEndObjectResponse {
+    successful: Vec<FrontEndObjectMeta>,
+    failed: Vec<FrontEndObjectRequest>,
+}
+
+pub async fn get_multiple_front_end_objects(
+    HandlerContext(builder): HandlerContext,
+    AccessBuilder(access_builder): AccessBuilder,
+    FriggStore(frigg): FriggStore,
+    Path((workspace_pk, change_set_id)): Path<(WorkspacePk, ChangeSetId)>,
+    Json(request): Json<MultipleFrontEndObjectRequest>,
+) -> IndexResult<Json<MultipleFrontEndObjectResponse>> {
+    let _ctx = builder
+        .build(access_builder.build(change_set_id.into()))
+        .await?;
+
+    let mut successful = Vec::new();
+    let mut failed = Vec::new();
+    for object_request in request.requests {
+        match front_end_object_meta(&frigg, workspace_pk, change_set_id, object_request.clone())
+            .await
+        {
+            Ok(meta) => successful.push(meta),
+            Err(error) => {
+                error!(?error);
+                failed.push(object_request);
+            }
+        };
+    }
+
+    Ok(Json(MultipleFrontEndObjectResponse { successful, failed }))
+}
+
+async fn front_end_object_meta(
+    frigg: &frigg::FriggStore,
+    workspace_pk: WorkspacePk,
+    change_set_id: ChangeSetId,
+    request: FrontEndObjectRequest,
+) -> IndexResult<FrontEndObjectMeta> {
     let (checksum, address) = match frigg
         .get_index_pointer_value(workspace_pk, change_set_id)
         .await?
@@ -72,9 +125,9 @@ pub async fn get_front_end_object(
             ))?;
     }
 
-    Ok(Json(FrontEndObjectMeta {
+    Ok(FrontEndObjectMeta {
         workspace_snapshot_address: address,
         index_checksum: checksum,
         front_end_object: obj,
-    }))
+    })
 }
