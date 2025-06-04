17f2826293b2880271f0b851a4f5fc1ef1afc847
stack72 <public@paulstack.co.uk>
2025-06-04 02:10:24 +0100

    fix(clover): Fixes the funcId that causes issue creating ec2 instance

--- bin/clover/src/pipeline-steps/assetSpecificOverrides.ts
+++ bin/clover/src/pipeline-steps/assetSpecificOverrides.ts
@@ -110,7 +110,7 @@ const overrides = new Map<string, OverrideFn>([
       },
     ];
     userDataProp.data.funcUniqueId =
-      "5a5b8c9d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b";
+      "5a5b8c9d6e7f8a5b0c1d2e3f4a5b6c7d8e9f0a1b2c3d8e5f6a7b8c9d0e1f2a3b";
     variant.sockets.push(userDataSocket);
 
     const launchTemplateProp = propForOverride(
