d939fefbcfb66106f157487d631186c4dfa0558f
Nick Gerace <nick@systeminit.com>
2025-06-05 18:24:32 -0400

    Add apply change set to new UI
    This change adds the ability to apply change sets in the new UI. This
    does not include approvals, but leaves the door open to them. The apply
    button follows the same design style as "Add a component".
    
    The modal supports keyboard input, including "ESC" for exiting the modal
    and "CMD + ENTER" for applying the change set. The modal also follows
    a similar style to the rest of the new UI, trading icons for keyboard
    shortcut suggestions.
    
    The actions list within the modal is now driven by props and contains
    pill counters for each kind of action seen in the list. The pill counter
    building block component has been extended to allow slotted-in icons.
    
    Co-authored-by: Wendy Bujalski <wendy@systeminit.com>
    Signed-off-by: Nick Gerace <nick@systeminit.com>

---
+++ app/web/src/newhotness/ApplyChangeSetButton.vue
@@ -0,0 +1,85 @@
+<template>
+  <section v-if="!changeSetsStore.headSelected">
+    <VButton
+      ref="applyButtonRef"
+      size="sm"
+      tone="action"
+      label="Apply Change Set"
+      class="ml-2xs mr-xs"
+      loadingText="Applying Changes"
+      :requestStatus="applyChangeSetReqStatus"
+      :disabled="disabled"
+      @click="openApprovalFlowModal"
+    >
+      <template #iconRight>
+        <!--
+          NOTE(nick): I wanted this to look like "Add a component", but its concept of a pill 
+          more of a plaintext helper. The pills look a little different and I don't love it, but
+          this will have to do in the meantime.
+        -->
+        <PillCounter
+          :count="actions.length"
+          :paddingX="actions.length > 10 ? '2xs' : 'xs'"
+          noColorStyles
+          class="border border-action-200 ml-2xs py-2xs"
+        />
+      </template>
+    </VButton>
+    <ApprovalFlowModal
+      ref="approvalFlowModalRef"
+      votingKind="merge"
+      :actions="actions"
+    />
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import * as _ from "lodash-es";
+import { VButton, PillCounter } from "@si/vue-lib/design-system";
+import { useQuery } from "@tanstack/vue-query";
+import { useChangeSetsStore } from "@/store/change_sets.store";
+import { useStatusStore } from "@/store/status.store";
+import { ChangeSetStatus } from "@/api/sdf/dal/change_set";
+import {
+  BifrostActionViewList,
+  EntityKind,
+} from "@/workers/types/entity_kind_types";
+import { bifrost, useMakeArgs, useMakeKey } from "@/store/realtime/heimdall";
+import ApprovalFlowModal from "./ApprovalFlowModal.vue";
+
+const changeSetsStore = useChangeSetsStore();
+const statusStore = useStatusStore();
+
+const applyChangeSetReqStatus =
+  changeSetsStore.getRequestStatus("APPLY_CHANGE_SET");
+
+const approvalFlowModalRef = ref<InstanceType<typeof ApprovalFlowModal>>();
+
+const openApprovalFlowModal = () => {
+  approvalFlowModalRef.value?.open();
+};
+
+const disabled = computed(
+  () =>
+    changeSetsStore.selectedChangeSet?.status !== ChangeSetStatus.Open ||
+    changeSetsStore.headSelected ||
+    statusStoreUpdating.value,
+);
+
+const statusStoreUpdating = computed(() => {
+  if (statusStore.globalStatus) {
+    return statusStore.globalStatus.isUpdating;
+  } else return false;
+});
+
+const key = useMakeKey();
+const args = useMakeArgs();
+
+const actionsRaw = useQuery<BifrostActionViewList | null>({
+  queryKey: key(EntityKind.ActionViewList),
+  queryFn: async () =>
+    await bifrost<BifrostActionViewList>(args(EntityKind.ActionViewList)),
+});
+const actions = computed(() => actionsRaw.data.value?.actions ?? []);
+</script>
