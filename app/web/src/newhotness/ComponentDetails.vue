245a8a61e684d021546fdc54aafeec37ce4a8d19
stack72 <public@paulstack.co.uk>
2025-07-08 03:08:44 +0300

    fix(web): Removing the duplicate place to edit a name

--- app/web/src/newhotness/ComponentDetails.vue
+++ app/web/src/newhotness/ComponentDetails.vue
@@ -39,52 +39,7 @@
       <template v-else>
         <div class="flex-none">{{ component.schemaVariantName }}</div>
         <div class="flex-none">/</div>
-        <div class="flex-1 min-w-0 m-[-4px]">
-          <nameForm.Field
-            :validators="{
-              onChange: required,
-              onBlur: required,
-            }"
-            name="name"
-          >
-            <template #default="{ field }">
-              <EditInPlace
-                ref="editInPlaceRef"
-                @hidden="reset"
-                @showing="focus"
-              >
-                <template #trigger>
-                  <VButton
-                    :label="field.state.value"
-                    class="border-0 font-normal max-w-full"
-                    iconRight="edit"
-                    size="sm"
-                    textSize="md"
-                    tone="shade"
-                    variant="ghost"
-                    truncateText
-                    @click="editInPlaceRef?.toggle"
-                  />
-                </template>
-                <template #input>
-                  <input
-                    ref="nameRef"
-                    :value="field.state.value"
-                    class="block w-full text-white bg-black border border-neutral-300 disabled:bg-neutral-900"
-                    type="text"
-                    @blur="blur"
-                    @input="
-                      (e) =>
-                        field.handleChange((e.target as HTMLInputElement).value)
-                    "
-                    @keydown.enter.stop.prevent="blur"
-                    @keydown.esc.stop.prevent="reset"
-                  />
-                </template>
-              </EditInPlace>
-            </template>
-          </nameForm.Field>
-        </div>
+        <div class="flex-1 min-w-0 m-[-4px]">{{ component.name }}</div>
       </template>
     </div>
 
@@ -239,16 +194,8 @@ import {
   themeClasses,
   IconButton,
 } from "@si/vue-lib/design-system";
-import {
-  computed,
-  ref,
-  nextTick,
-  onMounted,
-  onBeforeUnmount,
-  inject,
-  watch,
-} from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { computed, ref, onMounted, onBeforeUnmount, inject, watch } from "vue";
+import { useRouter } from "vue-router";
 import clsx from "clsx";
 import { bifrost, useMakeArgs, useMakeKey } from "@/store/realtime/heimdall";
 import {
@@ -264,9 +211,7 @@ import AttributePanel from "./AttributePanel.vue";
 import { attributeEmitter, keyEmitter } from "./logic_composables/emitters";
 import CollapsingFlexItem from "./layout_components/CollapsingFlexItem.vue";
 import DelayedLoader from "./layout_components/DelayedLoader.vue";
-import EditInPlace from "./layout_components/EditInPlace.vue";
-import { useApi, routes, componentTypes } from "./api_composables";
-import { useWatchedForm } from "./logic_composables/watched_form";
+import { useApi, routes } from "./api_composables";
 import { preserveExploreState } from "./util";
 import { SelectionsInQueryString } from "./Workspace.vue";
 import QualificationPanel from "./QualificationPanel.vue";
@@ -355,8 +300,6 @@ attributeEmitter.on("selectedDocs", (data) => {
 const attrRef = ref<typeof CollapsingFlexItem>();
 const actionRef = ref<typeof CollapsingFlexItem>();
 const mgmtRef = ref<typeof CollapsingFlexItem>();
-const nameRef = ref<HTMLInputElement>();
-const editInPlaceRef = ref<typeof EditInPlace>();
 
 // TODO(Wendy) - this code is for if we want the AttributeInput to float again
 // const scrollAttributePanel = (value: number) => {
@@ -384,97 +327,6 @@ const api = useApi();
 export type NameFormData = {
   name: string;
 };
-
-const nameFormData = computed<NameFormData>(() => {
-  return { name: component.value?.name ?? "" };
-});
-
-const wForm = useWatchedForm<NameFormData>(
-  `component.name.${props.componentId}`,
-);
-
-const route = useRoute();
-
-const nameForm = wForm.newForm({
-  data: nameFormData,
-  onSubmit: async ({ value }) => {
-    const name = value.name;
-    // i wish the validator narrowed this type to always be a string
-    if (name) {
-      const id = component.value?.id;
-      if (!id) throw new Error("Missing id");
-      const call = api.endpoint(routes.UpdateComponentName, { id });
-      const { req, newChangeSetId } =
-        await call.put<componentTypes.UpdateComponentNameArgs>({
-          name,
-        });
-      if (newChangeSetId && api.ok(req)) {
-        api.navigateToNewChangeSet(
-          {
-            name: "new-hotness-component",
-            params: {
-              workspacePk: route.params.workspacePk,
-              changeSetId: newChangeSetId,
-              componentId: props.componentId,
-            },
-          },
-          newChangeSetId,
-        );
-      }
-    }
-  },
-});
-
-const required = ({ value }: { value: string | undefined }) => {
-  const len = value?.trim().length ?? 0;
-  return len > 0 ? undefined : "Name required";
-};
-
-const reset = () => {
-  if (nameForm.state.isSubmitted || nameForm.state.isDirty)
-    nameForm.reset(nameFormData.value);
-};
-
-const focus = () => {
-  nextTick(() => {
-    if (nameRef.value) nameRef.value.focus();
-  });
-};
-
-const blur = () => {
-  if (nameForm.fieldInfo.name.instance?.state.meta.isDirty) {
-    // don't double submit if you were `select()'d'`
-    if (!nameForm.baseStore.state.isSubmitted) nameForm.handleSubmit();
-  } else {
-    reset();
-  }
-
-  if (editInPlaceRef.value) editInPlaceRef.value.hide();
-};
-
-// TODO(Wendy) - this code is for if we want the AttributeInput to float again
-// type ScrollFunc = (value: number) => void;
-
-// export type ComponentPageContext = {
-//   scrollAttributePanel: ScrollFunc;
-//   attributePanelScrollY: Ref<number>;
-// };
-
-// const context = reactive<ComponentPageContext>({
-//   scrollAttributePanel,
-//   attributePanelScrollY: ref(0),
-// });
-
-// watch(
-//   () => attrRef.value?.scrollY,
-//   () => {
-//     if (attrRef.value) {
-//       context.attributePanelScrollY = attrRef.value.scrollY;
-//     }
-//   },
-// );
-
-// provide("ComponentPageContext", context);
 
 // Import
 const importFunc = computed(() =>
