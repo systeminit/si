b8a684ad40042929fc4adc400cd7ffced52f87f2
John Obelenus <jobelenus@systeminit.com>
2025-07-17 18:02:09 -0400

    Bottom toast for when you are applying changes to HEAD

---
+++ app/web/src/newhotness/nav/ToastApplyingChanges.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col text-sm text-center">
+    <div>You are now on HEAD. Your changes are being applied...</div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
