915a5396943d34983072da940154379f0a5c2f2b
Fletcher Nichol <fletcher@systeminit.com>
2025-08-21 19:39:23 -0600

    feat(naxum): add optional status code to `JetstreamPostProcess` failures
    This change updates the `OnFailure::call` method signature to include
    `status: Option<StatusCode>`. As any `impl std::error::Error`'s have
    already been converted down into a `Response`, this `OnFailure`
    callback doesn't have access to the error which resulted in this
    callback being fired. However, a `StatusCode` can often be as useful to
    determine the *class* of the triggering error type and thus what needs
    to happen in the function. Future work will use this in the Rebaser
    codebase.
    
    Signed-off-by: Fletcher Nichol <fletcher@systeminit.com>

--- lib/naxum/src/middleware/jetstream_post_process/future.rs
+++ lib/naxum/src/middleware/jetstream_post_process/future.rs
@@ -1,6 +1,7 @@
 use std::{
     future::Future,
     pin::Pin,
+    sync::Arc,
     task::{
         Context,
         Poll,
@@ -13,37 +14,41 @@ use pin_project_lite::pin_project;
 use tower::Service;
 
 use crate::{
+    Head,
     message::Message,
+    middleware::jetstream_post_process::{
+        Info,
+        OnFailure,
+        OnSuccess,
+    },
     response::Response,
 };
 
 pin_project! {
-    pub struct ResponseFuture<S>
+    pub struct ResponseFuture<S, OnSuccessT, OnFailureT>
     where
         S: Service<Message<jetstream::Message>>,
     {
         #[pin]
         pub(crate) inner: S::Future,
-        #[pin]
-        pub(crate) on_success_fut: BoxFuture<'static, ()>,
-        #[pin]
-        pub(crate) on_failure_fut: BoxFuture<'static, ()>,
+        pub(crate) on_success: OnSuccessT,
+        pub(crate) on_failure: OnFailureT,
         pub(crate) state: State<S::Response, S::Error>,
     }
 }
 
-#[derive(Clone, Default)]
 pub(crate) enum State<T, E> {
-    #[default]
-    Initial,
-    Success(Option<T>),
-    Failure(Option<T>),
-    Err(Option<E>),
+    Initial(Option<(Arc<Head>, Arc<Info>)>),
+    Success(Option<T>, BoxFuture<'static, ()>),
+    Failure(Option<T>, BoxFuture<'static, ()>),
+    Err(Option<E>, BoxFuture<'static, ()>),
 }
 
-impl<S> Future for ResponseFuture<S>
+impl<S, OnSuccessT, OnFailureT> Future for ResponseFuture<S, OnSuccessT, OnFailureT>
 where
     S: Service<Message<jetstream::Message>, Response = Response>,
+    OnSuccessT: OnSuccess,
+    OnFailureT: OnFailure,
 {
     type Output = Result<S::Response, S::Error>;
 
@@ -53,42 +58,64 @@ where
         loop {
             match this.state {
                 // Poll the nested service to yield our result
-                State::Initial => {
+                State::Initial(args) => {
                     let result = futures::ready!(this.inner.as_mut().poll(cx));
 
                     match result {
                         Ok(response) => {
                             if response.status().is_success() {
+                                let (head, info) = args
+                                    .take()
+                                    .expect("extracting owned value only happens once");
+                                let status = response.status();
+
                                 // Transition the state to run the success case
-                                *this.state = State::Success(Some(response));
+                                *this.state = State::Success(
+                                    Some(response),
+                                    this.on_success.call(head, info, status),
+                                );
                             } else {
+                                let (head, info) = args
+                                    .take()
+                                    .expect("extracting owned value only happens once");
+                                let status = Some(response.status());
+
                                 // Transition the state to run the failure case
-                                *this.state = State::Failure(Some(response));
+                                *this.state = State::Failure(
+                                    Some(response),
+                                    this.on_failure.call(head, info, status),
+                                );
                             }
                         }
                         Err(err) => {
+                            let (head, info) = args
+                                .take()
+                                .expect("extracting owned value only happens once");
+                            let status = None;
+
                             // Transition the state to run the failure case
-                            *this.state = State::Err(Some(err));
+                            *this.state =
+                                State::Err(Some(err), this.on_failure.call(head, info, status));
                         }
                     }
                 }
                 // Poll the on_success future and when ready return the `Ok` type
-                State::Success(sucess_response) => {
-                    futures::ready!(this.on_success_fut.poll(cx));
+                State::Success(sucess_response, on_success_fut) => {
+                    futures::ready!(on_success_fut.as_mut().poll(cx));
                     return Poll::Ready(Ok(sucess_response
                         .take()
                         .expect("extracting owned value only happens once")));
                 }
                 // Poll the on_failure future and when ready return the `Ok` type
-                State::Failure(failure_response) => {
-                    futures::ready!(this.on_failure_fut.poll(cx));
+                State::Failure(failure_response, on_failure_fut) => {
+                    futures::ready!(on_failure_fut.as_mut().poll(cx));
                     return Poll::Ready(Ok(failure_response
                         .take()
                         .expect("extracting owned value only happens once")));
                 }
                 // Poll the on_failure future and when ready return the `Err` type
-                State::Err(err) => {
-                    futures::ready!(this.on_failure_fut.poll(cx));
+                State::Err(err, on_failure_fut) => {
+                    futures::ready!(on_failure_fut.as_mut().poll(cx));
                     return Poll::Ready(Err(err
                         .take()
                         .expect("extracting owned value only happens once")));
