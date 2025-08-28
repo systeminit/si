001dfdfeb6f27eb9949cd1edae557620ac5b29af
stack72 <public@paulstack.co.uk>
2025-08-28 21:17:20 +0300

    feat(sdf): Adding an abandon changeset to v2 routes

---
+++ lib/sdf-server/src/service/v2/change_set/abandon.rs
@@ -0,0 +1,52 @@
+use axum::extract::Path;
+use dal::{
+    ChangeSet,
+    ChangeSetId,
+    WorkspacePk,
+};
+use sdf_extract::{
+    PosthogEventTracker,
+    change_set::ChangeSetDalContext,
+};
+use si_events::audit_log::AuditLogKind;
+
+use super::{
+    ChangeSetAPIError,
+    Result,
+};
+
+pub async fn abandon(
+    ChangeSetDalContext(ref mut ctx): ChangeSetDalContext,
+    tracker: PosthogEventTracker,
+    Path((_workspace_pk, change_set_id)): Path<(WorkspacePk, ChangeSetId)>,
+) -> Result<()> {
+    if ctx.is_head().await? {
+        return Err(ChangeSetAPIError::CannotAbandonHead);
+    }
+
+    let mut change_set = ChangeSet::get_by_id(ctx, change_set_id).await?;
+    let old_status = change_set.status;
+    ctx.update_visibility_and_snapshot_to_visibility(change_set.id)
+        .await?;
+    change_set.abandon(ctx).await?;
+
+    ctx.write_audit_log(
+        AuditLogKind::AbandonChangeSet {
+            from_status: old_status.into(),
+        },
+        change_set.name,
+    )
+    .await?;
+
+    tracker.track(
+        ctx,
+        "abandon_change_set",
+        serde_json::json!({
+            "abandoned_change_set": change_set_id,
+        }),
+    );
+
+    ctx.commit_no_rebase().await?;
+
+    Ok(())
+}
