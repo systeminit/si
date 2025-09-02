41e31dc05dd7c7f5a30421742193bb5b6711eacd
Victor Bustamante <victor@systeminit.com>
2025-09-02 16:02:47 -0300

    feat: Component Details skeleton
    Signed-off-by: Victor Bustamante <victor@systeminit.com>

---
+++ app/web/src/newhotness/layout_components/ShimmeringTextLine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    :class="
+      clsx(
+        'skeleton-shimmer flex-none rounded-sm',
+        themeClasses('bg-neutral-400', 'bg-neutral-700'),
+      )
+    "
+  />
+</template>
+
+<script setup lang="ts">
+import { themeClasses } from "@si/vue-lib/design-system";
+import clsx from "clsx";
+</script>
