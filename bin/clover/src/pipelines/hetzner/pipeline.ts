a6c03c69c8c0fa4f139a075df7200d69e29a8b40
John Obelenus <jobelenus@systeminit.com>
2025-09-24 18:56:28 -0400

    WIP 2

--- bin/clover/src/pipelines/hetzner/pipeline.ts
+++ bin/clover/src/pipelines/hetzner/pipeline.ts
@@ -1,16 +1,17 @@
 import { CfProperty } from "../cfDb.ts";
 import { ExpandedPkgSpec } from "../../spec/pkgs.ts";
-import { ExpandedPropSpecFor } from "../../spec/props.ts";
+import { ExpandedPropSpecFor, DefaultPropType, createPropFromCf } from "../../spec/props.ts";
 import rawSchema from "../../provider-schemas/hetzner.json" with { type: "json" };
 
 export type HetznerSchema = {
   typeName: string;
-  description: string;
-  primaryIdentifier: JSONPointer[];
   sourceUrl?: string;
   documentationUrl?: string;
   properties: Record<string, CfProperty>;
+  requiredProperties: Set<string>;
 }
+
+export type HDB = Record<string, HetznerSchema>;
 
 type CreatePropArgs = {
   // The path to this prop, e.g. ["root", "domain"]
@@ -25,6 +26,7 @@ type CreatePropArgs = {
 
 export type HQueue = {
   cfSchema: HetznerSchema;
+  onlyProperties: OnlyProperties;
   queue: CreatePropArgs[];
 }
 
@@ -49,8 +51,7 @@ export async function generateHetznerSpecs(options: {
 }
 
 function pkgSpecFromHetnzer(allSchemas: object) {
-  const nouns = new Set();
-  const schemas = Record<string, object> = {};
+  const schemas: HDB = {};
   Object.entries(allSchemas.paths).forEach([endpoint, openApiDescription] => {
     if (!openApiDescription.get) throw new Error(`WHY NO GET? ${noun}`)
 
@@ -58,11 +59,111 @@ function pkgSpecFromHetnzer(allSchemas: object) {
     if (openApiDescription.get.operationId.startsWith("list_")) continue;
 
     const noun = endpoint.split("/")[1]
-    nouns.add(noun);
-    const schemaProps = openApiDescription.get.responses.200.content["application/json"].schema.properties.[noun].properties
-    schemas[noune] = schemaProps;
+
+    if (schemas[noun]) {
+      console.error(`ALREADY GOT ${noun}`);
+      continue;
+    }
+
+    const objShape = openApiDescription.get.responses["200"].content["application/json"].schema.properties[noun];
+    const properties = objShape.properties
+    const requiredProperties = new Set(objShape.required);
+    schemas[noun] = {
+      typeName: noun,
+      properties,
+      requiredProperties,
+    }
   });
 
-  // create our own "tree"
-  // for each schemaProp call createPropFromCf or something like it
+  Object.values(schemas).forEach((schema: HetznerSchema) => {
+    const onlyProperties: OnlyProperties = {
+      createOnly: [],
+      readOnly: [],
+      writeOnly: [],
+      primaryIdentifier: [],
+    };
+
+    const domain = createDefaultProp(
+      "domain",
+      schema.properties,
+      onlyProperties,
+      cfSchema,
+    );
+  });
+}
+
+const MAX_PROP_DEPTH = 30;
+
+function createDocLink(
+  { typeName }: CfSchema,
+  defName: string | undefined,
+  propName?: string,
+): string {
+ return "LATER GATOR"
+}
+
+function childIsRequired(
+  schema: HetznerSchema;
+  parentProp: ExpandedPropSpecFor["object" | "array" | "map"] | undefined,
+  childName: string,
+) {
+  // not correctly accounting for depth here, parent prop path is missing
+  // probably need to embed `required` into the record of properties somehow
+  return schema.requiredProperties.has(childName)
+}
+
+function createDefaultProp(
+  name: DefaultPropType,
+  properties: Record<string, CfProperty>,
+  onlyProperties: OnlyProperties,
+  cfSchema: HetznerSchema,
+) {
+  let rootProp: ExpandedPropSpecFor["object"] | undefined;
+
+  const queue: HQueue = {
+    cfSchema,
+    onlyProperties,
+    queue: [
+      {
+        propPath: ["root", name],
+        // Pretend the prop only has the specified properties (since we split it up)
+        cfProp: { ...cfSchema, properties },
+        parentProp: undefined,
+        addTo: (prop: ExpandedPropSpec) => {
+          if (prop.kind !== "object") {
+            throw new Error(`${name} prop is not an object`);
+          }
+          // Set "rootProp" before returning it
+          rootProp = prop;
+        },
+      },
+    ]
+  };
+
+  while (queue.queue.length > 0) {
+    const propArgs = queue.queue.shift()!;
+    if (propArgs.propPath.length > MAX_PROP_DEPTH) {
+      throw new Error(
+        `Prop tree loop detected: Tried creating prop more than ${MAX_PROP_DEPTH} levels deep in the prop tree: ${propArgs.propPath}`,
+      );
+    }
+
+    const prop = createPropFromCf(propArgs, queue, createDocLink, childIsRequired);
+    if (!prop) continue;
+    if (propArgs.addTo) propArgs.addTo(prop);
+  }
+
+  if (!rootProp) {
+    throw new Error(
+      `createProp for ${cfSchema.typeName} did not generate a ${name} prop`,
+    );
+  }
+
+  // Top level prop doesn't actually get the generated doc; we add that to the schema instead
+  rootProp.data.inputs = null;
+  rootProp.data.widgetOptions = null;
+  rootProp.data.hidden = false;
+  rootProp.data.documentation = null;
+
+  return rootProp;
 }
