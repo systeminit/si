8dba3065cbe4d80068205837c3f65e9d12c4ac88
stack72 <public@paulstack.co.uk>
2025-10-09 23:12:35 +0100

    chore(ci): Put luminork into maintenance mode during deploy

--- component/toolbox/scripts/supporting-funcs/ec2-funcs.sh
+++ component/toolbox/scripts/supporting-funcs/ec2-funcs.sh
@@ -5,7 +5,7 @@ list_instances() {
   filter=$1
   if [[ "${filter,,}" == "all" || -z "${filter}" ]]; then
     # shellcheck disable=SC2016
-    aws ec2 describe-instances --query 'Reservations[*].Instances[?State.Name==`running`].[Tags[?Key==`Name`].Value | [0],InstanceId,InstanceType,PrivateIpAddress]' --output text | grep -E 'sdf|veritech|pinga|rebaser|forklift|edda'
+    aws ec2 describe-instances --query 'Reservations[*].Instances[?State.Name==`running`].[Tags[?Key==`Name`].Value | [0],InstanceId,InstanceType,PrivateIpAddress]' --output text | grep -E 'sdf|veritech|pinga|rebaser|forklift|edda|luminork'
   elif [[ "${filter,,}" != "all" ]]; then
     # shellcheck disable=SC2016
     aws ec2 describe-instances --query 'Reservations[*].Instances[?State.Name==`running`].[Tags[?Key==`Name`].Value | [0],InstanceId,InstanceType,PrivateIpAddress]' --output text | grep -E "${filter}"
