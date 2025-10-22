68d8e34a1cfeae200a6cb560956ae562dbd4ed42
wendybujalski <wendydbujalski@gmail.com>
2025-10-22 13:17:08 -0400

    wform bug fixes, handling AV edge cases, BUG-1100

--- app/web/src/newhotness/AddViewModal.vue
+++ app/web/src/newhotness/AddViewModal.vue
@@ -2,18 +2,47 @@
   <!-- NOTE: the Modal CSS for height in "max" doesn't work as we might expect -->
   <Modal ref="modalRef" size="lg" title="Create View">
     <form @submit.prevent="nameForm.handleSubmit()">
+      <nameForm.Field name="name">
+        <template #default="{ field }">
+          <div
+            v-if="field.state.meta.errors.length > 0"
+            :class="
+              clsx(
+                'text-sm mb-xs',
+                themeClasses('text-destructive-600', 'text-destructive-200'),
+              )
+            "
+          >
+            {{ field.state.meta.errors[0] }}
+          </div>
+        </template>
+      </nameForm.Field>
+
       <label class="flex flex-row items-center relative">
         <span>View Name</span>
-        <nameForm.Field name="name">
+        <nameForm.Field
+          name="name"
+          :validators="{
+            onChange: ({ value }) =>
+              value.trim().length === 0 ? 'View name is required' : undefined,
+            onBlur: ({ value }) =>
+              value.trim().length === 0 ? 'View name is required' : undefined,
+          }"
+        >
           <template #default="{ field }">
             <input
               :class="
                 clsx(
                   'block w-72 ml-auto border',
-                  themeClasses(
-                    'text-black bg-white border-neutral-600 disabled:bg-neutral-100',
-                    'text-white bg-black border-neutral-400 disabled:bg-neutral-900',
-                  ),
+                  field.state.meta.errors.length > 0
+                    ? themeClasses(
+                        'text-black bg-white border-destructive-600 disabled:bg-neutral-100',
+                        'text-white bg-black border-destructive-400 disabled:bg-neutral-900',
+                      )
+                    : themeClasses(
+                        'text-black bg-white border-neutral-600 disabled:bg-neutral-100',
+                        'text-white bg-black border-neutral-400 disabled:bg-neutral-900',
+                      ),
                 )
               "
               :value="field.state.value"
@@ -104,7 +133,7 @@ const nameForm = wForm.newForm({
   },
   validators: {
     onSubmit: ({ value }) =>
-      value.name.length === 0 ? "Name is required" : undefined,
+      value.name.trim().length === 0 ? "Name is required" : undefined,
   },
   watchFn: () => props.views?.length ?? 0,
 });
