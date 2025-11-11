f1fc5c27cf3f5e58f9e11f49d231dc1fea621edc
Zachary Hamm <zack@systeminit.com>
2025-11-11 17:03:41 -0600

    dbg: inspect the tasks stream directly

--- lib/edda-server/src/server.rs
+++ lib/edda-server/src/server.rs
@@ -25,6 +25,7 @@ use frigg::{
     FriggStore,
     frigg_kv,
 };
+use futures::StreamExt;
 use naxum::{
     Message,
     MessageHead,
@@ -211,6 +212,21 @@ impl Server {
             .await?
             .messages()
             .await?;
+
+        let tasks = tasks.inspect(|m| match m {
+            Ok(message) => {
+                if message
+                    .subject()
+                    .as_str()
+                    .contains(super::BAD_CHANGE_SET_ID)
+                {
+                    info!("DBG: message on tasks for bad change set: {:?}", message);
+                }
+            }
+            Err(err) => {
+                info!("DBG: error on tasks: {:?}", err);
+            }
+        });
 
         let requests_stream = nats::edda_requests_jetstream_stream(&context).await?;
 
