73b8ed1096ab0a76606f1b71cca5d53225380790
Zachary Hamm <zack@systeminit.com>
2025-12-17 12:43:19 -0600

    fix: ensure no duplicate actions from other change sets
    When actions are replayed from head to other change sets, it's possible
    to get duplicate actions for the same exclusive "kind" (Create, Destroy,
    Refresh, Update). This correction prevents this.

--- lib/dal/tests/integration_test/node_weight/action.rs
+++ lib/dal/tests/integration_test/node_weight/action.rs
@@ -1,13 +1,18 @@
 use std::collections::HashSet;
 
+use base64::Engine;
 use dal::{
     self,
     Component,
     ComponentId,
     DalContext,
+    Func,
     action::{
         Action,
-        prototype::ActionKind,
+        prototype::{
+            ActionKind,
+            ActionPrototype,
+        },
     },
     component::delete::delete_component,
 };
@@ -78,6 +83,125 @@ async fn prevent_orphaned_update_actions(ctx: &mut DalContext) -> Result<()> {
     ctx.update_visibility_and_snapshot_to_visibility(beta_change_set_id)
         .await?;
     assert!(Action::list_topologically(ctx).await?.is_empty());
+
+    Ok(())
+}
+
+#[test]
+async fn prevent_duplicate_exclusive_actions(ctx: &mut DalContext) -> Result<()> {
+    let test_variant_id = dal_test::helpers::schema::variant::create(
+        ctx,
+        "delete_will_fail",
+        r#"
+        function main() {
+            return {
+               props: [
+                {
+                    name: "a_prop",
+                    kind: "string",
+                }
+               ]
+            };
+        }
+        "#,
+    )
+    .await?;
+
+    let create_action_code = r#"async function main() {
+            return { payload: { "data": "you gotta have it" }, status: "ok" };
+        }"#;
+
+    let create_func = Func::new(
+        ctx,
+        "test:createAction",
+        None::<String>,
+        None::<String>,
+        None::<String>,
+        false,
+        false,
+        dal::FuncBackendKind::JsAction,
+        dal::FuncBackendResponseType::Action,
+        "main".into(),
+        Some(base64::engine::general_purpose::STANDARD_NO_PAD.encode(create_action_code)),
+        false,
+    )
+    .await?;
+
+    let _create_prototype = ActionPrototype::new(
+        ctx,
+        ActionKind::Create,
+        "test:createAction".into(),
+        None,
+        test_variant_id,
+        create_func.id,
+    )
+    .await?;
+
+    let delete_action_code = r#"async function main() {
+            return { status: "error" };
+        }"#;
+
+    let delete_func = Func::new(
+        ctx,
+        "test:deleteAction",
+        None::<String>,
+        None::<String>,
+        None::<String>,
+        false,
+        false,
+        dal::FuncBackendKind::JsAction,
+        dal::FuncBackendResponseType::Action,
+        "main".into(),
+        Some(base64::engine::general_purpose::STANDARD_NO_PAD.encode(delete_action_code)),
+        false,
+    )
+    .await?;
+
+    let _delete_proto = ActionPrototype::new(
+        ctx,
+        ActionKind::Destroy,
+        "test:deleteAction".into(),
+        None,
+        test_variant_id,
+        delete_func.id,
+    )
+    .await?;
+
+    let component_id =
+        dal_test::helpers::component::create(ctx, test_variant_id, "du côté de chez swann").await?;
+
+    ChangeSetTestHelpers::apply_change_set_to_base(ctx).await?;
+    ChangeSetTestHelpers::wait_for_actions_to_run(ctx).await?;
+
+    let cs_1 = ChangeSetTestHelpers::fork_from_head_change_set(ctx)
+        .await?
+        .id;
+    ChangeSetTestHelpers::switch_to_change_set(ctx, cs_1).await?;
+
+    let component = Component::get_by_id(ctx, component_id).await?;
+    component.delete(ctx).await?;
+
+    let cs_2 = ChangeSetTestHelpers::fork_from_head_change_set(ctx)
+        .await?
+        .id;
+    ChangeSetTestHelpers::switch_to_change_set(ctx, cs_2).await?;
+
+    let component = Component::get_by_id(ctx, component_id).await?;
+    component.delete(ctx).await?;
+
+    ChangeSetTestHelpers::apply_change_set_to_base(ctx).await?;
+    ChangeSetTestHelpers::wait_for_actions_to_run(ctx).await?;
+
+    let actions = Action::list_topologically(ctx).await?;
+
+    let action_id = actions[0];
+    let action = Action::get_by_id(ctx, action_id).await?;
+    assert_eq!(ActionState::Failed, action.state());
+
+    ChangeSetTestHelpers::switch_to_change_set(ctx, cs_1).await?;
+
+    let actions = Action::list_topologically(ctx).await?;
+    assert_eq!(actions.len(), 1);
 
     Ok(())
 }
