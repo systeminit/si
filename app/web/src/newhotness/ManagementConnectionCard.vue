916db6285e4903134d7f964181ac4d021a6aaf14
Victor Bustamante <victor@systeminit.com>
2026-01-15 16:58:21 -0300

    feat(web): add navigation on ManagementConnectionCard

--- app/web/src/newhotness/ManagementConnectionCard.vue
+++ app/web/src/newhotness/ManagementConnectionCard.vue
@@ -2,16 +2,19 @@
   <li
     :class="
       clsx(
-        'flex flex-row items-center gap-xs [&>*]:text-sm [&>*]:font-bold',
+        'flex flex-row items-center gap-xs [&>*]:text-sm [&>*]:font-bold cursor-pointer mx-xs py-2xs',
         themeClasses('[&>*]:border-neutral-400', '[&>*]:border-neutral-600'),
+        themeClasses('hover:bg-neutral-100', 'hover:bg-neutral-700'),
         selectable && [
-          'border border-transparent p-2xs cursor-pointer',
+          'border border-transparent p-2xs',
           themeClasses('hover:border-action-500', 'hover:border-action-300'),
           selected && themeClasses('bg-action-200', 'bg-action-900'),
         ],
       )
     "
-    @click="emit('select')"
+    @click="selectable ? emit('select') : navigateToComponent($event)"
+    @mouseenter="isHovering = true"
+    @mouseleave="isHovering = false"
   >
     <TextPill
       mono
@@ -19,8 +22,8 @@
         clsx(
           'min-w-0',
           themeClasses(
-            'text-newhotness-greenlight',
-            'text-newhotness-greendark',
+            'text-newhotness-greenlight bg-neutral-100',
+            'text-newhotness-greendark bg-neutral-900',
           ),
         )
       "
@@ -36,10 +39,10 @@
       mono
       :class="
         clsx(
-          'min-w-0',
+          'min-w-0 flex flex-row items-center gap-xs',
           themeClasses(
-            'text-newhotness-purplelight',
-            'text-newhotness-purpledark',
+            'text-newhotness-purplelight bg-neutral-100',
+            'text-newhotness-purpledark bg-neutral-900',
           ),
         )
       "
@@ -52,16 +55,17 @@
 </template>
 
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, ref } from "vue";
 import clsx from "clsx";
 import {
   TextPill,
   themeClasses,
   TruncateWithTooltip,
 } from "@si/vue-lib/design-system";
+import { useRouter } from "vue-router";
 import { assertIsDefined, Context } from "./types";
 
-defineProps({
+const props = defineProps({
   componentId: { type: String, required: true },
   selectable: { type: Boolean },
   selected: { type: Boolean },
@@ -73,4 +77,19 @@ assertIsDefined<Context>(ctx);
 const emit = defineEmits<{
   (e: "select"): void;
 }>();
+
+const router = useRouter();
+const isHovering = ref(false);
+
+const navigateToComponent = (e: MouseEvent) => {
+  e.stopPropagation();
+  const params = { ...router.currentRoute.value.params };
+  const query = { ...router.currentRoute.value.query };
+  params.componentId = props.componentId;
+  router.push({
+    name: "new-hotness-component",
+    params,
+    query,
+  });
+};
 </script>
