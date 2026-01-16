9caa1163a4d07a6379d45430f22c493079a7353a
Nick Gerace <nick@systeminit.com>
2026-01-16 17:04:46 -0500

    Ensure tree builds despite secret parse error
    This change ensures that the attribute tree MV builds despite whatever
    secret parse error may occur. It is unclear what causes this error
    other than that it occurs during active schema/asset development.
    Regardless of the root cause, the MV should not fail under this
    condition; much like the existing secret key cache check in the code.
    
    Additional changes:
    - The root error enum variant now uses "source" rather than "from"
      (provided by "thiserror") in order to pinpoint what locations the
      error can occur at
    - All code related to rendering the value and secret has been moved into
      one function apart from the attribute tree MV assemble function
    
    Signed-off-by: Nick Gerace <nick@systeminit.com>

--- lib/dal-materialized-views/src/component/attribute_tree.rs
+++ lib/dal-materialized-views/src/component/attribute_tree.rs
@@ -6,21 +6,29 @@ use std::collections::{
 use dal::{
     AttributePrototype,
     AttributeValue,
+    AttributeValueId,
     Component,
     DalContext,
     Prop,
+    PropId,
     Secret,
+    SecretError,
+    SecretId,
     component::ControllingFuncData,
+    secret::EncryptedSecretKey,
     validation::ValidationOutputNode,
     workspace_snapshot::traits::func::FuncExt as _,
 };
-use si_frontend_mv_types::component::attribute_tree::{
-    self,
-    AttributeTree,
-    AttributeValue as AttributeValueMv,
-    AvTreeInfo,
-    ExternalSource,
-    ValidationOutput,
+use si_frontend_mv_types::{
+    component::attribute_tree::{
+        self,
+        AttributeTree,
+        AttributeValue as AttributeValueMv,
+        AvTreeInfo,
+        ExternalSource,
+        ValidationOutput,
+    },
+    secret::Secret as SecretMv,
 };
 use si_id::ComponentId;
 use telemetry::prelude::*;
@@ -63,57 +71,15 @@ pub async fn assemble(ctx: DalContext, component_id: ComponentId) -> crate::Resu
 
         // Build si_frontend_mv_types::AttributeValue & add to attribute_values HashMap.
         let key = AttributeValue::key_for_id(ctx, av_id).await?;
-        let (value, maybe_secret) = {
-            let mut default_none_secret_id = None;
-            let mut value = match AttributeValue::get_by_id(ctx, av_id)
-                .await?
-                .value(ctx)
-                .await?
-            {
-                Some(value) => value,
-                None => match prop_id {
-                    Some(prop_id) => Prop::default_value(ctx, prop_id)
-                        .await?
-                        .unwrap_or(serde_json::Value::Null),
-                    None => serde_json::Value::Null,
-                },
-            };
-
-            if value != serde_json::Value::Null
-                && maybe_parent_av_id == Some(secrets_category_av_id)
-            {
-                let secret_key = Secret::key_from_value_in_attribute_value(value)?;
-                value = match secret_ids_by_key.get(&secret_key) {
-                    Some(secret_id) => {
-                        let secret = secret::assemble(ctx, *secret_id).await?;
-                        default_none_secret_id = Some(secret);
-                        serde_json::to_value(secret_id)?
-                    }
-
-                    None => {
-                        // NOTE(nick): I ported this comment.
-                        //
-                        // If none of the secrets in the workspace have this key, we assume
-                        // that dependent values haven't updated yet and will be fixed
-                        // shortly. Thus we treat the property as missing for now and
-                        // return null.
-                        //
-                        // This is an expected issue, so we don't warn--but it could trigger
-                        // if something more serious is going on that is making the lookup
-                        // fail more persistently, so we may want to measure how often it
-                        // happens and figure out how to alert in that case.
-                        warn!(
-                            name: "Secret key does not match",
-                            av_id = %av_id,
-                            "Secret key in dependent value does not match any secret key; assuming that dependent values are not up to date and treating the property temporarily as missing",
-                        );
-                        serde_json::Value::Null
-                    }
-                }
-            }
-
-            (value, default_none_secret_id)
-        };
+        let (value, maybe_secret) = render_value_and_secret(
+            ctx,
+            av_id,
+            prop_id,
+            maybe_parent_av_id,
+            secrets_category_av_id,
+            &secret_ids_by_key,
+        )
+        .await?;
 
         let subscriptions = AttributeValue::subscriptions(ctx, av_id).await?;
 
@@ -215,4 +181,85 @@ pub async fn assemble(ctx: DalContext, component_id: ComponentId) -> crate::Resu
         component_name: component.name(ctx).await?,
         schema_name: component.schema(ctx).await?.name,
     })
+}
+
+async fn render_value_and_secret(
+    ctx: &DalContext,
+    av_id: AttributeValueId,
+    maybe_prop_id: Option<PropId>,
+    maybe_parent_av_id: Option<AttributeValueId>,
+    secrets_category_av_id: AttributeValueId,
+    secret_ids_by_key: &HashMap<EncryptedSecretKey, SecretId>,
+) -> crate::Result<(serde_json::Value, Option<SecretMv>)> {
+    let maybe_value = AttributeValue::get_by_id(ctx, av_id)
+        .await?
+        .value(ctx)
+        .await?;
+
+    // We only want to proceed if we are dealing with a non-null value and the parent is the secret
+    // category node. In all other conditions, either return the value if there is one or use the
+    // prop's default value if it exists.
+    let value = match maybe_value {
+        Some(value)
+            if value != serde_json::Value::Null
+                && maybe_parent_av_id == Some(secrets_category_av_id) =>
+        {
+            value
+        }
+        Some(value) => return Ok((value, None)),
+        None => {
+            let value = match maybe_prop_id {
+                Some(prop_id) => Prop::default_value(ctx, prop_id)
+                    .await?
+                    .unwrap_or(serde_json::Value::Null),
+                None => serde_json::Value::Null,
+            };
+            return Ok((value, None));
+        }
+    };
+
+    // Try to extract the secret key from the value in the attribute value.
+    match Secret::key_from_value_in_attribute_value(value) {
+        Ok(secret_key) => {
+            // Check the cache to assemble the secret accordingly.
+            match secret_ids_by_key.get(&secret_key) {
+                Some(secret_id) => {
+                    let rendered_value = serde_json::to_value(secret_id)?;
+                    let secret = secret::assemble(ctx, *secret_id).await?;
+                    Ok((rendered_value, Some(secret)))
+                }
+                None => {
+                    // NOTE(nick): I ported this comment.
+                    //
+                    // If none of the secrets in the workspace have this key, we assume
+                    // that dependent values haven't updated yet and will be fixed
+                    // shortly. Thus we treat the property as missing for now and
+                    // return null.
+                    //
+                    // This is an expected issue, so we don't warn--but it could trigger
+                    // if something more serious is going on that is making the lookup
+                    // fail more persistently, so we may want to measure how often it
+                    // happens and figure out how to alert in that case.
+                    warn!(
+                        name: "Secret key does not match",
+                        %av_id,
+                        "Secret key in dependent value does not match any secret key; assuming that dependent values are not up to date and treating the property temporarily as missing",
+                    );
+                    Ok((serde_json::Value::Null, None))
+                }
+            }
+        }
+        Err(SecretError::EncryptedSecretKeyParse(_)) => {
+            // NOTE(nick): this appears to happen during authoring, but the reason is
+            // unclear. We need to not only avoid hard failing the MV build here, but also
+            // use tracing to figure out why this happens in authoring over the long term.
+            warn!(
+                name: "Value for secret attribute value is not a secret key",
+                %av_id,
+                "Value in dependent value is not a secret key; assuming that dependent values are not up to date and treating the property temporarily as missing",
+            );
+            Ok((serde_json::Value::Null, None))
+        }
+        Err(err) => Err(err.into()),
+    }
 }
